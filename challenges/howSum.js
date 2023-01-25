3dadd5ee0a2bbf536ebc032f3a18fa7aaefd6a1c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-24 20:38:56 -0800

    Added how sum challenge functions

---
+++ challenges/howSum.js
@@ -0,0 +1,37 @@
+// == How Sum ==
+// Given a target value and an array, return an array of possible combinations of numbers that add up to the target. If multiple combinations are possible, return any of one of the combination arrays. If none return null.
+
+// NOTE: The howSum functions below need to be revisited, as they run into errors
+
+// This works but can be optimized to improve runtime
+function howSum1(target, numbers) {
+    // Base cases
+    if (target === 0) return [];
+    if (target < 0) return null;
+    for (let num in numbers) {
+        const remainder = target - num;
+        const remainderResult = howSum1(remainder, numbers);
+        if (remainderResult !== null) return [...remainderResult, num];
+    }
+    return null;
+}
+
+// Optimized version of howSum1
+function howSum2(target, numbers, memo={}) {
+    if (target in memo) return memo[target];
+    // Base cases
+    if (target === 0) return [];
+    if (target < 0) return null;
+    for (let num in numbers) {
+        const remainder = target - num;
+        const remainderResult = howSum2(remainder, numbers, memo);
+        if (remainderResult !== null) {
+            memo[target] = [...remainderResult, num];
+            return memo[target];
+        }
+    }
+    memo[target] = null;
+    return null;
+}
+
+console.log(howSum2(300, [7,14]));
