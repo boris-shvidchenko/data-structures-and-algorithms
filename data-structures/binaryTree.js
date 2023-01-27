e38e0d698f9aa3374d8ae708ac45d3b0a4283893
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-27 09:36:42 -0800

    Added sum and min problems for binary tree data structure

--- data-structures/binaryTree.js
+++ data-structures/binaryTree.js
@@ -19,13 +19,25 @@ b.left = d;
 b.right = e;
 c.right = f;
 
+const g = new Node(3);
+const h = new Node(11);
+const i = new Node(4);
+const j = new Node(4);
+const k = new Node(2);
+const l = new Node(1);
+g.left = h;
+g.right = i;
+h.left = j;
+h.right = k;
+i.right = l;
+
 // === Algorithms ===
 
 // Binary Tree depth first traversal algorithm
 // Prompt: Write a function that take in the root of a binary tree and returns an array with all of the values in the tree in depth-first order. Depth-first traversal means traverse top-down from left to right. 
 
 // Iterative
-function depthFirstIterative(root){
+function depthFirstIterative(root) {
     if (root === null) return [];
     const result = [];
     const stack = [root];
@@ -41,7 +53,7 @@ function depthFirstIterative(root){
 // console.log(depthFirstIterative(a));
 
 // Recursive
-function depthFirstRecursive(root){
+function depthFirstRecursive(root) {
     if (root === null) return [];
     const leftValues = depthFirstRecursive(root.left);
     const rightValues = depthFirstRecursive(root.right);
@@ -49,6 +61,15 @@ function depthFirstRecursive(root){
 }
 // Test
 // console.log(depthFirstRecursive(a));
+
+// Search for target value in tree (depth first), return T/F if it exists.
+function depthFirstSearch(root, target) {
+    if (root === null) return false;
+    if (root.val === target) return true;
+    return depthFirstSearch(root.left, target) || depthFirstSearch(root.right, target);
+}
+// Test
+// console.log(depthFirstSearch(a, 'e'));
 
 // Binary Tree breadth first traversal algorithm
 // Prompt: Write a function that takes the root of a binary tree and returns an array in breadth-first (level) order. Depth-first traverses top-down from left to right by level.
@@ -68,3 +89,73 @@ function breadthFirst(root) {
 // Test
 // console.log(breadthFirst(a));
 
+// Search for target value in tree (breadth first), return T/F if it exists.
+
+function breadthFirstSearch(root, target) {
+    if (root === null) return false;
+    const queue = [root];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        if (current.val === target) return true
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right)
+    }
+    return false;
+}
+// Test
+// console.log(breadthFirstSearch(a,'e'));
+
+// Tree Sum 
+// Prompt: Return the sum of a tree
+
+// Recursive
+function treeSumRecursive(root) {
+    if (root === null) return 0;
+    return root.val + treeSumRecursive(root.left) + treeSumRecursive(root.right);
+}
+// Test
+// console.log(treeSum(g));
+
+// Iterative
+function treeSumIterative(root) {
+    if (root === null) return 0;
+    let totalSum = 0;
+    const queue = [root];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        totalSum += current.val;
+        if (current.left !== null) queue.push(current.left);
+        if (current.right !== null) queue.push(current.right);
+    }
+    return totalSum;
+}
+// Test
+// console.log(treeSumIterative(g));
+
+// Tree Min
+// Prompt: Return the minimum value of a tree
+
+// Recursive
+function treeMinRecursive(root){
+    if (root === null) return Infinity;
+    const leftMin = treeMinRecursive(root.left);
+    const rightMin = treeMinRecursive(root.right);
+    return Math.min(root.val, leftMin, rightMin);
+}
+// Test
+console.log(treeMinRecursive(g));
+
+// Iterative
+function treeMinIterative(root){
+    let smallest = Infinity; // Inifinity will be the default value
+    const stack = [root];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        if (current.val < smallest) smallest = current.val;
+        if (current.left !== null) stack.push(current.left);
+        if (current.right !== null) stack.push(current.right);
+    }
+    return smallest;
+}
+// Test
+// console.log(treeMinIterative(g));
