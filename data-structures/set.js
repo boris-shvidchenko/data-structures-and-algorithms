72b0e8a5dfe37470c11a3f5dc47b2aca52c1cda8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-02-01 12:58:49 -0800

    Added reference to set object type

---
+++ data-structures/set.js
@@ -0,0 +1,16 @@
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
+
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
+
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
+
