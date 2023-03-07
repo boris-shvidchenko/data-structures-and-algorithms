9f0aa389ca9a07ec723cd6aed89cdbe4fb6c5ea3
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-07 15:12:35 -0800

    Practice with fib memo

--- challenges/fibMemoization.js
+++ challenges/fibMemoization.js
@@ -2,19 +2,21 @@
 // Find the nth value of a fibonacci sequence by optimizing a recursive function.
 
 // The following method works, but has a runtime that is exponential. Therefore a simple search of n can take a long time.
-function fib1(n) {
-    if (n <= 2) return 1;
-    return fib1(n-1) + fib(n-2);
-};
+// function fib1(n) {
+//     if (n <= 2) return 1;
+//     return fib1(n-1) + fib(n-2);
+// };
 
-// A better way is to use the following method. This pattern stores our recursive outcome in a js object (memoization), increasing overall runtime by optimizing the process.
-function fib2(n, memo = {}) {
-    // Check if n has already been saved in memo
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = fib2(n-1, memo) + fib2(n-2, memo);
-    return memo[n];
-}
+// // A better way is to use the following method. This pattern stores our recursive outcome in a js object (memoization), increasing overall runtime by optimizing the process.
+// function fib2(n, memo = {}) {
+//     // Check if n has already been saved in memo
+//     if (n in memo) return memo[n];
+//     if (n <= 2) return 1;
+//     memo[n] = fib2(n-1, memo) + fib2(n-2, memo);
+//     return memo[n];
+// }
+
+
 
 // Test 
 console.log(fib2(50));
