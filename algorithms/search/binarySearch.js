c8e01f354c699a325e779f44ab9e316c43d31895
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-08 19:17:16 -0800

    binary Search to original

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -2,49 +2,26 @@
 // Searchs an array for a target value by 1) splitting the array in half, 2) identifying whether the target value is ===, >, or < the value in the middle of the array, 3) removing the portion of the array that is < or > the target value, 3) repeats the process until the target value is identified. This algorithm assumes that the array is sorted.
 // Runtime: O(log(n)) / Binary
 
-// const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// Returns the index of the target value within the provided array
-// function binarySearch(array, targetValue) {
-//     const midPoint = Math.floor(array.length/2);
-//     // First statement is the base case
-//     if (array.length === 1) {
-//         if (targetValue === array[midPoint]) {
-//             return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
-//         } else {
-//             return `${targetValue} is not in [${testArray}]`;
-//         }
-//     } else if (targetValue < array[midPoint]) {
-//         const arrayLeft = array.slice(0, midPoint);
-//         return binarySearch(arrayLeft, targetValue);
-//     } else {
-//         const arrayRight = array.slice(midPoint);
-//         return binarySearch(arrayRight, targetValue);
-//     }
-// }
-
 const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const binarySearch = (list, targetValue) => {
-    console.log(list);
-    if (list.length <= 1) {
-        if (list[0] === targetValue) return true;
-        return false;
-    } else {
-        const midPoint = Math.floor(list.length/2);
-        if (targetValue === list[midPoint]) {
-            return true;
+// Returns the index of the target value within the provided array
+function binarySearch(array, targetValue) {
+    const midPoint = Math.floor(array.length/2);
+    // First statement is the base case
+    if (array.length === 1) {
+        if (targetValue === array[midPoint]) {
+            return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
         } else {
-            if (targetValue < list[midPoint]) {
-                const left = list.slice(0, midPoint);
-                return binarySearch(left, targetValue);
-            } else {
-                const right = list.slice(midPoint);
-                return binarySearch(right, targetValue);
-            }
+            return `${targetValue} is not in [${testArray}]`;
         }
+    } else if (targetValue < array[midPoint]) {
+        const arrayLeft = array.slice(0, midPoint);
+        return binarySearch(arrayLeft, targetValue);
+    } else {
+        const arrayRight = array.slice(midPoint);
+        return binarySearch(arrayRight, targetValue);
     }
-};
+}
 
 // Test log 
-console.log(binarySearch(testArray,3))
+console.log(binarySearch(testArray,2))
