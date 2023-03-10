8e3942c2bca4f94451f82ff33b397c946778697c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-10 14:29:37 -0800

    Pracitce with binary search alg

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -4,24 +4,48 @@
 
 const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Returns the index of the target value within the provided array
-function binarySearch(array, targetValue) {
-    const midPoint = Math.floor(array.length/2);
-    // First statement is the base case
-    if (array.length === 1) {
-        if (targetValue === array[midPoint]) {
-            return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
-        } else {
-            return `${targetValue} is not in [${testArray}]`;
-        }
-    } else if (targetValue < array[midPoint]) {
-        const arrayLeft = array.slice(0, midPoint);
-        return binarySearch(arrayLeft, targetValue);
-    } else {
-        const arrayRight = array.slice(midPoint);
-        return binarySearch(arrayRight, targetValue);
-    }
-}
+// // Returns the index of the target value within the provided array
+// function binarySearch(array, targetValue) {
+//     const midPoint = Math.floor(array.length/2);
+//     // First statement is the base case
+//     if (array.length === 1) {
+//         if (targetValue === array[midPoint]) {
+//             return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
+//         } else {
+//             return `${targetValue} is not in [${testArray}]`;
+//         }
+//     } else if (targetValue < array[midPoint]) {
+//         const arrayLeft = array.slice(0, midPoint);
+//         return binarySearch(arrayLeft, targetValue);
+//     } else {
+//         const arrayRight = array.slice(midPoint);
+//         return binarySearch(arrayRight, targetValue);
+//     }
+// }
+
 
 // Test log 
-console.log(binarySearch(testArray,2))
+
+const binarySearch = (list, target) => {
+    console.log(list);
+    if (list.length <= 1) {
+        if (list[0] === target) {
+            return true;
+        }
+        return false;
+    } else {
+        const midPoint = Math.floor(list.length/2);
+        if (list[midPoint] === target) {
+            return true;
+        } else {
+            if (list[midPoint] > target) {
+                return binarySearch(list.slice(0, midPoint), target);
+            }
+            return binarySearch(list.slice(midPoint), target);
+        }
+    }
+
+};
+
+
+console.log(binarySearch(testArray,5))
