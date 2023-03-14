79e8e6139b8d9727690268ae0ffd0b3ba2099c78
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-13 20:31:56 -0700

    graph review and depth first search practice

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -2,139 +2,139 @@
 
 // === Vertex and edge list ===
 
-const vertices = ['A', 'B', 'C', 'D', 'E'];
-const edges = [
-    ['A','B'],
-    ['A','D'],
-    ['B','C'],
-    ['C','D'],
-    ['C','E'],
-    ['D','E'],
-];
+// const vertices = ['A', 'B', 'C', 'D', 'E'];
+// const edges = [
+//     ['A','B'],
+//     ['A','D'],
+//     ['B','C'],
+//     ['C','D'],
+//     ['C','E'],
+//     ['D','E'],
+// ];
 
-// --- Operations ---
+// // --- Operations ---
 
-// Find adjacent nodes
-function findAdjacentNodes(node){
-    let adjacentNodes = [];
-    edges.forEach(edge => {
-        edge[0] === node ? adjacentNodes.push(edge[1]) : edge[1] === node ? adjacentNodes.push(edge[0]) : null;
-    });
-    return adjacentNodes;
-}
-// console.log(findAdjacentNodes('C')); // Expected: ['B','D','E']
-
-
-// Are nodes connected? (T/F)
-function isConnected(node1, node2) {
-    for (let edge of edges) {
-        if (edge.includes(node1) && edge.includes(node2)) {
-            return true;     
-        }
-    }
-    return false;
-}
-// console.log(isConnected('A','B')); // Expected: true
-// console.log(isConnected('A','E')); // Expected: false
-// console.log(isConnected('C','D')); // Expected: true
-
-
-// === Adjacency matrix ===
-
-const vertices2 = ['A', 'B', 'C', 'D', 'E'];
-const adjacencyMatrix = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 0],
-    [0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 1],
-    [0, 0, 1, 1, 0],
-];
-
-// --- Operations ---
-
-// Find adjacent nodes
-function findAdjacentNodes2(node) {
-    if (!vertices2.includes(node)) return 'Node not in vertices list';
-    let adjacencyArray = [];
-    const rowIndex = vertices2.indexOf(node);
-    const row = adjacencyMatrix[rowIndex];
-    for (let i = 0; i <= row.length - 1; i++) {
-        row[i] === 1 ? adjacencyArray.push(vertices2[i]) : null;
-    }
-    return adjacencyArray;
-}
-// console.log(findAdjacentNodes2('A')); // Expected: ['B', 'D']
-// console.log(findAdjacentNodes2('H')); // Expected: Node not in vertices list
-// console.log(findAdjacentNodes2('C')); // Expected: ['B', 'D', 'E']
-
-// Are nodes connected? (T/F)
-function isConnected2(node1, node2) {
-    const rowIndex = vertices2.indexOf(node1);
-    const colIndex = vertices2.indexOf(node2);
-    const row = adjacencyMatrix[rowIndex];
-    if (row[colIndex] === 1) return true;
-    return false;
-}
-// console.log(isConnected2('A','B')); // Expected: true
-// console.log(isConnected2('A','E')); // Expected: false
-// console.log(isConnected2('C','D')); // Expected: true
-
-
-// === Adjacency list ===
-// class Node {
-//     constructor(value) {
-//         this.value = value;
-//         this.edgesList = [];
-//     }
-//     // Connect node to another node
-//     connect(node) {
-//         this.edgesList.push(node);
-//         node.edgesList.push(this); // Undirected graph, make sure to push self to edge list of other node
-//     }
-//     // Adjacent nodes.
-//     getAdjacentNodes() {
-//         return this.edgesList.map(node => node.value);
-//     }
-//     // Is connected (T/F)?
-//     isConnected(node) {
-//         if (this.edgesList.includes(node)) return true;
-//         return false;
-//     }
+// // Find adjacent nodes
+// function findAdjacentNodes(node){
+//     let adjacentNodes = [];
+//     edges.forEach(edge => {
+//         edge[0] === node ? adjacentNodes.push(edge[1]) : edge[1] === node ? adjacentNodes.push(edge[0]) : null;
+//     });
+//     return adjacentNodes;
 // }
+// // console.log(findAdjacentNodes('C')); // Expected: ['B','D','E']
 
-// class Graph {
-//     constructor(nodes) {
-//         this.nodes = [...nodes];
-//     }
-//     // Add new node to graph after initialization 
-//     addToGraph(node) {
-//         this.nodes.push(node);
-//     }
 
+// // Are nodes connected? (T/F)
+// function isConnected(node1, node2) {
+//     for (let edge of edges) {
+//         if (edge.includes(node1) && edge.includes(node2)) {
+//             return true;     
+//         }
+//     }
+//     return false;
 // }
+// // console.log(isConnected('A','B')); // Expected: true
+// // console.log(isConnected('A','E')); // Expected: false
+// // console.log(isConnected('C','D')); // Expected: true
 
-// Create nodes
-// const nodeA = new Node('A');
-// const nodeB = new Node('B');
-// const nodeC = new Node('C');
-// const nodeD = new Node('D');
-// const nodeE = new Node('E');
-// Create graph
-// const graph = new Graph([nodeA, nodeB, nodeC, nodeD, nodeE]);
-// Add connections
-// nodeA.connect(nodeB);
-// nodeA.connect(nodeD);
-// nodeB.connect(nodeC);
-// nodeC.connect(nodeD);
-// nodeC.connect(nodeE);
-// nodeD.connect(nodeE);
 
-// console.log(nodeA.getAdjacentNodes()); // Expected: [B, D]
-// console.log(nodeC.getAdjacentNodes()); // Expected: [B, D, E]
-// console.log(nodeA.isConnected(nodeB)); // Expected: true
-// console.log(nodeA.isConnected(nodeC)); // Expected: false
+// // === Adjacency matrix ===
 
-// === Graph breadth first traversal, depth first traversal and shortest path ===
+// const vertices2 = ['A', 'B', 'C', 'D', 'E'];
+// const adjacencyMatrix = [
+//     [0, 1, 0, 1, 0],
+//     [1, 0, 1, 0, 0],
+//     [0, 1, 0, 1, 1],
+//     [1, 0, 1, 0, 1],
+//     [0, 0, 1, 1, 0],
+// ];
+
+// // --- Operations ---
+
+// // Find adjacent nodes
+// function findAdjacentNodes2(node) {
+//     if (!vertices2.includes(node)) return 'Node not in vertices list';
+//     let adjacencyArray = [];
+//     const rowIndex = vertices2.indexOf(node);
+//     const row = adjacencyMatrix[rowIndex];
+//     for (let i = 0; i <= row.length - 1; i++) {
+//         row[i] === 1 ? adjacencyArray.push(vertices2[i]) : null;
+//     }
+//     return adjacencyArray;
+// }
+// // console.log(findAdjacentNodes2('A')); // Expected: ['B', 'D']
+// // console.log(findAdjacentNodes2('H')); // Expected: Node not in vertices list
+// // console.log(findAdjacentNodes2('C')); // Expected: ['B', 'D', 'E']
+
+// // Are nodes connected? (T/F)
+// function isConnected2(node1, node2) {
+//     const rowIndex = vertices2.indexOf(node1);
+//     const colIndex = vertices2.indexOf(node2);
+//     const row = adjacencyMatrix[rowIndex];
+//     if (row[colIndex] === 1) return true;
+//     return false;
+// }
+// // console.log(isConnected2('A','B')); // Expected: true
+// // console.log(isConnected2('A','E')); // Expected: false
+// // console.log(isConnected2('C','D')); // Expected: true
+
+
+// // === Adjacency list ===
+// // class Node {
+// //     constructor(value) {
+// //         this.value = value;
+// //         this.edgesList = [];
+// //     }
+// //     // Connect node to another node
+// //     connect(node) {
+// //         this.edgesList.push(node);
+// //         node.edgesList.push(this); // Undirected graph, make sure to push self to edge list of other node
+// //     }
+// //     // Adjacent nodes.
+// //     getAdjacentNodes() {
+// //         return this.edgesList.map(node => node.value);
+// //     }
+// //     // Is connected (T/F)?
+// //     isConnected(node) {
+// //         if (this.edgesList.includes(node)) return true;
+// //         return false;
+// //     }
+// // }
+
+// // class Graph {
+// //     constructor(nodes) {
+// //         this.nodes = [...nodes];
+// //     }
+// //     // Add new node to graph after initialization 
+// //     addToGraph(node) {
+// //         this.nodes.push(node);
+// //     }
+
+// // }
+
+// // Create nodes
+// // const nodeA = new Node('A');
+// // const nodeB = new Node('B');
+// // const nodeC = new Node('C');
+// // const nodeD = new Node('D');
+// // const nodeE = new Node('E');
+// // Create graph
+// // const graph = new Graph([nodeA, nodeB, nodeC, nodeD, nodeE]);
+// // Add connections
+// // nodeA.connect(nodeB);
+// // nodeA.connect(nodeD);
+// // nodeB.connect(nodeC);
+// // nodeC.connect(nodeD);
+// // nodeC.connect(nodeE);
+// // nodeD.connect(nodeE);
+
+// // console.log(nodeA.getAdjacentNodes()); // Expected: [B, D]
+// // console.log(nodeC.getAdjacentNodes()); // Expected: [B, D, E]
+// // console.log(nodeA.isConnected(nodeB)); // Expected: true
+// // console.log(nodeA.isConnected(nodeC)); // Expected: false
+
+// // === Graph breadth first traversal, depth first traversal and shortest path ===
 
 // class Node {
 //     constructor(value) {
@@ -223,6 +223,39 @@ function isConnected2(node1, node2) {
 //     }
 // }
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.edgesList = [];
+    }
+    connect(node) {
+        this.edgesList.push(node);
+        node.edgesList.push(this);
+    }
+}
+
+class Graph {
+    constructor(nodes) {
+        this.nodes = [...nodes];
+    }
+    depthFirstTraversal(start, end, visited = new Set()) {
+        if (start.value === end.value) {
+            console.log('Found it!');
+            return;
+        }
+        console.log('Visiting node: ', start.value);
+        visited.add(start);
+        for (let adjacency of start.edgesList) {
+            if (!visited.has(adjacency)) {
+                this.depthFirstTraversal(adjacency, end, visited);
+            }
+        }
+    }
+    // breadthFirstTraversal(start, end) {}
+    // shortestPath(start, end) {}
+    // reconstructPath(results, end) {}
+}
+
 const DFW = new Node('DFW');
 const JFK = new Node('JFK');
 const LAX = new Node('LAX');
@@ -250,7 +283,7 @@ MIA.connect(PBI);
 MCO.connect(PBI);
 
 // graph.breadthFirstTraversal(DFW, PBI);
-// graph.depthFirstTraversal(DFW, HKG);
+graph.depthFirstTraversal(DFW, HKG);
 
 // Practice Question
 // Given 2 airports find the smallest distance between the 2. (ex: DFW and PBI > DFW-JFK-MIA-PBI)
