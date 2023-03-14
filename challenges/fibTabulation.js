a6abe9059e2e18e071d44973864dc5a0791ca79a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-13 19:50:52 -0700

    fib practice using tabulation and memoization

--- challenges/fibTabulation.js
+++ challenges/fibTabulation.js
@@ -1,15 +1,27 @@
 // == Fibonacci Sequence using tabulation ==
 // Find the nth value of a fibonacci sequence, the first num will be 0, the second will be 1. Use tabulation.
 
-function fibTabulation(n){
-    const table = Array(n + 1).fill(0);
+// function fibTabulation(n){
+//     const table = Array(n + 1).fill(0);
+//     table[1] = 1;
+//     for (let i = 0; i <= n; i++) {
+//         table[i + 1] += table[i];
+//         table[i + 2] += table[i];
+//     }
+//     return table[n];
+// }
+
+function fibTabulation(v) {
+    if (v <= 2) return 1;
+    const table = new Array(v + 1).fill(0);
     table[1] = 1;
-    for (let i = 0; i <= n; i++) {
+    for (let i = 0; i < v; i++) {
         table[i + 1] += table[i];
         table[i + 2] += table[i];
     }
-    return table[n];
+    return table[v];
 }
 
 // Test
-console.log(fibTabulation(6));
+console.log(fibTabulation(13));
+console.log(fibTabulation(170));
