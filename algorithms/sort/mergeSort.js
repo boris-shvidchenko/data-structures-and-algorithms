c45941d4f5b124e1727b61e48b090ced83f2c0c9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-15 19:44:26 -0700

    quick sort practice

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -5,30 +5,30 @@
 const testArray = [9,4,8,3,1,10,2,5,7,6];
 
 // Splits arrays in two
-function mergeSort(list) {
-    // Base Case
-    if (list.length <= 1) return list;
-    const midPoint = Math.floor(list.length/2);
-    const left = list.slice(0,midPoint);
-    const right = list.slice(midPoint);
-    return merge(mergeSort(left), mergeSort(right));
-}
+// function mergeSort(list) {
+//     // Base Case
+//     if (list.length <= 1) return list;
+//     const midPoint = Math.floor(list.length/2);
+//     const left = list.slice(0,midPoint);
+//     const right = list.slice(midPoint);
+//     return merge(mergeSort(left), mergeSort(right));
+// }
 
-// Merges arrays together
-function merge(left, right) {
-    let resultsArray = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            resultsArray.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            resultsArray.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-}
+// // Merges arrays together
+// function merge(left, right) {
+//     let resultsArray = [];
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+//     while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             resultsArray.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             resultsArray.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+//     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+// }
 
 console.log(mergeSort(testArray)); 
