706432501fe1b32517ed88c96e0774cfc9a7c911
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-20 20:23:15 -0700

    Quick Sort practice

--- algorithms/sort/quickSort.js
+++ algorithms/sort/quickSort.js
@@ -2,40 +2,22 @@
 // Sorts an array by identifying the first item as the 'pivot'. All items in the array, not including the pivot, are groups into two subarrays based on whether they are < or > the pivot. This is repeated until only multiple single item arrays (just like in a merge sort) are left. These items are sorted and merged to create a sorted array.
 // Runtime: O(n^2) / Quadratic
 
-const testArray = [5, 4, 6, 1, 2, 3, 10, 9, 7, 8, 3 , 4, 6, 1, 2, 3, 10, 9, 7, 8];
-
-// function quickSort(list) {
-//     //Base case
-//     if (list.length <=1) return list;
-//     const pivot = list[0];
-//     let left = [];
-//     let right = [];
-//     for (let i = 1; i<list.length; i++) {
-//         if (list[i] < pivot) {
-//             left.push(list[i])
-//         } else {
-//             right.push(list[i])
-//         }
-//     }
-//     return [...quickSort(left), pivot, ...quickSort(right)];
-// }
+const testArray = [5, 4, 6, 1, 2, 3, 10, 9, 7, 8];
 
 function quickSort(list) {
-    if (list.length <= 1) return list;
+    //Base case
+    if (list.length <=1) return list;
     const pivot = list[0];
-    const left = [];
-    const right = [];
+    let left = [];
+    let right = [];
     for (let i = 1; i<list.length; i++) {
-        if (list[i] <= pivot) {
-            left.push(list[i]);
+        if (list[i] < pivot) {
+            left.push(list[i])
         } else {
-            right.push(list[i]);
+            right.push(list[i])
         }
     }
-    return [...quickSort(left), pivot, ...quickSort(right)];   
-    
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
-
-
 
 console.log(quickSort(testArray));
