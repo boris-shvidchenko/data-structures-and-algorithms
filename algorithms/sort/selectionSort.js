77c50d25288368e412fec8431d28ee80e028e944
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-28 08:16:44 -0700

    Practice with quick sort alg and binary tree traversal

--- algorithms/sort/selectionSort.js
+++ algorithms/sort/selectionSort.js
@@ -2,9 +2,9 @@
 // Sorts an array by identifying the smallest number within the array and storing the value in a variable. Once a full iteration has been performed, the smallest value is pushed into a results array and also removed from the original list. This process continues until the original array is empty and the results array is sorted in the correct order.
 // Runtime: O(n^2) / Quadratic
 
-const testArray = [9,10,4,2,8,1,3,5,7,6,9,10,4,2,8,1,3,5,7,6];
+// const testArray = [9,10,4,2,8,1,3,5,7,6];
 
-// Stores the sorted array
+// // Stores the sorted array
 // let newArray = [];
 
 // function selectionSort(list) {
@@ -23,41 +23,49 @@ const testArray = [9,10,4,2,8,1,3,5,7,6,9,10,4,2,8,1,3,5,7,6];
 //     return selectionSort(list);
 // }
 
-// Method 2: Using reduce
-// function selectionSort(list, results=[]) {
-//     if (list.length === 0) return results;
-//     const min = list.reduce((a,b) => a < b ? a : b);
-//     results.push(min);
-//     const minIndex = list.indexOf(min);
-//     list.splice(minIndex, 1);
-//     return selectionSort(list, results);
-// }
+// // Method 2: Using reduce
+// // function selectionSort(list, results=[]) {
+// //     if (list.length === 0) return results;
+// //     const min = list.reduce((a,b) => a < b ? a : b);
+// //     results.push(min);
+// //     const minIndex = list.indexOf(min);
+// //     list.splice(minIndex, 1);
+// //     return selectionSort(list, results);
+// // }
 
 // console.log(selectionSort(testArray));
 
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length / 2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
-
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let results = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            results.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            results.push(right[rightIndex]);
-            rightIndex++;
-        }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
     }
-    return results.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
 
-console.log(mergeSort(testArray));
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
 
+
+function bft(root, t) {
+    if (root === null) return false;
+    let queue = [root];
+    while (queue.length > 0) {
+        let current = queue.shift();
+        if (current.val === t) return true;
+        if (current.left !== null) queue.push(current.left);
+        if (current.right !== null) queue.push(current.right);
+    }
+    return false;
+}
+console.log(bft(a, 'a'));
+console.log(bft(a, 'z'));
