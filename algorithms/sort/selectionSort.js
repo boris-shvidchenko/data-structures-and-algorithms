394867797d86c50c863a38669cfb335491aae401
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-28 07:57:13 -0700

    selection sort practice

--- algorithms/sort/selectionSort.js
+++ algorithms/sort/selectionSort.js
@@ -2,26 +2,26 @@
 // Sorts an array by identifying the smallest number within the array and storing the value in a variable. Once a full iteration has been performed, the smallest value is pushed into a results array and also removed from the original list. This process continues until the original array is empty and the results array is sorted in the correct order.
 // Runtime: O(n^2) / Quadratic
 
-const testArray = [9,10,4,2,8,1,3,5,7,6];
+const testArray = [9,10,4,2,8,1,3,5,7,6,4,2,8,1,3,5,7,6];
 
 // Stores the sorted array
-let newArray = [];
+// let newArray = [];
 
-function selectionSort(list) {
-    let min = list[0];
-    // Base case
-    if (list.length === 0) return newArray;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] < min) min = list[i];
-    }
-    newArray.push(min);
-    const minIndex = list.indexOf(min);
-    list.splice(minIndex,1);
-    // Test log
-    console.log('Input List: ' + list);
-    console.log('Output List: ' + newArray);
-    return selectionSort(list);
-}
+// function selectionSort(list) {
+//     let min = list[0];
+//     // Base case
+//     if (list.length === 0) return newArray;
+//     for (let i = 0; i < list.length; i++) {
+//         if (list[i] < min) min = list[i];
+//     }
+//     newArray.push(min);
+//     const minIndex = list.indexOf(min);
+//     list.splice(minIndex,1);
+//     // Test log
+//     console.log('Input List: ' + list);
+//     console.log('Output List: ' + newArray);
+//     return selectionSort(list);
+// }
 
 // Method 2: Using reduce
 // function selectionSort(list, results=[]) {
@@ -32,6 +32,14 @@ function selectionSort(list) {
 //     list.splice(minIndex, 1);
 //     return selectionSort(list, results);
 // }
+
+function selectionSort(list, results=[]) {
+    if (list.length <= 1) return results;;
+    let min = list.reduce((a,b) => a < b ? a : b);
+    results.push(min);
+    list.splice(list.indexOf(min), 1);
+    return selectionSort(list, results);
+}
 
 console.log(selectionSort(testArray));
 
