064a4fc78f0a2d8d8eda45ef3b3aba0ad4d3d051
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-03-29 19:31:34 -0700

    binary search practice

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -3,25 +3,42 @@
 // Runtime: O(log(n)) / Binary
 
 const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const testArray = [9,4,8,3,1,10,2,5,7,6];
 
 // Returns the index of the target value within the provided array
-function binarySearch(array, targetValue) {
-    const midPoint = Math.floor(array.length/2);
-    // First statement is the base case
-    if (array.length === 1) {
-        if (targetValue === array[midPoint]) {
-            return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
-        } else {
-            return `${targetValue} is not in [${testArray}]`;
-        }
-    } else if (targetValue < array[midPoint]) {
-        const arrayLeft = array.slice(0, midPoint);
-        return binarySearch(arrayLeft, targetValue);
+// function binarySearch(array, targetValue) {
+//     const midPoint = Math.floor(array.length/2);
+//     // First statement is the base case
+//     if (array.length === 1) {
+//         if (targetValue === array[midPoint]) {
+//             return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
+//         } else {
+//             return `${targetValue} is not in [${testArray}]`;
+//         }
+//     } else if (targetValue < array[midPoint]) {
+//         const arrayLeft = array.slice(0, midPoint);
+//         return binarySearch(arrayLeft, targetValue);
+//     } else {
+//         const arrayRight = array.slice(midPoint);
+//         return binarySearch(arrayRight, targetValue);
+//     }
+// }
+
+const binarySearch = (list, target) => {
+    if (list.length <= 1) {
+        if (list[0] === target) return true;
+        return false;
     } else {
-        const arrayRight = array.slice(midPoint);
-        return binarySearch(arrayRight, targetValue);
+        const mid = Math.floor(list.length/2);
+        let half;
+        if (list[mid] === target) return true;
+        if (target < list[mid]) half = list.slice(0, mid);
+        if (target > list[mid]) half = list.slice(mid);
+        return binarySearch(half, target);
     }
+
 }
 
 // Test log 
 console.log(binarySearch(testArray,2))
+console.log(binarySearch(testArray,20))
