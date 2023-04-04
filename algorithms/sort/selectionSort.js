d75fb2bf676e0663c26fe1a6d79ed8a4c23f05d1
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-03 20:01:00 -0700

    binary tree traversal and search practice

--- algorithms/sort/selectionSort.js
+++ algorithms/sort/selectionSort.js
@@ -2,72 +2,36 @@
 // Sorts an array by identifying the smallest number within the array and storing the value in a variable. Once a full iteration has been performed, the smallest value is pushed into a results array and also removed from the original list. This process continues until the original array is empty and the results array is sorted in the correct order.
 // Runtime: O(n^2) / Quadratic
 
-// const testArray = [9,10,4,2,8,1,3,5,7,6];
+const testArray = [9,10,4,2,8,1,3,5,7,6];
 
-// // Stores the sorted array
-// let newArray = [];
+// Stores the sorted array
+let newArray = [];
 
-// function selectionSort(list) {
-//     let min = list[0];
-//     // Base case
-//     if (list.length === 0) return newArray;
-//     for (let i = 0; i < list.length; i++) {
-//         if (list[i] < min) min = list[i];
-//     }
-//     newArray.push(min);
+function selectionSort(list) {
+    let min = list[0];
+    // Base case
+    if (list.length === 0) return newArray;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] < min) min = list[i];
+    }
+    newArray.push(min);
+    const minIndex = list.indexOf(min);
+    list.splice(minIndex,1);
+    // Test log
+    console.log('Input List: ' + list);
+    console.log('Output List: ' + newArray);
+    return selectionSort(list);
+}
+
+// Method 2: Using reduce
+// function selectionSort(list, results=[]) {
+//     if (list.length === 0) return results;
+//     const min = list.reduce((a,b) => a < b ? a : b);
+//     results.push(min);
 //     const minIndex = list.indexOf(min);
-//     list.splice(minIndex,1);
-//     // Test log
-//     console.log('Input List: ' + list);
-//     console.log('Output List: ' + newArray);
-//     return selectionSort(list);
+//     list.splice(minIndex, 1);
+//     return selectionSort(list, results);
 // }
 
-// // Method 2: Using reduce
-// // function selectionSort(list, results=[]) {
-// //     if (list.length === 0) return results;
-// //     const min = list.reduce((a,b) => a < b ? a : b);
-// //     results.push(min);
-// //     const minIndex = list.indexOf(min);
-// //     list.splice(minIndex, 1);
-// //     return selectionSort(list, results);
-// // }
-
-// console.log(selectionSort(testArray));
-
-
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
-const e = new Node('E');
-const f = new Node('F');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function bft(root, target) {
-    if (root === null) return false;;
-    const queue = [root];
-    while (queue.length > 0) {
-        const current = queue.shift();
-        if (current.val === target) return true;
-        if (current.left !== null) queue.push(current.left);
-        if (current.right !== null) queue.push(current.right);
-    }
-    return false;;
-}
-console.log(bft(a, 'A'));
-console.log(bft(a, 'K'));
+console.log(selectionSort(testArray));
 
