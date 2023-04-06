13262c52c35f0a6700b9ded79f05ea97d3e66944
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-06 07:57:30 -0700

    selection sort practice

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -2,8 +2,7 @@
 // Sorts an array by splitting the array into subarrays of size 1 (one element/item per array). Then, adjacent arrays are sorted and merged. This continues until a single array exists with sorted values.
 // Runtime: O(n(log(n))) / Quasilinear
 
-// const testArray = [9,4,8,3,1,10,2,5,7,6];
-const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testArray = [9,4,8,3,1,10,2,5,7,6,9,4,8,3,1,10,2,5,7,6,-100];
 
 // // Splits arrays in two
 // function mergeSort(list) {
@@ -32,23 +31,20 @@ const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 // }
 
-function binarySearch(list, target) {
-    if (list.length === 1) {
-        if (list[0] === target) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        let half;
-        if (list[mid] === target) return true;
-        if (target < list[mid]) {
-            half = list.slice(0, mid);
-        } else {
-            half = list.slice(mid);
-        }
-        return binarySearch(half, target);
-        
-    }
+// sel sort
+// quick sort
+// binary tree
+// hash
+// graph
+// fib (memo/tab)
+
+function selectionSort(list, results=[]) {
+    if (list.length === 0) return results;
+    const min = list.reduce((a,b) => a < b ? a : b);
+    const indexOfMin = list.indexOf(min);
+    results.push(min);
+    list.splice(indexOfMin, 1);
+    return selectionSort(list, results);
 }
 
-console.log(binarySearch(testArray, 10)); 
-console.log(binarySearch(testArray, 90)); 
+console.log(selectionSort(testArray)); 
