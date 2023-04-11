24b36210075c53e8b514ef4ca2eda87172329bf5
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-10 20:00:26 -0700

    binary tree traversal practice

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -2,66 +2,34 @@
 // Sorts an array by splitting the array into subarrays of size 1 (one element/item per array). Then, adjacent arrays are sorted and merged. This continues until a single array exists with sorted values.
 // Runtime: O(n(log(n))) / Quasilinear
 
-// const testArray = [9,4,8,3,1,10,2];
+const testArray = [9,4,8,3,1,10,2,5,7,6];
 
-// // // Splits arrays in two
-// function mergeSort(list) {
-//     // Base Case
-//     if (list.length <= 1) return list;
-//     const midPoint = Math.floor(list.length/2);
-//     const left = list.slice(0,midPoint);
-//     const right = list.slice(midPoint);
-//     return merge(mergeSort(left), mergeSort(right));
-// }
-
-// // // Merges arrays together
-// function merge(left, right) {
-//     let resultsArray = [];
-//     let leftIndex = 0;
-//     let rightIndex = 0;
-//     while (leftIndex < left.length && rightIndex < right.length) {
-//         if (left[leftIndex] < right[rightIndex]) {
-//             resultsArray.push(left[leftIndex]);
-//             leftIndex++;
-//         } else {
-//             resultsArray.push(right[rightIndex]);
-//             rightIndex++;
-//         }
-//     }
-//     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-// }
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+// // Splits arrays in two
+function mergeSort(list) {
+    // Base Case
+    if (list.length <= 1) return list;
+    const midPoint = Math.floor(list.length/2);
+    const left = list.slice(0,midPoint);
+    const right = list.slice(midPoint);
+    return merge(mergeSort(left), mergeSort(right));
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-
-function bft(root, t) {
-    if (root === null) return false;
-    const queue = [root];
-    while (queue.length > 0) {
-        const current = queue.shift();
-        if (current.val === t) return true;
-        if (current.left !== null) queue.push(current.left);
-        if (current.right !== null) queue.push(current.right);
+// // Merges arrays together
+function merge(left, right) {
+    let resultsArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            resultsArray.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            resultsArray.push(right[rightIndex]);
+            rightIndex++;
+        }
     }
-    return false;
+    return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
-console.log(bft(a, 'c'));
-console.log(bft(a, 'o'));
+
+
+console.log(mergeSort(testArray)); 
