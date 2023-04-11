31c9c5a2cbbd8e273bd57d2b81a456969955554a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-10 19:20:43 -0700

    tabulation and memoization practice with fib

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -2,34 +2,50 @@
 // Sorts an array by splitting the array into subarrays of size 1 (one element/item per array). Then, adjacent arrays are sorted and merged. This continues until a single array exists with sorted values.
 // Runtime: O(n(log(n))) / Quasilinear
 
-const testArray = [9,4,8,3,1,10,2,5,7,6];
+// const testArray = [9,4,8,3,1,10,2,5,7,6];
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// // Splits arrays in two
-function mergeSort(list) {
-    // Base Case
-    if (list.length <= 1) return list;
-    const midPoint = Math.floor(list.length/2);
-    const left = list.slice(0,midPoint);
-    const right = list.slice(midPoint);
-    return merge(mergeSort(left), mergeSort(right));
-}
+// // // Splits arrays in two
+// function mergeSort(list) {
+//     // Base Case
+//     if (list.length <= 1) return list;
+//     const midPoint = Math.floor(list.length/2);
+//     const left = list.slice(0,midPoint);
+//     const right = list.slice(midPoint);
+//     return merge(mergeSort(left), mergeSort(right));
+// }
 
-// // Merges arrays together
-function merge(left, right) {
-    let resultsArray = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            resultsArray.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            resultsArray.push(right[rightIndex]);
-            rightIndex++;
-        }
+// // // Merges arrays together
+// function merge(left, right) {
+//     let resultsArray = [];
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+//     while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             resultsArray.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             resultsArray.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+//     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+// }
+
+
+// console.log(mergeSort(testArray)); 
+
+function fib(v) {
+    if (v <= 2) return 1;
+    const table = Array(v + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i<v; i++) {
+        table[i+1] += table[i];
+        table[i+2] += table[i];
     }
-    return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    return table[v];
 }
 
 
-console.log(mergeSort(testArray)); 
+console.log(fib(20));
+console.log(fib(200));
