ad831ec33bb11da823186c471891a6b8d582d0fa
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-10 19:27:21 -0700

    merge sort practice

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -2,8 +2,7 @@
 // Sorts an array by splitting the array into subarrays of size 1 (one element/item per array). Then, adjacent arrays are sorted and merged. This continues until a single array exists with sorted values.
 // Runtime: O(n(log(n))) / Quasilinear
 
-// const testArray = [9,4,8,3,1,10,2,5,7,6];
-const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testArray = [9,4,8,3,1,10,2,5,7,6,9,4,8,3,1,10,2,5,7,6];
 
 // // // Splits arrays in two
 // function mergeSort(list) {
@@ -32,25 +31,28 @@ const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 // }
 
+function mergeSort(list) {
+    if (list.length === 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
+}
 
-// console.log(mergeSort(testArray)); 
-
-const binarySearch = (list, target) => {
-    if (list.length === 1) {
-        if (list[0] === target) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (list[mid] === target) return true;
-        if (list[mid] < target) {
-            const right = list.slice(mid);
-            return binarySearch(right, target);
+function sort(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let results = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            results.push(left[leftIndex]);
+            leftIndex++;
         } else {
-            const left = list.slice(0, mid);
-            return binarySearch(left, target);
+            results.push(right[rightIndex]);
+            rightIndex++;
         }
     }
-};
+    return results.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
 
-console.log(binarySearch(testArray, 7));
-console.log(binarySearch(testArray, 100));
+console.log(mergeSort(testArray)); 
