cc321cc4d4ef43b321be43915ac8344d931eacc9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-13 08:10:48 -0700

    quick sort practice

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -2,83 +2,108 @@
 // key -> /hash function/ -> index => value
 // 3 operations must be supported 1) store a key/value pair 2) Retrieve a key/value pair 3) Delete a key/value pair.
 
-class HashTable {
-    constructor(size) {
-        this.table = new Array(size);
-        this.size = size;
-    }
+// class HashTable {
+//     constructor(size) {
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
 
-    // Hash Function - creates an index
-    hash(key) {
-        let total = 0;
-        for (let i = 0; i < key.length; i++) {
-            // charCodeAt returns an integer between 0-65535, representing the UTF-8 code for the specified character, this will be the unique index
-            total += key.charCodeAt(i); 
-        }
-        return total % this.size;
-    }
+//     // Hash Function - creates an index
+//     hash(key) {
+//         let total = 0;
+//         for (let i = 0; i < key.length; i++) {
+//             // charCodeAt returns an integer between 0-65535, representing the UTF-8 code for the specified character, this will be the unique index
+//             total += key.charCodeAt(i); 
+//         }
+//         return total % this.size;
+//     }
 
-    // Set
-    set(key,value) {
-        const index = this.hash(key);
-        const bucket = this.table[index];
-        // collison check
-        if (!bucket) { 
-            this.table[index] = [[key, value]];
+//     // Set
+//     set(key,value) {
+//         const index = this.hash(key);
+//         const bucket = this.table[index];
+//         // collison check
+//         if (!bucket) { 
+//             this.table[index] = [[key, value]];
+//         } else {
+//             const sameKeyItem = bucket.find(item => item[0] === key);
+//             if (sameKeyItem) {
+//                 sameKeyItem[1] = value;
+//             } else {
+//                 bucket.push([key, value]);
+//             }
+//         }
+//     }
+
+//     // Get
+//     get(key) {
+//         const index = this.hash(key);
+//         const bucket = this.table[index];
+//         // collison check
+//         if (bucket) {
+//             const sameKeyItem = bucket.find(item => item[0] === key);
+//             if (sameKeyItem) {
+//                 return sameKeyItem[1];
+//             } 
+//         }
+//         return undefined;
+//     }
+
+//     // Remove
+//     remove(key) {
+//         const index = this.hash(key);
+//         const bucket = this.table[index];
+//         // collison check
+//         if (bucket) {
+//             const sameKeyItem = bucket.find(item => item[0] === key);
+//             if (sameKeyItem) {
+//                 bucket.splice(bucket.indexOf(key), 1);
+//             }
+//         }
+//     }
+
+//     // Prints the content of the hash table
+//     display() {
+//         this.table.forEach(value => {
+//             if (value !== undefined) {
+//                 const index = this.table.indexOf(value);
+//                 console.log(`${index} > [${value}]`)
+//             }
+//         })
+//     }
+// }
+
+// const table = new HashTable(50);
+// table.set('name', 'Ryan');
+// table.set('age', 25);
+// table.set('color', 'red');
+// table.display();
+// console.log(table.get('name'));
+// table.remove('color');
+// table.set('mane', 'Clark');
+// table.display();
+
+// quick sort
+// linked list
+// graph
+// merge sort
+// tree
+// hash
+
+const list = [9,8,7,4,6,5,1,3,4,2,6,6,66,23,-10,0,23,4.7];
+
+function quickSort(list) {
+    if (list.length === 0) return list;
+    const pivot = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < pivot) {
+            left.push(list[i])
         } else {
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if (sameKeyItem) {
-                sameKeyItem[1] = value;
-            } else {
-                bucket.push([key, value]);
-            }
+            right.push(list[i]);
         }
     }
-
-    // Get
-    get(key) {
-        const index = this.hash(key);
-        const bucket = this.table[index];
-        // collison check
-        if (bucket) {
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if (sameKeyItem) {
-                return sameKeyItem[1];
-            } 
-        }
-        return undefined;
-    }
-
-    // Remove
-    remove(key) {
-        const index = this.hash(key);
-        const bucket = this.table[index];
-        // collison check
-        if (bucket) {
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if (sameKeyItem) {
-                bucket.splice(bucket.indexOf(key), 1);
-            }
-        }
-    }
-
-    // Prints the content of the hash table
-    display() {
-        this.table.forEach(value => {
-            if (value !== undefined) {
-                const index = this.table.indexOf(value);
-                console.log(`${index} > [${value}]`)
-            }
-        })
-    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
-
-const table = new HashTable(50);
-table.set('name', 'Ryan');
-table.set('age', 25);
-table.set('color', 'red');
-table.display();
-console.log(table.get('name'));
-table.remove('color');
-table.set('mane', 'Clark');
-table.display();
+console.log(quickSort(list));
