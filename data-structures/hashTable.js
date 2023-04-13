cbe3fea5575ee826d1b57326ecefbd7bc3a21ec9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-13 09:12:05 -0700

    hash table review

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -2,130 +2,83 @@
 // key -> /hash function/ -> index => value
 // 3 operations must be supported 1) store a key/value pair 2) Retrieve a key/value pair 3) Delete a key/value pair.
 
-// class HashTable {
-//     constructor(size) {
-//         this.table = new Array(size);
-//         this.size = size;
-//     }
-
-//     // Hash Function - creates an index
-//     hash(key) {
-//         let total = 0;
-//         for (let i = 0; i < key.length; i++) {
-//             // charCodeAt returns an integer between 0-65535, representing the UTF-8 code for the specified character, this will be the unique index
-//             total += key.charCodeAt(i); 
-//         }
-//         return total % this.size;
-//     }
-
-//     // Set
-//     set(key,value) {
-//         const index = this.hash(key);
-//         const bucket = this.table[index];
-//         // collison check
-//         if (!bucket) { 
-//             this.table[index] = [[key, value]];
-//         } else {
-//             const sameKeyItem = bucket.find(item => item[0] === key);
-//             if (sameKeyItem) {
-//                 sameKeyItem[1] = value;
-//             } else {
-//                 bucket.push([key, value]);
-//             }
-//         }
-//     }
-
-//     // Get
-//     get(key) {
-//         const index = this.hash(key);
-//         const bucket = this.table[index];
-//         // collison check
-//         if (bucket) {
-//             const sameKeyItem = bucket.find(item => item[0] === key);
-//             if (sameKeyItem) {
-//                 return sameKeyItem[1];
-//             } 
-//         }
-//         return undefined;
-//     }
-
-//     // Remove
-//     remove(key) {
-//         const index = this.hash(key);
-//         const bucket = this.table[index];
-//         // collison check
-//         if (bucket) {
-//             const sameKeyItem = bucket.find(item => item[0] === key);
-//             if (sameKeyItem) {
-//                 bucket.splice(bucket.indexOf(key), 1);
-//             }
-//         }
-//     }
-
-//     // Prints the content of the hash table
-//     display() {
-//         this.table.forEach(value => {
-//             if (value !== undefined) {
-//                 const index = this.table.indexOf(value);
-//                 console.log(`${index} > [${value}]`)
-//             }
-//         })
-//     }
-// }
-
-// const table = new HashTable(50);
-// table.set('name', 'Ryan');
-// table.set('age', 25);
-// table.set('color', 'red');
-// table.display();
-// console.log(table.get('name'));
-// table.remove('color');
-// table.set('mane', 'Clark');
-// table.display();
-
-class Hash {
+class HashTable {
     constructor(size) {
-        this.size = size;
         this.table = new Array(size);
+        this.size = size;
     }
-    hash(k) {
+
+    // Hash Function - creates an index
+    hash(key) {
         let total = 0;
-        for (let i = 0; i < k.length; i++) {
-            total += k.charCodeAt(i);
+        for (let i = 0; i < key.length; i++) {
+            // charCodeAt returns an integer between 0-65535, representing the UTF-8 code for the specified character, this will be the unique index
+            total += key.charCodeAt(i); 
         }
         return total % this.size;
     }
-    set(k, v) {
-        const index = this.hash(k);
+
+    // Set
+    set(key,value) {
+        const index = this.hash(key);
         const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
+        // collison check
+        if (!bucket) { 
+            this.table[index] = [[key, value]];
         } else {
-            const col = bucket.find(i => i[0] === k);
-            if (col) {
-                col[1] = v;
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if (sameKeyItem) {
+                sameKeyItem[1] = value;
             } else {
-                bucket.push([k,v]);
+                bucket.push([key, value]);
             }
         }
+    }
 
-    }
-    remove(k) {
-        const index = this.hash(k);
+    // Get
+    get(key) {
+        const index = this.hash(key);
         const bucket = this.table[index];
-        const col = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(col), 1);
+        // collison check
+        if (bucket) {
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if (sameKeyItem) {
+                return sameKeyItem[1];
+            } 
+        }
+        return undefined;
     }
+
+    // Remove
+    remove(key) {
+        const index = this.hash(key);
+        const bucket = this.table[index];
+        // collison check
+        if (bucket) {
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if (sameKeyItem) {
+                bucket.splice(bucket.indexOf(key), 1);
+            }
+        }
+    }
+
+    // Prints the content of the hash table
     display() {
-        this.table.forEach(value => console.log(value));
+        this.table.forEach(value => {
+            if (value !== undefined) {
+                const index = this.table.indexOf(value);
+                console.log(`${index} > [${value}]`)
+            }
+        })
     }
 }
 
-const table = new Hash(50);
+const table = new HashTable(50);
 table.set('name', 'Ryan');
 table.set('age', 25);
 table.set('color', 'red');
 table.display();
+console.log(table.get('name'));
 table.remove('color');
 table.set('mane', 'Clark');
 table.display();
