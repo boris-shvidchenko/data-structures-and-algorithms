0698f0a8b84a4a8fd73635624962e88dc2e40cb8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-17 20:12:42 -0700

    quick sort practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -16,6 +16,25 @@
 
 // merge sort
 // quick sort
-// b search
 
+
+
+const a = [6,3,87,1,0.4,-231,23,7,0.0129,1,3,3,3,3,3,21];
+
+function quickSort(list) {
+    if (list.length <= 1) return list;
+    const pivot = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i<list.length; i++) {
+        if (list[i] < pivot) {
+            left.push(list[i]);
+        } else {
+            right.push(list[i]);
+        } 
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort(a));
 
