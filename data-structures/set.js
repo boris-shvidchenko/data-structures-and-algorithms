0b29f91d64242f63ca7c1e59a71c626add74e0d8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-17 20:13:43 -0700

    quick sort practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,40 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
-
-// merge sort
-// quick sort
-
-
-
-const a = [6,3,87,1,0.4,-231,23,7,0.0129,1,3,3,3,3,3,21];
-
-function quickSort(list) {
-    if (list.length <= 1) return list;
-    const pivot = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i<list.length; i++) {
-        if (list[i] < pivot) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        } 
-    }
-    return [...quickSort(left), pivot, ...quickSort(right)];
-}
-
-console.log(quickSort(a));
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
