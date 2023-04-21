1d739c7e388510515549a24879ad4989cb7f185a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-20 19:34:41 -0700

    hash map practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,56 +14,9 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-//hash
 //tree
 //sel sort
 //link
 
 
-class Hash {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let total = 0;
-        for (let i = 0; i < k.length; i++) {
-            total += k.charCodeAt(i);
-        }
-        return total % this.size;
-    }
-    add(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
-        }
-
-    }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
-    }
-    print() {
-        this.table.forEach(i => console.log(i));
-    }
-}
-
-const newHash = new Hash(50);
-newHash.add('name', 'ryan');
-newHash.add('age', '30');
-newHash.add('color', 'red');
-newHash.print();
-newHash.remove('color');
-newHash.add('age', '50');
-newHash.print();
 
