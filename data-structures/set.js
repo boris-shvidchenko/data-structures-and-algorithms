a0dfc544c815cf768628f504d792ab88eece8881
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-24 14:03:43 -0700

    tabulation and memoization practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,7 +14,6 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-// graph
 // fib memo/tab
 // linke list
 // hash
@@ -22,75 +21,16 @@
 
 // const list = [2,4,23,-0.9,-300,222,32,1.4];
 
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.edges = [];
+function fib(v) {
+    if (v <= 2) return 1;
+    const table = new Array(v+1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i < v; i++) {
+        table[i+1] += table[i];
+        table[i+2] += table[i];
     }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
-    }
-}
+    return table[v];
+} 
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
-    }
-    sp(start, end) {
-        const visited = {};
-        const q = [start];
-        visited[start.val] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) {
-                return this.rp(visited, end);
-            }
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
-            }
-        }
-    }
-    rp(visited, end) {
-        let current = end;
-        const path = [];
-        while (current !== null) {
-            path.push(current.val);
-            current = visited[current.val];
-        }
-        return path.reverse();
-    }
-}
-
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-// graph.bft(DFW, PBI);
-// graph.dft(DFW, HKG);
-console.log(graph.sp(DFW, PBI));
+console.log(fib(20))
+console.log(fib(120))
