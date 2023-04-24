e4f000744fceee7f40af2cedee9961a1795bb87a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-04-24 14:40:02 -0700

    binary search alg practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,42 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
-let array = [1,2,3,4,5,6,7,8,9,10];
-
-
-const bSearch = (list, t) => {
-    if (list.length === 1) {
-        if (list[0] === t) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length / 2);
-        if (list[mid] === t) {
-            return true;
-        } else {
-            if (t < list[mid]) {
-                const left = list.slice(0, mid);
-                return bSearch(left, t);
-            } else {
-                const right = list.slice(mid);
-                return bSearch(right, t);
-            }
-        }
-    }
-}
-
-
-console.log(bSearch(array, 7));
-console.log(bSearch(array, 70));
