b4a4ba931931bb155f0410d22d6454834e966dd1
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-03 19:33:32 -0700

    linked list practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -15,34 +15,93 @@
 // for (let char of nums) console.log(char); 
 
 // l list
-// merge sort
 // stack
 
-const array = [9,3,-0.8, -300, 4023, 1.3, 4, 5, 5];
-
-const sort = (left, right) => {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    const res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
 
-const mergeSort = (list) => {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+    }
+    append(node) {
+        const newNode = new Node(node);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(node, index) {
+        const newNode = new Node(node);
+        let current = this.head;
+        let currentIndex = 0;
+        let prev;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = current;
+        } else {
+            while (currentIndex < index) {
+                prev = current;
+                current = current.next;
+                currentIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = current;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let current = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = current.next;
+        } else {
+            while (current.val !== node) {
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
+        }
+        this.length--;
+    }
+    removeAll() {
+        this.length = 0;
+        this.head = null;
+    }
+    print() {
+        let str = '';
+        let current = this.head;
+        while (current !== null) {
+            str += current.val + '>';
+            current = current.next;
+        }
+        console.log(str);
+    }
 }
 
+const list = new LinkedList();
+list.append('A');
+list.append('B');
+list.append('C');
+list.print();
+list.appendAt('Z', 0);
+list.print();
+list.appendAt('X', 2);
+list.print();
+list.remove('B');
+list.remove('Z');
+list.print();
+list.removeAll();
+list.print();
 
-console.log(mergeSort(array));
