479830e5a604183167ab0e726c6c14cbc2137baa
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-05 12:54:40 -0700

    graph practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,31 +14,80 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-// hash
-// fib
+
 // graph
-// b sear
 // leet prob
 
-const arr = [1,2,3,4,5,6,7,8,9,10];
-
-const bSearch = (list, tar) => {
-    console.log(list);
-    if (list.length === 1) {
-        if (list[0] === tar) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length / 2);
-        if (list[mid] === tar) return true;
-        if (tar < list[mid]) {
-            return bSearch(list.slice(0, mid), tar);
-        } else {
-            return bSearch(list.slice(mid), tar);
-        }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.edges = [];
+    }
+    connect(node) {
+        this.edges.push(node);
+        node.edges.push(this);
     }
 }
 
-console.log(bSearch(arr, 5));
-console.log(bSearch(arr, 2));
-console.log(bSearch(arr, -10));
-console.log(bSearch(arr, 89));
+class Graph {
+    constructor(nodes) {
+        this.nodes = [...nodes];
+    }
+
+    sp(start, end) {
+        const visited = {};
+        const q = [start];
+        visited[start.val] = null;
+        while (q.length > 0) {
+            const cur = q.shift();
+            if (cur === end) {
+                return this.rp(visited, end);
+            }
+            for (let a of cur.edges) {
+                if (!visited.hasOwnProperty(a.val)) {
+                    visited[a.val] = cur;
+                    q.push(a);
+                }
+            }
+        }
+    }
+    rp(visited, end) {
+        let cur = end;
+        let path = [];
+        while (cur !== null) {
+            path.push(cur.val);
+            cur = visited[cur.val];
+        }
+        return path.reverse();
+    }
+}
+
+const DFW = new Node('DFW');
+const JFK = new Node('JFK');
+const LAX = new Node('LAX');
+const HNL = new Node('HNL');
+const SAN = new Node('SAN');
+const EWR = new Node('EWR');
+const BOS = new Node('BOS');
+const MIA = new Node('MIA');
+const MCO = new Node('MCO');
+const PBI = new Node('PBI');
+const HKG = new Node('HKG');
+
+const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
+
+DFW.connect(JFK);
+DFW.connect(LAX);
+JFK.connect(BOS);
+JFK.connect(MIA);
+LAX.connect(HNL);
+LAX.connect(EWR);
+LAX.connect(SAN);
+SAN.connect(HKG);
+MIA.connect(MCO);
+MIA.connect(PBI);
+MCO.connect(PBI);
+
+// graph.bft(DFW, PBI);
+// graph.dft(DFW, HKG);
+console.log(graph.sp(DFW, PBI));
