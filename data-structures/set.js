061d7666d2edf8ec9c85d4cc69e4c2aa9227ea18
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-09 09:43:34 -0700

    sorting algorithm review

--- data-structures/set.js
+++ data-structures/set.js
@@ -19,49 +19,20 @@
 // tree
 // quick sort
 
-class HashMap {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let total = 0;
-        for (let i = 0; i < k.length; i++) {
-            total += k.charCodeAt(i);
-        }
-        return total % this.size;
-    }
-    add(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
+const quickSort = (list) => {
+    if (list.length <= 1) return list;
+    const pivot = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < pivot) {
+            left.push(list[i]);
         } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
+            right.push(list[i]);
         }
     }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
-    }
-    print() {
-        this.table.forEach(i => console.log(i));
-    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-const table = new HashMap(50);
-table.add('name', 'Ryan');
-table.add('age', 25);
-table.add('color', 'red');
-table.print();
-table.remove('color');
-table.add('mane', 'Clark');
-table.print();
-
+const arr = [5,2,1,-9,0.6,333,333,-0.3];
+console.log(quickSort(arr));
