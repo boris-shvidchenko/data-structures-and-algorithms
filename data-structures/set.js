a5dd3fba82c8aafdafee7303fd3be41e83981c29
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-11 11:16:39 -0700

    Selection sort review

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,53 +14,18 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-// hash
 // sel sort
 // bTree bft
 
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let total = 0;
-        for (let i = 0; i < k.length; i++) {
-            total += k.charCodeAt(i);
-        }
-        return total % this.size;
-    }
-    set(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] =[[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
-        }
-    }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
-    }
-    display() {
-        this.table.forEach(i => console.log(i));
-    }
+const arr = [90,-9,0.44,23, 23,2,-99];
+
+const sSort = (list, res=[]) => {
+    if (list.length === 0) return res;
+    const min = list.reduce((a,b) => a < b ? a : b);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return sSort(list, res);
 }
 
-const table = new HashTable(50);
-table.set('name', 'Ryan');
-table.set('age', 25);
-table.set('color', 'red');
-table.display();
-// console.log(table.get('name'));
-table.remove('color');
-table.set('mane', 'Clark');
-table.display();
+console.log(sSort(arr));
