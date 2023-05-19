dc58475f27eefba17aea39bef6a5abc5dcfd296a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-19 10:52:49 -0700

    string/obj method overview

--- algorithms/sort/selectionSort.js
+++ algorithms/sort/selectionSort.js
@@ -1,37 +1,46 @@
-// == Selection Sort ==
-// Sorts an array by identifying the smallest number within the array and storing the value in a variable. Once a full iteration has been performed, the smallest value is pushed into a results array and also removed from the original list. This process continues until the original array is empty and the results array is sorted in the correct order.
-// Runtime: O(n^2) / Quadratic
+// // == Selection Sort ==
+// // Sorts an array by identifying the smallest number within the array and storing the value in a variable. Once a full iteration has been performed, the smallest value is pushed into a results array and also removed from the original list. This process continues until the original array is empty and the results array is sorted in the correct order.
+// // Runtime: O(n^2) / Quadratic
 
-const testArray = [9,10,4,2,8,1,3,5,7,6];
+// const testArray = [9,10,4,2,8,1,3,5,7,6];
 
-// Stores the sorted array
-let newArray = [];
+// // Stores the sorted array
+// let newArray = [];
 
-function selectionSort(list) {
-    let min = list[0];
-    // Base case
-    if (list.length === 0) return newArray;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] < min) min = list[i];
-    }
-    newArray.push(min);
-    const minIndex = list.indexOf(min);
-    list.splice(minIndex,1);
-    // Test log
-    console.log('Input List: ' + list);
-    console.log('Output List: ' + newArray);
-    return selectionSort(list);
-}
-
-// Method 2: Using reduce
-// function selectionSort(list, results=[]) {
-//     if (list.length === 0) return results;
-//     const min = list.reduce((a,b) => a < b ? a : b);
-//     results.push(min);
+// function selectionSort(list) {
+//     let min = list[0];
+//     // Base case
+//     if (list.length === 0) return newArray;
+//     for (let i = 0; i < list.length; i++) {
+//         if (list[i] < min) min = list[i];
+//     }
+//     newArray.push(min);
 //     const minIndex = list.indexOf(min);
-//     list.splice(minIndex, 1);
-//     return selectionSort(list, results);
+//     list.splice(minIndex,1);
+//     // Test log
+//     console.log('Input List: ' + list);
+//     console.log('Output List: ' + newArray);
+//     return selectionSort(list);
 // }
 
-console.log(selectionSort(testArray));
+// // Method 2: Using reduce
+// // function selectionSort(list, results=[]) {
+// //     if (list.length === 0) return results;
+// //     const min = list.reduce((a,b) => a < b ? a : b);
+// //     results.push(min);
+// //     const minIndex = list.indexOf(min);
+// //     list.splice(minIndex, 1);
+// //     return selectionSort(list, results);
+// // }
 
+// console.log(selectionSort(testArray));
+
+const obj = {
+    name: 'Bryan',
+    age: 35,
+    city: 'SF',
+    job: true,
+    hobbies: ['read', 'write', 'run'],
+};
+
+console.log(Object.en(obj));
