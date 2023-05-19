3e9a3814825f8758088035759147da815e136699
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-18 19:21:14 -0700

    binary tree and merge sort review

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -4,65 +4,32 @@
 
 const testArray = [9,4,8,3,1,10,2,5,7,6];
 
-// // // Splits arrays in two
-// function mergeSort(list) {
-//     // Base Case
-//     if (list.length <= 1) return list;
-//     const midPoint = Math.floor(list.length/2);
-//     const left = list.slice(0,midPoint);
-//     const right = list.slice(midPoint);
-//     return merge(mergeSort(left), mergeSort(right));
-// }
-
-// // // Merges arrays together
-// function merge(left, right) {
-//     let resultsArray = [];
-//     let leftIndex = 0;
-//     let rightIndex = 0;
-//     while (leftIndex < left.length && rightIndex < right.length) {
-//         if (left[leftIndex] < right[rightIndex]) {
-//             resultsArray.push(left[leftIndex]);
-//             leftIndex++;
-//         } else {
-//             resultsArray.push(right[rightIndex]);
-//             rightIndex++;
-//         }
-//     }
-//     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-// }
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+// // Splits arrays in two
+function mergeSort(list) {
+    // Base Case
+    if (list.length <= 1) return list;
+    const midPoint = Math.floor(list.length/2);
+    const left = list.slice(0,midPoint);
+    const right = list.slice(midPoint);
+    return merge(mergeSort(left), mergeSort(right));
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
+// // Merges arrays together
+function merge(left, right) {
+    let resultsArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            resultsArray.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            resultsArray.push(right[rightIndex]);
+            rightIndex++;
+        }
     }
-    return false;
+    return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
-// console.log(bft(a))
-console.log(bft(a, 'b'))
-console.log(bft(a, 'z'))
+
+
+console.log(mergeSort(testArray)); 
