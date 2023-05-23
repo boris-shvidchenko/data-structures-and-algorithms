63808ebb1c537867bb5667491f05daa9d6c5f64c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-22 19:10:40 -0700

    merge sort review

--- algorithms/sort/mergeSort.js
+++ algorithms/sort/mergeSort.js
@@ -4,31 +4,55 @@
 
 const testArray = [9,4,8,3,1,10,2,5,7,6];
 
-// // Splits arrays in two
+// // // Splits arrays in two
+// function mergeSort(list) {
+//     // Base Case
+//     if (list.length <= 1) return list;
+//     const midPoint = Math.floor(list.length/2);
+//     const left = list.slice(0,midPoint);
+//     const right = list.slice(midPoint);
+//     return merge(mergeSort(left), mergeSort(right));
+// }
+
+// // // Merges arrays together
+// function merge(left, right) {
+//     let resultsArray = [];
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+//     while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             resultsArray.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             resultsArray.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+//     return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+// }
+
 function mergeSort(list) {
-    // Base Case
     if (list.length <= 1) return list;
-    const midPoint = Math.floor(list.length/2);
-    const left = list.slice(0,midPoint);
-    const right = list.slice(midPoint);
-    return merge(mergeSort(left), mergeSort(right));
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
 }
 
-// // Merges arrays together
-function merge(left, right) {
-    let resultsArray = [];
+function sort(left, right){
     let leftIndex = 0;
     let rightIndex = 0;
+    let res = [];
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
-            resultsArray.push(left[leftIndex]);
+            res.push(left[leftIndex]);
             leftIndex++;
         } else {
-            resultsArray.push(right[rightIndex]);
+            res.push(right[rightIndex]);
             rightIndex++;
         }
     }
-    return resultsArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
 
 
