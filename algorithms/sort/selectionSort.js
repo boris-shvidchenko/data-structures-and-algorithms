e818733dad6b75c55902bbcca0cb347f8b08af67
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-24 10:28:29 -0700

    Algorithm, memoization, and tabulation review

--- algorithms/sort/selectionSort.js
+++ algorithms/sort/selectionSort.js
@@ -4,24 +4,24 @@
 
 const testArray = [9,10,4,2,8,1,3,5,7,6];
 
-// Stores the sorted array
-let newArray = [];
+// // Stores the sorted array
+// let newArray = [];
 
-function selectionSort(list) {
-    let min = list[0];
-    // Base case
-    if (list.length === 0) return newArray;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] < min) min = list[i];
-    }
-    newArray.push(min);
-    const minIndex = list.indexOf(min);
-    list.splice(minIndex,1);
-    // Test log
-    console.log('Input List: ' + list);
-    console.log('Output List: ' + newArray);
-    return selectionSort(list);
-}
+// function selectionSort(list) {
+//     let min = list[0];
+//     // Base case
+//     if (list.length === 0) return newArray;
+//     for (let i = 0; i < list.length; i++) {
+//         if (list[i] < min) min = list[i];
+//     }
+//     newArray.push(min);
+//     const minIndex = list.indexOf(min);
+//     list.splice(minIndex,1);
+//     // Test log
+//     console.log('Input List: ' + list);
+//     console.log('Output List: ' + newArray);
+//     return selectionSort(list);
+// }
 
 // Method 2: Using reduce
 // function selectionSort(list, results=[]) {
@@ -33,5 +33,28 @@ function selectionSort(list) {
 //     return selectionSort(list, results);
 // }
 
-console.log(selectionSort(testArray));
+function selectionSort(list, res=[]) {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selectionSort(list, res);
+}
+
+// console.log(selectionSort(testArray));
+
+const fib = (v) => {
+    if (v <= 2) return 1;
+    const table = new Array(v + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i < v; i++) {
+        table[i+1] += table[i];
+        table[i+2] += table[i];
+    }
+    return table[v];
+}
+
+console.log(fib(20))
+console.log(fib(200))
 
