3441780de3af0af233418367cc1fccd085211d47
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-31 10:54:08 -0700

    Practice problem

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,80 +14,18 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-// graph
 // leet code ?
 // dsa notes review ?
 
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.edges = [];
-    }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
+function singleNumber(arr, index = 0) {
+    while (true) {
+        if (arr.filter(i => i === arr[index]).length === 1) return arr[index];
+        index++;
     }
 }
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
-    }
-    sp(start, end) {
-        const q = [start];
-        const visited = {};
-        visited[start.val] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) {
-                return this.rp(visited, end);
-            }
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
-            }
-        }
-    }
-    rp(visited, end) {
-        const path = [];
-        let cur = end;
-        while (cur !== null) {
-            path.push(cur.val);
-            cur = visited[cur.val];
-        }
-        return path.reverse();
-    }
+console.log(singleNumber([2,2,1])); // 1
+console.log(singleNumber([4,1,2,1,2])); // 4
+console.log(singleNumber([1])); //1 => THIS
 
-}
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-// graph.bft(DFW, PBI);
-// graph.dft(DFW, HKG);
-console.log(graph.sp(DFW, PBI));
