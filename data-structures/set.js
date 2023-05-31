cdd8175c3d616af1593afa7c047ec8717df44cee
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-31 10:54:56 -0700

    Practice problem

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,31 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
-
-// leet code ?
-// dsa notes review ?
-
-function singleNumber(arr, index = 0) {
-    while (true) {
-        if (arr.filter(i => i === arr[index]).length === 1) return arr[index];
-        index++;
-    }
-}
-
-console.log(singleNumber([2,2,1])); // 1
-console.log(singleNumber([4,1,2,1,2])); // 4
-console.log(singleNumber([1])); //1 => THIS
-
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
