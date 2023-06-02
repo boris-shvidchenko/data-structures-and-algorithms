8e67516fb44a6d0164da26378800a33d535dbcce
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-02 09:51:27 -0700

    merge sort review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,64 +1,106 @@
-// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// Common operations include node insertion, checking if a node exists, and removal of nodes.
+// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// // Common operations include node insertion, checking if a node exists, and removal of nodes.
 
-class Node {
-    constructor() {
-        this.children = {};
-        this.isWord = false;
-    }
+// class Node {
+//     constructor() {
+//         this.children = {};
+//         this.isWord = false;
+//     }
+// }
+
+// class Trie {
+//     constructor() {
+//         this.root = new Node();
+//     }
+
+//     // Insert a word
+//     insert(word) {
+//         let current = this.root;
+//         for (let i = 0; i < word.length; i++) {
+//             let charToInsert = word[i];
+//             if (!(charToInsert in current.children)) {
+//                 current.children[charToInsert] = new Node();
+//             }
+//             current = current.children[charToInsert];
+//         }
+//         current.isWord = true;
+//     }
+
+//     // Check if trie contains
+//     contains(word) {
+//         let current = this.root;
+//         for (let i = 0; i < word.length; i++) {
+//             let charToFind = word[i];
+//             if (!(charToFind in current.children)) {
+//                 return false;
+//             }
+//             current = current.children[charToFind];
+//         }
+//         return current.isWord;
+//     }
+
+//     // Check if word starts with prefix
+//     startsWithPrefix(prefix) {
+//         let current = this.root;
+//         for (let i = 0; i < prefix.length; i++) {
+//             let charToFind = prefix[i];
+//             if (!(charToFind in current.children)) {
+//                 return false;
+//             }
+//             current = current.children[charToFind];
+//         }
+//         return true;
+//     }
+
+//     // Print trie
+//     print() {
+//         console.log(this.root);
+//     }
+// }
+
+// let trie = new Trie();
+// trie.insert('Hello');
+// trie.insert('Goodbye');
+// trie.print();
+
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// merge sort
+
+const arr = [9,0,-3,5.6,5,5,100,-40];
+
+function mergesort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length / 2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergesort(left), mergesort(right));
 }
 
-class Trie {
-    constructor() {
-        this.root = new Node();
-    }
-
-    // Insert a word
-    insert(word) {
-        let current = this.root;
-        for (let i = 0; i < word.length; i++) {
-            let charToInsert = word[i];
-            if (!(charToInsert in current.children)) {
-                current.children[charToInsert] = new Node();
-            }
-            current = current.children[charToInsert];
+function sort(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let res = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
         }
-        current.isWord = true;
     }
-
-    // Check if trie contains
-    contains(word) {
-        let current = this.root;
-        for (let i = 0; i < word.length; i++) {
-            let charToFind = word[i];
-            if (!(charToFind in current.children)) {
-                return false;
-            }
-            current = current.children[charToFind];
-        }
-        return current.isWord;
-    }
-
-    // Check if word starts with prefix
-    startsWithPrefix(prefix) {
-        let current = this.root;
-        for (let i = 0; i < prefix.length; i++) {
-            let charToFind = prefix[i];
-            if (!(charToFind in current.children)) {
-                return false;
-            }
-            current = current.children[charToFind];
-        }
-        return true;
-    }
-
-    // Print trie
-    print() {
-        console.log(this.root);
-    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
 }
 
-let trie = new Trie();
-trie.insert('Hello');
-trie.insert('Goodbye');
-trie.print();
+console.log(mergesort(arr));
