9a3ba15faa3b6d9b6eb1f8c9fdfefe0de9a2b69b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-05 19:30:35 -0700

    hash and depth first traversal/search of trees practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,44 +1,10 @@
-// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-// let countries = new Map();
-// countries.set('US', 'North America');
-// countries.set('Canada', 'North America');
-// countries.set('Thailand', 'Asia');
-// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+let countries = new Map();
+countries.set('US', 'North America');
+countries.set('Canada', 'North America');
+countries.set('Thailand', 'Asia');
+console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// // Iteration splits each pair into sub-array
-// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
-
-
-// b tree - dft search
-
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-
-const dft = (root, val) => {
-    if (root === null) return false;
-    if (root.val === val) return true;
-    return dft(root.left, val) || dft(root.right, val);
-}
-
-console.log(dft(a, 'd'));
-console.log(dft(a, 'z'));
+// Iteration splits each pair into sub-array
+for (let country of countries) console.log(country[0] + ' > ' + country[1]);
