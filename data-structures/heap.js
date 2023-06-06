aaeba132a0b599c6ffd6b7d610a4374809590d04
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-06 10:38:27 -0700

    Graph setup/traversal review

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -1,174 +1,97 @@
-// // Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
+// Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
 
-// class Heap {
+class Heap {
 
-//     constructor() {
-//         this.data = [];
-//     }
-
-//     // Helper functions
-
-//     getParentIndex(index) {
-//         return Math.floor((index - 1) / 2);
-//     }
-
-//     getLeftChildIndex(index) {
-//         return index * 2 + 1;
-//     }
-
-//     getRightChildIndex(index) {
-//         return index * 2 + 2;
-//     }
-
-//     swap(index1, index2) {
-//         const temp = this.data[index1];
-//         this.data[index1] = this.data[index2];
-//         this.data[index2] = temp;
-//     }
-
-//     // Operations
-
-//     // Pushes new data (key) into our array/heap.
-//     push(key) {
-//         this.data[this.data.length] = key;
-//         this.heapifyUp();
-//     }
-
-//     // Moves our elements up so that they are in the correct heap order.
-//     heapifyUp() {
-//         let currentIndex = this.data.length - 1;
-//         while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
-//             this.swap(currentIndex, this.getParentIndex(currentIndex));
-//             currentIndex = this.getParentIndex(currentIndex);
-//         }
-//     }
-
-//     heapifyDown() {
-//         let currentIndex = 0;
-//         while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
-//             let biggestChildIndex = this.getLeftChildIndex(currentIndex);
-//             if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
-//                 biggestChildIndex = this.getRightChildIndex(currentIndex);
-//             }
-//             if (this.data[currentIndex] < this.data[biggestChildIndex]) {
-//                 this.swap(currentIndex, biggestChildIndex);
-//                 currentIndex = biggestChildIndex;
-//             } else {
-//                 return;
-//             }
-//         }
-//     }
-
-//     // Remove the top element and replaces it with the last element of our array/heap.
-//     pull() {
-//         const maxValue = this.data[0];
-//         this.data[0] = this.data[this.data.length - 1];
-//         this.data.length--;
-//         this.heapifyDown();
-//         return maxValue;
-//     }
-// }
-
-// const heap = new Heap();
-// console.log(heap);
-// heap.push(25);
-// heap.push(5);
-// heap.push(40);
-// heap.push(70);
-// heap.push(90);
-// heap.push(44);
-
-// console.log(heap.data.join(' > '));
-
-// //        90
-// //       /  \
-// //     70    44
-// //    /  \     \
-// //   5    40    25
-
-// let a = [];
-// a.push(heap.pull());
-// a.push(heap.pull());
-// a.push(heap.pull());
-// a.push(heap.pull());
-// a.push(heap.pull());
-
-// console.log('Top 5 items: ', a);
-// console.log(heap.data.join(' > '));
-
-
-// graph
-
-
-class Node {
-    constructor(v) {
-        this.v = v;
-        this.edges = [];
+    constructor() {
+        this.data = [];
     }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
-    }
-}
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
+    // Helper functions
+
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
     }
-    sp(start, end) {
-        const visited = {};
-        const q = [start];
-        visited[start.v] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) {
-                return this.rp(visited, end);
+
+    getLeftChildIndex(index) {
+        return index * 2 + 1;
+    }
+
+    getRightChildIndex(index) {
+        return index * 2 + 2;
+    }
+
+    swap(index1, index2) {
+        const temp = this.data[index1];
+        this.data[index1] = this.data[index2];
+        this.data[index2] = temp;
+    }
+
+    // Operations
+
+    // Pushes new data (key) into our array/heap.
+    push(key) {
+        this.data[this.data.length] = key;
+        this.heapifyUp();
+    }
+
+    // Moves our elements up so that they are in the correct heap order.
+    heapifyUp() {
+        let currentIndex = this.data.length - 1;
+        while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
+            this.swap(currentIndex, this.getParentIndex(currentIndex));
+            currentIndex = this.getParentIndex(currentIndex);
+        }
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+        while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
+            let biggestChildIndex = this.getLeftChildIndex(currentIndex);
+            if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
+                biggestChildIndex = this.getRightChildIndex(currentIndex);
             }
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.v)) {
-                    visited[a.v] = cur;
-                    q.push(a);
-                }
+            if (this.data[currentIndex] < this.data[biggestChildIndex]) {
+                this.swap(currentIndex, biggestChildIndex);
+                currentIndex = biggestChildIndex;
+            } else {
+                return;
             }
         }
     }
-    rp(visited, end) {
-        const path = [];
-        let cur = end;
-        while (cur !== null) {
-            path.push(cur.v);
-            cur = visited[cur.v];
-        }
-        return path.reverse();
+
+    // Remove the top element and replaces it with the last element of our array/heap.
+    pull() {
+        const maxValue = this.data[0];
+        this.data[0] = this.data[this.data.length - 1];
+        this.data.length--;
+        this.heapifyDown();
+        return maxValue;
     }
 }
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
+const heap = new Heap();
+console.log(heap);
+heap.push(25);
+heap.push(5);
+heap.push(40);
+heap.push(70);
+heap.push(90);
+heap.push(44);
 
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
+console.log(heap.data.join(' > '));
 
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
+//        90
+//       /  \
+//     70    44
+//    /  \     \
+//   5    40    25
 
-// graph.dft(DFW, HKG);
-// graph.bft(DFW, PBI);
-console.log(graph.sp(DFW, PBI));
+let a = [];
+a.push(heap.pull());
+a.push(heap.pull());
+a.push(heap.pull());
+a.push(heap.pull());
+a.push(heap.pull());
+
+console.log('Top 5 items: ', a);
+console.log(heap.data.join(' > '));
