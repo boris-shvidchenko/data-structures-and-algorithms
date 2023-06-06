a329531779cc0e4d5e02d0c7df9ed6b4f7227286
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-05 19:18:16 -0700

    selection sort review

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,10 +1,29 @@
-// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-let countries = new Map();
-countries.set('US', 'North America');
-countries.set('Canada', 'North America');
-countries.set('Thailand', 'Asia');
-console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+// let countries = new Map();
+// countries.set('US', 'North America');
+// countries.set('Canada', 'North America');
+// countries.set('Thailand', 'Asia');
+// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// Iteration splits each pair into sub-array
-for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+// // Iteration splits each pair into sub-array
+// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+
+// sel sort
+// hash
+// b tree - dft search
+// quick sort
+
+const arr = [2,2,0,40,-90,-4,5.993,5.990,1,34,3];
+
+const selSort = (list, res=[]) => {
+    if (list.length === 0) return res;
+    const min = list.reduce((a,b) => a < b ? a : b);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selSort(list, res);
+}
+
+// console.log(selSort(arr));
+
