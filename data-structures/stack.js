8552845e684dac7eb0a1e5a264b0856107dd1083
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-07 20:48:08 -0700

    algorithm review

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,51 +1,30 @@
-// // Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
+// Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
 
-// class Stack {
-//     constructor() {
-//         this.items = []; // Will store our items
-//     }
-//     push(value) {
-//         this.items.push(value)
-//     }
-//     pop() {
-//         if (this.items.length !== 0) this.items.pop();
-//     }
-//     peek() {
-//         if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
-//     }
-//     print() {
-//         console.log(this.items.join('|'));
-//     }
-// }
-
-// let stack = new Stack();
-// stack.push(1);
-// stack.print();
-// stack.push(2);
-// stack.print();
-// stack.push(3);
-// stack.print();
-// stack.push(4);
-// stack.print();
-// stack.peek(); // Expects 4
-
-// quick sort
-
-const arr = [3,0,5,5,-60,3.90,4.04,122];
-
-const quickSort = (list) => {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
+class Stack {
+    constructor() {
+        this.items = []; // Will store our items
     }
-    return [...quickSort(left), piv, ...quickSort(right)];
-};
+    push(value) {
+        this.items.push(value)
+    }
+    pop() {
+        if (this.items.length !== 0) this.items.pop();
+    }
+    peek() {
+        if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
+    }
+    print() {
+        console.log(this.items.join('|'));
+    }
+}
 
-console.log(quickSort(arr));
+let stack = new Stack();
+stack.push(1);
+stack.print();
+stack.push(2);
+stack.print();
+stack.push(3);
+stack.print();
+stack.push(4);
+stack.print();
+stack.peek(); // Expects 4
