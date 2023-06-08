5589735c08c2f651d28341de36307b09b9f7791c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-08 09:51:21 -0700

    binary tree level order search review

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -1,65 +1,27 @@
-// // == Binary Search ==
-// // Searchs an array for a target value by 1) splitting the array in half, 2) identifying whether the target value is ===, >, or < the value in the middle of the array, 3) removing the portion of the array that is < or > the target value, 3) repeats the process until the target value is identified. This algorithm assumes that the array is sorted.
-// // Runtime: O(log(n)) / Binary
+// == Binary Search ==
+// Searchs an array for a target value by 1) splitting the array in half, 2) identifying whether the target value is ===, >, or < the value in the middle of the array, 3) removing the portion of the array that is < or > the target value, 3) repeats the process until the target value is identified. This algorithm assumes that the array is sorted.
+// Runtime: O(log(n)) / Binary
 
-// const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// // Returns the index of the target value within the provided array
-// function binarySearch(array, targetValue) {
-//     const midPoint = Math.floor(array.length/2);
-//     // First statement is the base case
-//     if (array.length === 1) {
-//         if (targetValue === array[midPoint]) {
-//             return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
-//         } else {
-//             return `${targetValue} is not in [${testArray}]`;
-//         }
-//     } else if (targetValue < array[midPoint]) {
-//         const arrayLeft = array.slice(0, midPoint);
-//         return binarySearch(arrayLeft, targetValue);
-//     } else {
-//         const arrayRight = array.slice(midPoint);
-//         return binarySearch(arrayRight, targetValue);
-//     }
-// }
-
-// // Test log 
-// console.log(binarySearch(testArray,2))
-
-
-// b tree bft list
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+// Returns the index of the target value within the provided array
+function binarySearch(array, targetValue) {
+    const midPoint = Math.floor(array.length/2);
+    // First statement is the base case
+    if (array.length === 1) {
+        if (targetValue === array[midPoint]) {
+            return `Index of ${array[midPoint]} is ${testArray.indexOf(array[midPoint])}`; 
+        } else {
+            return `${targetValue} is not in [${testArray}]`;
+        }
+    } else if (targetValue < array[midPoint]) {
+        const arrayLeft = array.slice(0, midPoint);
+        return binarySearch(arrayLeft, targetValue);
+    } else {
+        const arrayRight = array.slice(midPoint);
+        return binarySearch(arrayRight, targetValue);
     }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function bft(root) {
-    if (root === null) return [];
-    const q = [root];
-    const res = [];
-    while (q.length > 0) {
-        const cur = q.shift();
-        res.push(cur.val);
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
-    }
-    return res;
-}
-
-console.log(bft(a));
+// Test log 
+console.log(binarySearch(testArray,2))
