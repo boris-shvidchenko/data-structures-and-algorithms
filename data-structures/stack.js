11360e08f32aeb6033b4c97639507cfabb8b6de4
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-12 19:10:23 -0700

    Hash map review

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -30,20 +30,52 @@
 // stack.peek(); // Expects 4
 
 
-// hash
-// fib tab
-
-
-const fib = (v) => {
-    if (v <= 2) return 1;
-    const table = new Array(v + 1).fill(0);
-    table[1] = 1;
-    for (let i = 0; i < v; i++) {
-        table[i+1] += table[i];
-        table[i+2] += table[i];
+class HashTable {
+    constructor(size) {
+        this.size = size;
+        this.table = new Array(size);
     }
-    return table[v];
+    hash(k) {
+        let t = 0;
+        for (let i=0; i < k.length; i++) {
+            t += k.charCodeAt(i);
+        }
+        return t % this.size;
+    }
+    set(k,v) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        if (!bucket) {
+            this.table[index] = [[k,v]];
+        } else {
+            const sameKey = bucket.find(i => i[0] === k);
+            if (sameKey) {
+                sameKey[1] = v;
+            } else {
+                bucket.push([k,v]);
+            }
+        }
+    }
+    remove(k) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        const sameKey = bucket.find(i => i[0] === k);
+    }
+    display() {
+        this.table.forEach(i => console.log(i));
+    }
 }
 
-console.log(fib(10));
-console.log(fib(100));
+
+
+
+
+
+const table = new HashTable(50);
+table.set('name', 'Ryan');
+table.set('age', 25);
+table.set('color', 'red');
+table.display();
+table.remove('color');
+table.set('mane', 'Clark');
+table.display();
