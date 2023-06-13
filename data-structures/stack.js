e376b95c452b2ff1c6d4f309471ce117f61412a8
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-12 19:10:42 -0700

    Hash map review

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,81 +1,30 @@
-// // Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
+// Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
 
-// class Stack {
-//     constructor() {
-//         this.items = []; // Will store our items
-//     }
-//     push(value) {
-//         this.items.push(value)
-//     }
-//     pop() {
-//         if (this.items.length !== 0) this.items.pop();
-//     }
-//     peek() {
-//         if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
-//     }
-//     print() {
-//         console.log(this.items.join('|'));
-//     }
-// }
-
-// let stack = new Stack();
-// stack.push(1);
-// stack.print();
-// stack.push(2);
-// stack.print();
-// stack.push(3);
-// stack.print();
-// stack.push(4);
-// stack.print();
-// stack.peek(); // Expects 4
-
-
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
+class Stack {
+    constructor() {
+        this.items = []; // Will store our items
     }
-    hash(k) {
-        let t = 0;
-        for (let i=0; i < k.length; i++) {
-            t += k.charCodeAt(i);
-        }
-        return t % this.size;
+    push(value) {
+        this.items.push(value)
     }
-    set(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
-        }
+    pop() {
+        if (this.items.length !== 0) this.items.pop();
     }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
+    peek() {
+        if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
     }
-    display() {
-        this.table.forEach(i => console.log(i));
+    print() {
+        console.log(this.items.join('|'));
     }
 }
 
-
-
-
-
-
-const table = new HashTable(50);
-table.set('name', 'Ryan');
-table.set('age', 25);
-table.set('color', 'red');
-table.display();
-table.remove('color');
-table.set('mane', 'Clark');
-table.display();
+let stack = new Stack();
+stack.push(1);
+stack.print();
+stack.push(2);
+stack.print();
+stack.push(3);
+stack.print();
+stack.push(4);
+stack.print();
+stack.peek(); // Expects 4
