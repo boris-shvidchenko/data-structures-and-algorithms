b6723de8ad127b1a7b0e8de07bac12c2ad8563e5
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-13 09:14:37 -0700

    memoization review

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,54 +14,19 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-// quick sort
-// merge sort
+
+
 // fib memo
 // linked list
 // graph
 // btree bft target
 
-const arr = [70,9,0,-200,40.5,4.4,20000,10,-2030];
-
-const quickSort = (list) => {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
-    }
-    return [...quickSort(left), piv, ...quickSort(right)];
+const fib = (v, memo={}) => {
+    if (v <= 2) return 1;
+    if (v in memo) return memo[v];
+    memo[v] = fib(v-1, memo) + fib(v-2, memo);
+    return memo[v];
 }
 
-// console.log(quickSort(arr));
-
-console.log(mergeSort(arr));
-
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
-
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    const res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-}
+console.log(fib(10));
+console.log(fib(100));
