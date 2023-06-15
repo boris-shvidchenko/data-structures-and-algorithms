b1a7236ec398f3adebb8d29d58387d065609caa6
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-15 12:56:12 -0700

    linked list practice

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -64,73 +64,82 @@
 // trie.print();
 
 // link list
-// graph sp
 
 class Node {
     constructor(val) {
         this.val = val;
-        this.edges = [];
-    }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
+        this.next = null;
     }
 }
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
     }
-    sp(start, end) {
-        const visited = {};
-        const q = [start];
-        visited[start.val] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) return this.rp(visited, end);
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
+    append(node) { 
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
             }
+            cur.next = newNode;
         }
+        this.length++;
     }
-    rp(visited, end) {
-        let cur = end;
-        const path = [];
-        while (cur !== null) {
-            path.push(cur.val);
-            cur = visited[cur.val];
+    appendAt(node, index) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let curIndex = 0;
+        let prev;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
         }
-        return path.reverse();
+        this.length++;
+    }
+    print() {
+        let cur = this.head;
+        let str = '';
+        while (cur !== null) {
+            str += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(str);
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
     }
 }
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-console.log(graph.sp(PBI, MCO));
+// Test
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.print();
+list.remove('b'); // a -> c -> etc.
+list.appendAt('z', 0);
+list.print();
