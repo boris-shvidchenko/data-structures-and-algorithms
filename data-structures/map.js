92c87d760daa58a615f02af8db4923b744cf009d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-14 19:32:24 -0700

    level order traversal review

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,49 +1,10 @@
-// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-// let countries = new Map();
-// countries.set('US', 'North America');
-// countries.set('Canada', 'North America');
-// countries.set('Thailand', 'Asia');
-// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+let countries = new Map();
+countries.set('US', 'North America');
+countries.set('Canada', 'North America');
+countries.set('Thailand', 'Asia');
+console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// // Iteration splits each pair into sub-array
-// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
-
-
-// graph bft list
-
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function bft(root) {
-    if (root === null) return [];
-    const q = [root];
-    const res = [];
-    while (q.length > 0) {
-        const cur = q.shift();
-        res.push(cur.val);
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
-    }
-    return res;
-}
-
-console.log(bft(a));
+// Iteration splits each pair into sub-array
+for (let country of countries) console.log(country[0] + ' > ' + country[1]);
