185d29ed0b3d4057eec226b2f448a4314d5a034b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-15 12:45:19 -0700

    sel sort and memoization review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,64 +1,91 @@
-// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// Common operations include node insertion, checking if a node exists, and removal of nodes.
+// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// // Common operations include node insertion, checking if a node exists, and removal of nodes.
 
-class Node {
-    constructor() {
-        this.children = {};
-        this.isWord = false;
-    }
+// class Node {
+//     constructor() {
+//         this.children = {};
+//         this.isWord = false;
+//     }
+// }
+
+// class Trie {
+//     constructor() {
+//         this.root = new Node();
+//     }
+
+//     // Insert a word
+//     insert(word) {
+//         let current = this.root;
+//         for (let i = 0; i < word.length; i++) {
+//             let charToInsert = word[i];
+//             if (!(charToInsert in current.children)) {
+//                 current.children[charToInsert] = new Node();
+//             }
+//             current = current.children[charToInsert];
+//         }
+//         current.isWord = true;
+//     }
+
+//     // Check if trie contains
+//     contains(word) {
+//         let current = this.root;
+//         for (let i = 0; i < word.length; i++) {
+//             let charToFind = word[i];
+//             if (!(charToFind in current.children)) {
+//                 return false;
+//             }
+//             current = current.children[charToFind];
+//         }
+//         return current.isWord;
+//     }
+
+//     // Check if word starts with prefix
+//     startsWithPrefix(prefix) {
+//         let current = this.root;
+//         for (let i = 0; i < prefix.length; i++) {
+//             let charToFind = prefix[i];
+//             if (!(charToFind in current.children)) {
+//                 return false;
+//             }
+//             current = current.children[charToFind];
+//         }
+//         return true;
+//     }
+
+//     // Print trie
+//     print() {
+//         console.log(this.root);
+//     }
+// }
+
+// let trie = new Trie();
+// trie.insert('Hello');
+// trie.insert('Goodbye');
+// trie.print();
+
+// link list
+// graph sp
+// fib memo
+
+const ar = [5,-8,32,0.8,3.1,22,3,33,3,-7];
+
+function selSort(list, res=[]) {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selSort(list, res);
 }
 
-class Trie {
-    constructor() {
-        this.root = new Node();
-    }
+// console.log(selSort(ar));
 
-    // Insert a word
-    insert(word) {
-        let current = this.root;
-        for (let i = 0; i < word.length; i++) {
-            let charToInsert = word[i];
-            if (!(charToInsert in current.children)) {
-                current.children[charToInsert] = new Node();
-            }
-            current = current.children[charToInsert];
-        }
-        current.isWord = true;
-    }
-
-    // Check if trie contains
-    contains(word) {
-        let current = this.root;
-        for (let i = 0; i < word.length; i++) {
-            let charToFind = word[i];
-            if (!(charToFind in current.children)) {
-                return false;
-            }
-            current = current.children[charToFind];
-        }
-        return current.isWord;
-    }
-
-    // Check if word starts with prefix
-    startsWithPrefix(prefix) {
-        let current = this.root;
-        for (let i = 0; i < prefix.length; i++) {
-            let charToFind = prefix[i];
-            if (!(charToFind in current.children)) {
-                return false;
-            }
-            current = current.children[charToFind];
-        }
-        return true;
-    }
-
-    // Print trie
-    print() {
-        console.log(this.root);
-    }
+const fib = (v, memo={}) => {
+    if (v <= 2) return 1;
+    if (v in memo) return memo[v];
+    memo[v] = fib(v-1, memo) + fib(v-2, memo);
+    return memo[v];
 }
 
-let trie = new Trie();
-trie.insert('Hello');
-trie.insert('Goodbye');
-trie.print();
+console.log(fib(10))
+console.log(fib(100))
