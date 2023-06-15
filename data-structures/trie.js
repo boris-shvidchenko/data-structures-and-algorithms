41fb7621e9b6301b80602fa4d4bb14695ae9d470
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-15 12:56:41 -0700

    linked list practice

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,145 +1,64 @@
-// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// // Common operations include node insertion, checking if a node exists, and removal of nodes.
-
-// class Node {
-//     constructor() {
-//         this.children = {};
-//         this.isWord = false;
-//     }
-// }
-
-// class Trie {
-//     constructor() {
-//         this.root = new Node();
-//     }
-
-//     // Insert a word
-//     insert(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToInsert = word[i];
-//             if (!(charToInsert in current.children)) {
-//                 current.children[charToInsert] = new Node();
-//             }
-//             current = current.children[charToInsert];
-//         }
-//         current.isWord = true;
-//     }
-
-//     // Check if trie contains
-//     contains(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToFind = word[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return current.isWord;
-//     }
-
-//     // Check if word starts with prefix
-//     startsWithPrefix(prefix) {
-//         let current = this.root;
-//         for (let i = 0; i < prefix.length; i++) {
-//             let charToFind = prefix[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return true;
-//     }
-
-//     // Print trie
-//     print() {
-//         console.log(this.root);
-//     }
-// }
-
-// let trie = new Trie();
-// trie.insert('Hello');
-// trie.insert('Goodbye');
-// trie.print();
-
-// link list
+// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// Common operations include node insertion, checking if a node exists, and removal of nodes.
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-class LinkedList {
     constructor() {
-        this.length = 0;
-        this.head = null;
-    }
-    append(node) { 
-        const newNode = new Node(node);
-        let cur = this.head;
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            while (cur.next !== null) {
-                cur = cur.next;
-            }
-            cur.next = newNode;
-        }
-        this.length++;
-    }
-    appendAt(node, index) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        let curIndex = 0;
-        let prev;
-        if (index === 0) {
-            this.head = newNode;
-            newNode.next = cur;
-        } else {
-            while (curIndex < index) {
-                prev = cur;
-                cur = cur.next;
-                curIndex++;
-            }
-            prev.next = newNode;
-            newNode.next = cur;
-        }
-        this.length++;
-    }
-    print() {
-        let cur = this.head;
-        let str = '';
-        while (cur !== null) {
-            str += cur.val + '>';
-            cur = cur.next;
-        }
-        console.log(str);
-    }
-    remove(node) {
-        let cur = this.head;
-        let prev;
-        if (this.head.val === node) {
-            this.head = cur.next;
-        } else {
-            while (cur.val !== node) {
-                prev = cur;
-                cur = cur.next
-            }
-            prev.next = cur.next;
-        }
-        this.length--;
+        this.children = {};
+        this.isWord = false;
     }
 }
 
-// Test
-const list = new LinkedList();
-list.append('a');
-list.append('b');
-list.append('c');
-list.print();
-list.remove('b'); // a -> c -> etc.
-list.appendAt('z', 0);
-list.print();
+class Trie {
+    constructor() {
+        this.root = new Node();
+    }
+
+    // Insert a word
+    insert(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToInsert = word[i];
+            if (!(charToInsert in current.children)) {
+                current.children[charToInsert] = new Node();
+            }
+            current = current.children[charToInsert];
+        }
+        current.isWord = true;
+    }
+
+    // Check if trie contains
+    contains(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToFind = word[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return current.isWord;
+    }
+
+    // Check if word starts with prefix
+    startsWithPrefix(prefix) {
+        let current = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let charToFind = prefix[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return true;
+    }
+
+    // Print trie
+    print() {
+        console.log(this.root);
+    }
+}
+
+let trie = new Trie();
+trie.insert('Hello');
+trie.insert('Goodbye');
+trie.print();
