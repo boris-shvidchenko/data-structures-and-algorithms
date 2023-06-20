8925e58cd916c59766aa4c93743fea30e9b1d20f
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-19 19:20:24 -0700

    merge sort review

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,10 +1,42 @@
-// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-let countries = new Map();
-countries.set('US', 'North America');
-countries.set('Canada', 'North America');
-countries.set('Thailand', 'Asia');
-console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+// let countries = new Map();
+// countries.set('US', 'North America');
+// countries.set('Canada', 'North America');
+// countries.set('Thailand', 'Asia');
+// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// Iteration splits each pair into sub-array
-for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+// // Iteration splits each pair into sub-array
+// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+
+
+// merge sort
+// hash
+
+const arr = [1,5,-9,9,9,0.2,0.3,3.5,514,-9898];
+
+const sort = (left, right) => {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    const res = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+const mergeSort = (list) => {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length / 2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort(arr));
