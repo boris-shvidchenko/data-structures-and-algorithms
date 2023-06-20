bc5da121c5628cdf760dedb37665219da7e507dc
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-20 12:50:39 -0700

    binary tree traversale review

--- data-structures/map.js
+++ data-structures/map.js
@@ -10,39 +10,46 @@
 // for (let country of countries) console.log(country[0] + ' > ' + country[1]);
 
 
-// quick sort
 // graph
 // btree dfsearch
 
-const fib = (val) => {
-    if (val <= 2) return 1;
-    const table = new Array(val + 1).fill(0);
-    table[1] = 1;
-    for (let i = 0; i < val; i++) {
-        table[i + 1] += table[i];
-        table[i + 2] += table[i];
+class Node {
+    constructor(v) {
+        this.v = v;
+        this.left = null;
+        this.right = null;
     }
-    return table[val];
 }
 
-// console.log(fib(10));
-// console.log(fib(100));
+const g = new Node(3);
+const h = new Node(11);
+const i = new Node(4);
+const j = new Node(4);
+const k = new Node(2);
+const l = new Node(1);
+g.left = h;
+g.right = i;
+h.left = j;
+h.right = k;
+i.right = l;
 
-const a = [1,3,4,2,-90,5.9,5,90,0,333,-99];
+//    3
+//   / \
+//  11  4
+// /  \  \
+//4   2   1 
 
-function quickSort(list) {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
+function bft(root, t) {
+    if (root === null) return false;
+    const q = [root];
+    while (q.length > 0) {
+        const cur = q.shift();
+        if (cur.v === t) return true;
+        if (cur.left !== null) q.push(cur.left);
+        if (cur.right !== null) q.push(cur.right);
     }
-    return [...quickSort(left), piv, ...quickSort(right)];
+    return false;
 }
 
-console.log(quickSort(a));
+console.log(bft(g, 2));
+console.log(bft(g, 20));
