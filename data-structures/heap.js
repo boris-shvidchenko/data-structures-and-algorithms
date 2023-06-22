188cf9c2f88c1fac06d15e98fd9ae7e6b9bc2908
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-22 10:44:22 -0700

    Worked on algorithm review

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -1,97 +1,162 @@
-// Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
+// // Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
 
-class Heap {
+// class Heap {
 
-    constructor() {
-        this.data = [];
-    }
+//     constructor() {
+//         this.data = [];
+//     }
 
-    // Helper functions
+//     // Helper functions
 
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
+//     getParentIndex(index) {
+//         return Math.floor((index - 1) / 2);
+//     }
 
-    getLeftChildIndex(index) {
-        return index * 2 + 1;
-    }
+//     getLeftChildIndex(index) {
+//         return index * 2 + 1;
+//     }
 
-    getRightChildIndex(index) {
-        return index * 2 + 2;
-    }
+//     getRightChildIndex(index) {
+//         return index * 2 + 2;
+//     }
 
-    swap(index1, index2) {
-        const temp = this.data[index1];
-        this.data[index1] = this.data[index2];
-        this.data[index2] = temp;
-    }
+//     swap(index1, index2) {
+//         const temp = this.data[index1];
+//         this.data[index1] = this.data[index2];
+//         this.data[index2] = temp;
+//     }
 
-    // Operations
+//     // Operations
 
-    // Pushes new data (key) into our array/heap.
-    push(key) {
-        this.data[this.data.length] = key;
-        this.heapifyUp();
-    }
+//     // Pushes new data (key) into our array/heap.
+//     push(key) {
+//         this.data[this.data.length] = key;
+//         this.heapifyUp();
+//     }
 
-    // Moves our elements up so that they are in the correct heap order.
-    heapifyUp() {
-        let currentIndex = this.data.length - 1;
-        while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
-            this.swap(currentIndex, this.getParentIndex(currentIndex));
-            currentIndex = this.getParentIndex(currentIndex);
-        }
-    }
+//     // Moves our elements up so that they are in the correct heap order.
+//     heapifyUp() {
+//         let currentIndex = this.data.length - 1;
+//         while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
+//             this.swap(currentIndex, this.getParentIndex(currentIndex));
+//             currentIndex = this.getParentIndex(currentIndex);
+//         }
+//     }
 
-    heapifyDown() {
-        let currentIndex = 0;
-        while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
-            let biggestChildIndex = this.getLeftChildIndex(currentIndex);
-            if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
-                biggestChildIndex = this.getRightChildIndex(currentIndex);
-            }
-            if (this.data[currentIndex] < this.data[biggestChildIndex]) {
-                this.swap(currentIndex, biggestChildIndex);
-                currentIndex = biggestChildIndex;
-            } else {
-                return;
-            }
-        }
-    }
+//     heapifyDown() {
+//         let currentIndex = 0;
+//         while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
+//             let biggestChildIndex = this.getLeftChildIndex(currentIndex);
+//             if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
+//                 biggestChildIndex = this.getRightChildIndex(currentIndex);
+//             }
+//             if (this.data[currentIndex] < this.data[biggestChildIndex]) {
+//                 this.swap(currentIndex, biggestChildIndex);
+//                 currentIndex = biggestChildIndex;
+//             } else {
+//                 return;
+//             }
+//         }
+//     }
 
-    // Remove the top element and replaces it with the last element of our array/heap.
-    pull() {
-        const maxValue = this.data[0];
-        this.data[0] = this.data[this.data.length - 1];
-        this.data.length--;
-        this.heapifyDown();
-        return maxValue;
+//     // Remove the top element and replaces it with the last element of our array/heap.
+//     pull() {
+//         const maxValue = this.data[0];
+//         this.data[0] = this.data[this.data.length - 1];
+//         this.data.length--;
+//         this.heapifyDown();
+//         return maxValue;
+//     }
+// }
+
+// const heap = new Heap();
+// console.log(heap);
+// heap.push(25);
+// heap.push(5);
+// heap.push(40);
+// heap.push(70);
+// heap.push(90);
+// heap.push(44);
+
+// console.log(heap.data.join(' > '));
+
+// //        90
+// //       /  \
+// //     70    44
+// //    /  \     \
+// //   5    40    25
+
+// let a = [];
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+
+// console.log('Top 5 items: ', a);
+// console.log(heap.data.join(' > '));
+
+// binary tree
+
+// const a1 = [1,2,3,4,5,6];
+const a2 = [3,-90,402,1,4.5,4.6,2,2];
+
+function bs(list, t) {
+    if (list.length === 1) {
+        if (list[0] === t) return true;
+        return false;
+    } else {
+        const mid = Math.floor(list.length/2);
+        if (list[mid] === t) return true;
+        if (t < list[mid]) return bs(list.slice(0, mid), t);
+        return bs(list.slice(mid), t);
     }
 }
 
-const heap = new Heap();
-console.log(heap);
-heap.push(25);
-heap.push(5);
-heap.push(40);
-heap.push(70);
-heap.push(90);
-heap.push(44);
+function selSort(list, res=[]) {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    return selSort(list.splice(minIndex, 1), res.push(min));
+}
 
-console.log(heap.data.join(' > '));
+const qs = (list) => {
+    if (list.length <= 1) return list;
+    const pivot = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < pivot) {
+            left.push(list[i]);
+        } else {
+            right.push(list[i]);
+        }
+    }
+    return [...qs(left), pivot, ...qs(right)];
+}
 
-//        90
-//       /  \
-//     70    44
-//    /  \     \
-//   5    40    25
+function mergeSort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
+}
 
-let a = [];
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
+function sort(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    const res = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
 
-console.log('Top 5 items: ', a);
-console.log(heap.data.join(' > '));
+console.log(mergeSort(a2));
