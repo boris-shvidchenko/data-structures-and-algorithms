b5ff2d13ba67e17cb0cdbcfa644126a516308d57
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-06-21 19:39:33 -0700

    Linked list practice

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,123 +1,30 @@
-// // Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
+// Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
 
-// class Stack {
-//     constructor() {
-//         this.items = []; // Will store our items
-//     }
-//     push(value) {
-//         this.items.push(value)
-//     }
-//     pop() {
-//         if (this.items.length !== 0) this.items.pop();
-//     }
-//     peek() {
-//         if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
-//     }
-//     print() {
-//         console.log(this.items.join('|'));
-//     }
-// }
-
-// let stack = new Stack();
-// stack.push(1);
-// stack.print();
-// stack.push(2);
-// stack.print();
-// stack.push(3);
-// stack.print();
-// stack.push(4);
-// stack.print();
-// stack.peek(); // Expects 4
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-class LinkedList {
+class Stack {
     constructor() {
-        this.head = null;
-        this.length = 0;
+        this.items = []; // Will store our items
     }
-    append(node) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            while (cur.next !== null) {
-                cur = cur.next;
-            }
-            cur.next = newNode;
-        }
-        this.length++;
+    push(value) {
+        this.items.push(value)
     }
-    appendAt(node, index) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        let curIndex = 0;
-        let prev;
-        if (index === 0) {
-            this.head = newNode;
-            newNode.next = cur;
-        } else {
-            while (curIndex < index) {
-                prev = cur;
-                cur = cur.next;
-                curIndex++;
-            }
-            prev.next = newNode;
-            newNode.next = cur;
-        }
-        this.length++;
+    pop() {
+        if (this.items.length !== 0) this.items.pop();
     }
-    contains(node) {
-        let cur = this.head;
-        while (cur !== null) {
-            if (cur.val === node) {
-                console.log('True');
-                return;
-            }
-            cur = cur.next;
-        }
-        console.log('False');
-    }
-    remove(node) {
-        let cur = this.head;
-        let prev;
-        if (this.head.val === node) {
-            this.head = cur.next;
-        } else {
-            while (cur.val !== node) {
-                prev = cur;
-                cur = cur.next;
-            }
-            prev.next = cur.next;
-        }
-        this.length--;
+    peek() {
+        if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
     }
     print() {
-        let cur = this.head;
-        let str = '';
-        while (cur !== null) {
-            str += cur.val + '>';
-            cur = cur.next;
-        }
-        console.log(str);
+        console.log(this.items.join('|'));
     }
 }
 
-// Test
-const list = new LinkedList();
-list.append('a');
-list.append('b');
-list.append('c');
-list.contains('a'); // true
-list.contains('z'); // false
-list.print();
-list.remove('b'); // a -> c -> etc.
-list.appendAt('z', 1);
-list.print();
-list.contains('z'); // true
+let stack = new Stack();
+stack.push(1);
+stack.print();
+stack.push(2);
+stack.print();
+stack.push(3);
+stack.print();
+stack.push(4);
+stack.print();
+stack.peek(); // Expects 4
