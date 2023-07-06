0131d767e32fd91b8d4e45bc8c897d2f7f6b780d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-06 12:53:15 -0700

    tabulation review

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -1,28 +1,45 @@
-// Queues are 'first-in first-out' data structures. They use shift/unshift type operations to add/remove elements.
+// // Queues are 'first-in first-out' data structures. They use shift/unshift type operations to add/remove elements.
 
-class Queue {
-    constructor() {
-        this.items = [];
+// class Queue {
+//     constructor() {
+//         this.items = [];
+//     }
+//     add(item) {
+//         this.items.unshift(item);
+//     }
+//     remove() {
+//         if (this.items.length !== 0) this.items.pop();
+//     }
+//     print() {
+//         if (this.items.length !== 0) console.log(this.items.join('|'));
+//     }
+// }
+
+// let queue = new Queue();
+// queue.add(1);
+// queue.print()
+// queue.add(2);
+// queue.print()
+// queue.add(3);
+// queue.print()
+// queue.add(4);
+// queue.print()
+// queue.remove()
+// queue.print()
+
+// fib tab
+// sel sort
+// graph all
+
+const fib = (val) => {
+    if (val <= 2) return 1;
+    const tab = new Array(val + 1).fill(0);
+    tab[1] = 1;
+    for (let i = 0; i < val; i++) {
+        tab[i+1] += tab[i];
+        tab[i+2] += tab[i];
     }
-    add(item) {
-        this.items.unshift(item);
-    }
-    remove() {
-        if (this.items.length !== 0) this.items.pop();
-    }
-    print() {
-        if (this.items.length !== 0) console.log(this.items.join('|'));
-    }
+    return tab[val];
 }
-
-let queue = new Queue();
-queue.add(1);
-queue.print()
-queue.add(2);
-queue.print()
-queue.add(3);
-queue.print()
-queue.add(4);
-queue.print()
-queue.remove()
-queue.print()
+console.log(fib(20))
+console.log(fib(202))
