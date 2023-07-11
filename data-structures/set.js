f36aecf7a674e5d8939891fbe015059bb9aed387
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-10 19:39:18 -0700

    hashmap review

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,108 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
-
-
-
-// hash
-
-
-const ar = [1,90,-30,3.5,35.5,9,9];
-
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
-
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-}
-
-// console.log(mergeSort(ar));
-
-
-const selSort = (list, res=[]) => {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return selSort(list, res);
-}
-
-// console.log(selSort(ar));
-
-class HashMap {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let t = 0;
-        for (let i = 0; i < k.length; i++) {
-            t += k.charCodeAt(i);
-        }
-        return t % this.size;
-    }
-    set(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
-        }
-    }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
-    }
-    display() {
-        this.table.forEach(i => console.log(i));
-    }
-}
-
-const table = new HashMap(50);
-table.set('name', 'Ryan');
-table.set('age', 25);
-table.set('color', 'red');
-table.display();
-// console.log(table.get('name'));
-table.remove('color');
-table.set('mane', 'Clark');
-table.display();
