a76a9179617d3e5b53e7e41cfeba4ddac3b725e6
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-10 19:38:56 -0700

    hashmap review

--- data-structures/set.js
+++ data-structures/set.js
@@ -13,8 +13,12 @@
 
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
+
+
+
+
 // hash
-// sel sort
+
 
 const ar = [1,90,-30,3.5,35.5,9,9];
 
@@ -55,3 +59,50 @@ const selSort = (list, res=[]) => {
 }
 
 // console.log(selSort(ar));
+
+class HashMap {
+    constructor(size) {
+        this.size = size;
+        this.table = new Array(size);
+    }
+    hash(k) {
+        let t = 0;
+        for (let i = 0; i < k.length; i++) {
+            t += k.charCodeAt(i);
+        }
+        return t % this.size;
+    }
+    set(k,v) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        if (!bucket) {
+            this.table[index] = [[k,v]];
+        } else {
+            const sameKey = bucket.find(i => i[0] === k);
+            if (sameKey) {
+                sameKey[1] = v;
+            } else {
+                bucket.push([k,v]);
+            }
+        }
+    }
+    remove(k) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        const sameKey = bucket.find(i => i[0] === k);
+        bucket.splice(bucket.indexOf(sameKey), 1);
+    }
+    display() {
+        this.table.forEach(i => console.log(i));
+    }
+}
+
+const table = new HashMap(50);
+table.set('name', 'Ryan');
+table.set('age', 25);
+table.set('color', 'red');
+table.display();
+// console.log(table.get('name'));
+table.remove('color');
+table.set('mane', 'Clark');
+table.display();
