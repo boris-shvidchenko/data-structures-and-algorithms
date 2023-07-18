bbcea348534d3a0016af32a9734b803c1f804bdc
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-17 19:31:33 -0700

    Algorithm practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,77 +1,10 @@
-// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-// let countries = new Map();
-// countries.set('US', 'North America');
-// countries.set('Canada', 'North America');
-// countries.set('Thailand', 'Asia');
-// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+let countries = new Map();
+countries.set('US', 'North America');
+countries.set('Canada', 'North America');
+countries.set('Thailand', 'Asia');
+console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// // Iteration splits each pair into sub-array
-// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
-
-
-
-
-const a = [1,-90,3.4,3.5,3,3,403,-93];
-
-
-// merge sort
-
-function merge(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(merge(left), merge(right));
-}
-
-function sort(left, right) {
-    const res = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-}
-
-// console.log(merge(a));
-
-// q sort
-
-function qs(list) {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] <= piv) left.push(list[i]);
-        if (list[i] > piv) right.push(list[i]);
-    }
-    return [...qs(left), piv, ...qs(right)];
-}
-
-// console.log(qs(a));
-
-
-// fib
-
-const fib = (v) => {
-    if (v <= 2) return 1;
-    const table = new Array(v+1).fill(0);
-    table[1] = 1;
-    for (let i = 0; i < v; i++) {
-        table[i+1] += table[i];
-        table[i+2] += table[i];
-    }
-    return table[v];
-}
-
-console.log(fib(22))
-console.log(fib(120))
+// Iteration splits each pair into sub-array
+for (let country of countries) console.log(country[0] + ' > ' + country[1]);
