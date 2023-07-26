f5436d8e9e7069f6e1fa119c5f63ec423a096646
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-25 19:20:13 -0700

    fib sequence review

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,10 +1,28 @@
-// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-let countries = new Map();
-countries.set('US', 'North America');
-countries.set('Canada', 'North America');
-countries.set('Thailand', 'Asia');
-console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+// let countries = new Map();
+// countries.set('US', 'North America');
+// countries.set('Canada', 'North America');
+// countries.set('Thailand', 'Asia');
+// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// Iteration splits each pair into sub-array
-for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+// // Iteration splits each pair into sub-array
+// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+
+// linked list
+// hash
+// fib tab
+
+const fib = (val) => {
+    if (val <= 2) return 1;
+    const tab = new Array(val + 1).fill(0);
+    tab[1] = 1;
+    for (let i = 0; i < val; i++) {
+        tab[i+1] += tab[i];
+        tab[i+2] += tab[i];
+    }
+    return tab[val];
+}
+
+console.log(fib(10))
+console.log(fib(320))
