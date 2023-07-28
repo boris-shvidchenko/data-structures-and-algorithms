ba5d63b758826d84bfbf64ba2f929f32e6d22e60
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-28 12:38:11 -0700

    merge sort review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -69,13 +69,28 @@
 
 let a = [0.3, 32, -50, 42, 4, 4 ,10.443, -904];
 
-const selSort = (list, res=[]) => {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return selSort(list, res);
+function mergeSort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
 }
 
-console.log(selSort(a));
+function sort(left, right) {
+    let res = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+console.log(mergeSort(a));
