cf3716697a7606b0d34e19a4766de268fe1016db
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-28 12:45:01 -0700

    general review, practice with binary tree breadth first traversals

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,97 +1,64 @@
-// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// // Common operations include node insertion, checking if a node exists, and removal of nodes.
-
-// class Node {
-//     constructor() {
-//         this.children = {};
-//         this.isWord = false;
-//     }
-// }
-
-// class Trie {
-//     constructor() {
-//         this.root = new Node();
-//     }
-
-//     // Insert a word
-//     insert(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToInsert = word[i];
-//             if (!(charToInsert in current.children)) {
-//                 current.children[charToInsert] = new Node();
-//             }
-//             current = current.children[charToInsert];
-//         }
-//         current.isWord = true;
-//     }
-
-//     // Check if trie contains
-//     contains(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToFind = word[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return current.isWord;
-//     }
-
-//     // Check if word starts with prefix
-//     startsWithPrefix(prefix) {
-//         let current = this.root;
-//         for (let i = 0; i < prefix.length; i++) {
-//             let charToFind = prefix[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return true;
-//     }
-
-//     // Print trie
-//     print() {
-//         console.log(this.root);
-//     }
-// }
-
-// let trie = new Trie();
-// trie.insert('Hello');
-// trie.insert('Goodbye');
-// trie.print();
-
-// bt bft 2
+// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// Common operations include node insertion, checking if a node exists, and removal of nodes.
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+    constructor() {
+        this.children = {};
+        this.isWord = false;
     }
 }
 
-// const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
+class Trie {
+    constructor() {
+        this.root = new Node();
+    }
 
-function fib(v, memo={}) {
-    if (v <= 2) return 1;
-    if (v in memo) return memo[v];
-    memo[v] = fib(v-1, memo) + fib(v-2, memo);
-    return memo[v];
+    // Insert a word
+    insert(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToInsert = word[i];
+            if (!(charToInsert in current.children)) {
+                current.children[charToInsert] = new Node();
+            }
+            current = current.children[charToInsert];
+        }
+        current.isWord = true;
+    }
+
+    // Check if trie contains
+    contains(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToFind = word[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return current.isWord;
+    }
+
+    // Check if word starts with prefix
+    startsWithPrefix(prefix) {
+        let current = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let charToFind = prefix[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return true;
+    }
+
+    // Print trie
+    print() {
+        console.log(this.root);
+    }
 }
 
-// console.log(fib(10))
-// console.log(fib(108))
-
-function print(...items) {
-    items.forEach(i => console.log(i));
-}
-
-const a = [1,2,3,4,5,6];
-
-print(a, [1,2,3], [4,5,6]);
+let trie = new Trie();
+trie.insert('Hello');
+trie.insert('Goodbye');
+trie.print();
