b5328bbca0ca65af3fc3b9aab143d08fa7482d16
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-31 19:26:25 -0700

    merge sort review

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,43 +1,10 @@
-// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-// let countries = new Map();
-// countries.set('US', 'North America');
-// countries.set('Canada', 'North America');
-// countries.set('Thailand', 'Asia');
-// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+let countries = new Map();
+countries.set('US', 'North America');
+countries.set('Canada', 'North America');
+countries.set('Thailand', 'Asia');
+console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// // Iteration splits each pair into sub-array
-// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
-
-
-// ms
-
-const a = [11,701,-41.5,331.2,312];
-
-
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
-
-function sort(left, right) {
-    const res = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-
-}
-
-console.log(mergeSort(a));
+// Iteration splits each pair into sub-array
+for (let country of countries) console.log(country[0] + ' > ' + country[1]);
