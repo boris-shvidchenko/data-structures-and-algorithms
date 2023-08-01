440e4cac57001a1a127363030d6914080e7d1cf1
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-07-31 19:24:41 -0700

    merge sort review

--- data-structures/map.js
+++ data-structures/map.js
@@ -15,13 +15,29 @@
 const a = [11,701,-41.5,331.2,312];
 
 
-const ss = (list, res=[]) => {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return ss(list, res);
+function mergeSort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
 }
 
-// console.log(ss(a));
+function sort(left, right) {
+    const res = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+
+}
+
+console.log(mergeSort(a));
