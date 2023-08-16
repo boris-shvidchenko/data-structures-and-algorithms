e8f856fdfb044f4b20bc5a8a8351e2038186a1c9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-08-16 14:38:44 -0700

    quick sort review

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,16 +1,40 @@
-// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-let str = new Set('bookkeeper');
-console.log(str); // Expects ['b','o','k','e','p','r']
+// let str = new Set('bookkeeper');
+// console.log(str); // Expects ['b','o','k','e','p','r']
 
-let nums = new Set();
-nums.add(1);
-nums.add(2);
-nums.add(1); // Will be ignored
-nums.add('Hello');
-nums.add('Hi');
-console.log(nums); // Expects ['1','2','Hello','Hi']
+// let nums = new Set();
+// nums.add(1);
+// nums.add(2);
+// nums.add(1); // Will be ignored
+// nums.add('Hello');
+// nums.add('Hi');
+// console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// Iteration works just like with an array
-for (let char of nums) console.log(char); 
+// // Iteration works just like with an array
+// for (let char of nums) console.log(char); 
 
+
+// qsort
+// msort
+// hash
+// btree
+
+const l = [10,-40,3.4,3.5,6,6,0.34,-74,2323];
+
+const qs = (list) => {
+    if (list.length <= 1) return list;
+    const piv = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < piv) {
+            left.push(list[i]);
+        } else {
+            right.push(list[i]);
+        }
+    }
+    return [...qs(left), piv, ...qs(right)];
+}
+
+console.log(qs(l));
