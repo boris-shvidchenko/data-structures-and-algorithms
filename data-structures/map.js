53fc63b4310a0ae7a850e4cc474dcd5daa5e4c02
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-08-17 14:07:29 -0700

    linked list practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -11,51 +11,84 @@
 
 // ll
 // graph
-// fib 2
 
-// const l = [1,2,3,4,5];
-// const l = [1,-90,3,4.5,5,5,-87];
-
-function bs(list, t) {
-    if (list.length === 1) {
-        if (list[0] === t) return true;
-        return false;
-    }
-    const mid = Math.floor(list.length/2);
-    if (t === list[mid]) return true;
-    if (t < list[mid]) {
-        const left = list.slice(0, mid);
-        return bs(left, t);
-    } else {
-        const right = list.slice(mid);
-        return bs(right, t);
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
 }
 
-// console.log(bs(l, 2))
-// console.log(bs(l, 28))
-
-const selSort = (list, res=[]) => {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return selSort(list, res);
-}
-
-// console.log(selSort(l));
-
-const fib = (v) => {
-    if (v <= 2) return 1;
-    const tab = new Array(v+1).fill(0);
-    tab[1] = 1;
-    for (let i = 0; i < v; i++) {
-        tab[i+1] += tab[i];
-        tab[i+2] += tab[i];
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
     }
-    return tab[v];
+    append(node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(index, node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let prev;
+        let curIndex = 0;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let str = '';
+        let cur = this.head;
+        while (cur !== null) {
+            str += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(str);
+    }
 }
 
-console.log(fib(20))
-console.log(fib(200))
+
+// Test
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.print();
+list.remove('b'); // a -> c -> etc.
+list.appendAt(1,'z');
+list.print();
+
