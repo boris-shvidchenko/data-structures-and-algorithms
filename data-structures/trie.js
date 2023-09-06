3a6e42e0788536fad63a959754b31137f83a9eab
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-05 18:42:27 -0700

    linked list review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -72,43 +72,79 @@
 class Node {
     constructor(val) {
         this.val = val;
-        this.left = null;
-        this.right = null;
+        this.next = null;
     }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function dft(root, t) {
-    if (root === null) return false;
-    if (root.val === t) return true;
-    return dft(root.left, t) || dft(root.right, t);
-}
-// console.log(dft(a));
-// console.log(dft(a, 'v'));
-// console.log(dft(a, 'b'));
-
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left); 
-        if (cur.right !== null) q.push(cur.right);
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
     }
-    return false;
+    append(node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(index, node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let prev;
+        let curIndex = 0;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let cur = this.head;
+        let str = '';
+        while (cur !== null) {
+            str += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(str);
+    }
 }
-// console.log(bft(a));
-console.log(bft(a, 'v'));
-console.log(bft(a, 'b'));
+
+
+// Test
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.print();
+list.remove('b'); // a -> c -> etc.
+list.appendAt(0,'z');
+list.print();
