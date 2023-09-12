f711dc61be99b31b6b34dbe5c4a5a89d86ca55bd
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-11 19:47:44 -0700

    linked list and hashmap review

--- data-structures/map.js
+++ data-structures/map.js
@@ -10,48 +10,138 @@
 // for (let country of countries) console.log(country[0] + ' > ' + country[1]);
 
 
-// bs
 // ms
 // qs
 // ss
 // hash
-// ll
 // bt
 // graph
 
 const l = [1,2,3,4,5,6];
 // const l = [1,-90,3,4.5,333,0.9,-900];
 
-const fib = (v) => {
-    if (v <= 2) return 1;
-    const table = new Array(v+1).fill(0);
-    table[1] = 1;
-    for (let i=0; i<v; i++) {
-        table[i+1] += table[i] ;
-        table[i+2] += table[i] ;
+class Node {
+    constructor(v) {
+        this.v = v;
+        this.next = null;
     }
-    return table[v];
 }
 
-// console.log(fib(20))
-// console.log(fib(200))
-
-function bs(list, t) {
-    if (list.length === 1) {
-        if (list[0] === t) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (t === list[mid]) return true;
-        if (t < list[mid]) {
-            const left = list.slice(0, mid);
-            return bs(left, t);
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+    append(node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
         } else {
-            const right = list.slice(mid);
-            return bs(right, t);
+            while (cur.next !== null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(index, node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let curIndex = 0;
+        let prev;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.v === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.v !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let cur = this.head;
+        let str = '';
+        while (cur !== null) {
+            str += cur.v + '>';
+            cur = cur.next;
+        }
+        console.log(str);
+    }
+}
+
+// Test
+// const list = new LinkedList();
+// list.append('a');
+// list.append('b');
+// list.append('c');
+// list.print();
+// list.remove('b'); // a -> c -> etc.
+// list.appendAt(1,'z');
+// list.print();
+
+
+class HashTable {
+    constructor(size) {
+        this.size = size;
+        this.table = new Array(size);
+    }
+    hash(k) {
+        let t = 0;
+        for (let i=0; i<k.length; i++) {
+            t += k.charCodeAt(i);
+        }
+        return t % this.size;
+    }
+    set(k,v) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        if (!bucket) {
+            this.table[index] = [[k,v]];
+        } else {
+            const sameKey = bucket.find(i => i[0] === k);
+            if (sameKey) {
+                sameKey[1] = v;
+            } else {
+                bucket.push([k,v]);
+            }
         }
     }
+    remove(k) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        const sameKey = bucket.find(i => i[0] === k);
+        bucket.splice(bucket.indexOf(sameKey), 1);
+    }
+    display() {
+        this.table.forEach(i => console.log(i));
+    }
 }
 
-console.log(bs(l, 2))
-console.log(bs(l, 29))
+const table = new HashTable(50);
+table.set('name', 'Ryan');
+table.set('age', 25);
+table.set('color', 'red');
+table.display();
+table.remove('color');
+table.set('mane', 'Clark');
+table.display();
