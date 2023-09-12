eaac3502de8a31cf1cc763244974ea0889183121
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-11 20:28:10 -0700

    practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -10,152 +10,153 @@
 // for (let country of countries) console.log(country[0] + ' > ' + country[1]);
 
 
-// ms
 // bt
 // graph
 
-const l = [12,-902,3,42.52,3323,20.9,-9200];
 
-function qs(list) {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    let left = [];
-    let right = [];
-    for (let i = 1; i<list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
-    }
-    return [...qs(left), piv, ...qs(right)];
-}
+// class Node {
+//     constructor(v) {
+//         this.v = v;
+//         this.edges = [];
+//     }
+//     connect(node) {
+//         this.edges.push(node);
+//         node.edges.push(this);
+//     }
+// }
 
+// class Graph {
+//     constructor(nodes) {
+//         this.nodes = [...nodes];
+//     }
+//     dft(start, end, visited = new Set()) {
+//         if (start === end) {
+//             console.log('found node');
+//         }
+//         console.log('node: ', start.v);
+//         visited.add(start);
+//         for (let a of start.edges) {
+//             if (!visited.has(a)) {
+//                 visited.add(a);
+//                 this.dft(a, end, visited);
+//             }
+//         }
+//     }
+//     bft(start, end) {
+//         const q = [start];
+//         const visited = new Set();
+//         while (q.length > 0) {
+//             const cur = q.shift();
+//             visited.add(cur);
+//             if (cur === end) {
+//                 console.log('Found node');
+//             } 
+//             for (let a of cur.edges) {
+//                 if (!visited.has(a)) {
+//                     visited.add(a);
+//                     q.push(a);
+//                 }
+//             }
+//             console.log('visintg node: ', cur.v);
+//         }
+//     }
+//     sp(start, end) {
+//         const visited = {};
+//         const q = [start];
+//         visited[start.v] = null;
+//         while (q.length > 0) {
+//             const cur = q.shift();
+//             if (cur === start) {
+//                 return this.rp(visited, end);
+//             }
+//             for (let a of cur.edges) {
+//                 if (!visited.hasOwnProperty(a.val)) {
+//                     visited[a.val] = cur;
+//                     q.push(a);
+//                 }
+//             }
+//         }
+//     }
+//     rp(visited, end) {
+//         let path = [];
+//         let cur = end;
+//         while (cur !== null) {
+//             path.push(cur);
+//             cur = visited[cur.v];
+//         }
+//         return path.reverse();
+//     }
+// }
 
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
+// const DFW = new Node('DFW');
+// const JFK = new Node('JFK');
+// const LAX = new Node('LAX');
+// const HNL = new Node('HNL');
+// const SAN = new Node('SAN');
+// const EWR = new Node('EWR');
+// const BOS = new Node('BOS');
+// const MIA = new Node('MIA');
+// const MCO = new Node('MCO');
+// const PBI = new Node('PBI');
+// const HKG = new Node('HKG');
 
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-}
+// const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
 
-console.log(mergeSort(l));
+// DFW.connect(JFK);
+// DFW.connect(LAX);
+// JFK.connect(BOS);
+// JFK.connect(MIA);
+// LAX.connect(HNL);
+// LAX.connect(EWR);
+// LAX.connect(SAN);
+// SAN.connect(HKG);
+// MIA.connect(MCO);
+// MIA.connect(PBI);
+// MCO.connect(PBI);
+
+// console.log(graph.sp(DFW, PBI));
 
 class Node {
     constructor(v) {
         this.v = v;
-        this.edges = [];
-    }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
+        this.left = null;
+        this.right = null;
     }
 }
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
-    }
-    dft(start, end, visited = new Set()) {
-        if (start === end) {
-            console.log('found node');
-        }
-        console.log('node: ', start.v);
-        visited.add(start);
-        for (let a of start.edges) {
-            if (!visited.has(a)) {
-                visited.add(a);
-                this.dft(a, end, visited);
-            }
-        }
-    }
-    bft(start, end) {
-        const q = [start];
-        const visited = new Set();
-        while (q.length > 0) {
-            const cur = q.shift();
-            visited.add(cur);
-            if (cur === end) {
-                console.log('Found node');
-            } 
-            for (let a of cur.edges) {
-                if (!visited.has(a)) {
-                    visited.add(a);
-                    q.push(a);
-                }
-            }
-            console.log('visintg node: ', cur.v);
-        }
-    }
-    sp(start, end) {
-        const visited = {};
-        const q = [start];
-        visited[start.v] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === start) {
-                return this.rp(visited, end);
-            }
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
-            }
-        }
-    }
-    rp(visited, end) {
-        let path = [];
-        let cur = end;
-        while (cur !== null) {
-            path.push(cur);
-            cur = visited[cur.v];
-        }
-        return path.reverse();
-    }
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+function dft(root, t) {
+    if (root === null) return [];
+    const left = dft(root.left, t);
+    const right = dft(root.right, t);
+    return [...left, root.v, ...right]
 }
+console.log(dft(a))
+// console.log(dft(a, 'd'))
+// console.log(dft(a, 'z'))
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-// console.log(graph.sp(DFW, PBI));
+function bft(root, t) {
+    if (root === null) return false;
+    const q = [root];
+    while (q.length > 0) {
+        const cur = q.shift();
+        if (cur.v === t) return true;
+        if (cur.left !== null) q.push(cur.left);
+        if (cur.right !== null) q.push(cur.right);
+    }
+    return false;
+}
+// console.log(bft(a))
+console.log(bft(a, 'd'))
+console.log(bft(a, 'z'))
