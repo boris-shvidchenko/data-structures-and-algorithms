f1977fcdb0695f5b13c3fcfcd767b0c8cd5d6b20
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-13 19:56:19 -0700

    Graph traversal and setup review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,169 +1,64 @@
-// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// // Common operations include node insertion, checking if a node exists, and removal of nodes.
-
-// class Node {
-//     constructor() {
-//         this.children = {};
-//         this.isWord = false;
-//     }
-// }
-
-// class Trie {
-//     constructor() {
-//         this.root = new Node();
-//     }
-
-//     // Insert a word
-//     insert(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToInsert = word[i];
-//             if (!(charToInsert in current.children)) {
-//                 current.children[charToInsert] = new Node();
-//             }
-//             current = current.children[charToInsert];
-//         }
-//         current.isWord = true;
-//     }
-
-//     // Check if trie contains
-//     contains(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToFind = word[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return current.isWord;
-//     }
-
-//     // Check if word starts with prefix
-//     startsWithPrefix(prefix) {
-//         let current = this.root;
-//         for (let i = 0; i < prefix.length; i++) {
-//             let charToFind = prefix[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return true;
-//     }
-
-//     // Print trie
-//     print() {
-//         console.log(this.root);
-//     }
-// }
-
-// let trie = new Trie();
-// trie.insert('Hello');
-// trie.insert('Goodbye');
-// trie.print();
-
-
+// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// Common operations include node insertion, checking if a node exists, and removal of nodes.
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.edges = [];
-    }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
+    constructor() {
+        this.children = {};
+        this.isWord = false;
     }
 }
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
+class Trie {
+    constructor() {
+        this.root = new Node();
     }
-    dft(start, end, visited=new Set()) {
-        if (start === end) {
-            console.log('Found node');
-        }
-        visited.add(start);
-        console.log('Node: ', start.val);
-        for (let a of start.edges) {
-            if (!visited.has(a)) {
-                visited.add(a);
-                this.dft(a, end, visited);
+
+    // Insert a word
+    insert(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToInsert = word[i];
+            if (!(charToInsert in current.children)) {
+                current.children[charToInsert] = new Node();
             }
+            current = current.children[charToInsert];
         }
+        current.isWord = true;
     }
-    bft(start, end) {
-        const visited = new Set();
-        const q = [start];
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) {
-                console.log('Found node');
+
+    // Check if trie contains
+    contains(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToFind = word[i];
+            if (!(charToFind in current.children)) {
+                return false;
             }
-            console.log('Node: ', cur.val);
-            for (let a of cur.edges) {
-                if (!visited.has(a)) {
-                    visited.add(a);
-                    q.push(a);
-                }
-            }
+            current = current.children[charToFind];
         }
+        return current.isWord;
     }
-    sp(start, end) {
-        const visited = {};
-        const q = [start];
-        visited[start.val] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) {
-                return this.rp(visited, end);
+
+    // Check if word starts with prefix
+    startsWithPrefix(prefix) {
+        let current = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let charToFind = prefix[i];
+            if (!(charToFind in current.children)) {
+                return false;
             }
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
-            }
+            current = current.children[charToFind];
         }
+        return true;
     }
-    rp(visited, end) {
-        let cur = end;
-        let path = [];
-        while (cur !== null) {
-            path.push(cur.val);
-            cur = visited[cur.val];
-        }
-        return path.reverse();
+
+    // Print trie
+    print() {
+        console.log(this.root);
     }
 }
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-// graph.bft(DFW, PBI);
-// graph.dft(DFW, HKG);
-console.log(graph.sp(DFW, PBI));
+let trie = new Trie();
+trie.insert('Hello');
+trie.insert('Goodbye');
+trie.print();
