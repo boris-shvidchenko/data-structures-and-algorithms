48ba8b5d1c01910117374ca97ccd89a513f0141a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-18 18:14:24 -0700

    algorithm reveiw

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,30 +1,147 @@
-// Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
+// // Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
 
-class Stack {
-    constructor() {
-        this.items = []; // Will store our items
-    }
-    push(value) {
-        this.items.push(value)
-    }
-    pop() {
-        if (this.items.length !== 0) this.items.pop();
-    }
-    peek() {
-        if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
-    }
-    print() {
-        console.log(this.items.join('|'));
+// class Stack {
+//     constructor() {
+//         this.items = []; // Will store our items
+//     }
+//     push(value) {
+//         this.items.push(value)
+//     }
+//     pop() {
+//         if (this.items.length !== 0) this.items.pop();
+//     }
+//     peek() {
+//         if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
+//     }
+//     print() {
+//         console.log(this.items.join('|'));
+//     }
+// }
+
+// let stack = new Stack();
+// stack.push(1);
+// stack.print();
+// stack.push(2);
+// stack.print();
+// stack.push(3);
+// stack.print();
+// stack.push(4);
+// stack.print();
+// stack.peek(); // Expects 4
+
+const a1 = [1,2,3,4,5,6,7,8];
+const a2 = [1,4,-94,5.6,324,-999843,34];
+
+// bsearch
+// fib memo tab
+// ssort
+// qsort
+// msort
+// hash
+// llist
+// graph
+// btree
+
+const bSearch = (list, t) => {
+    if (list.length === 1) {
+        if (list[0] === t) return true;
+        return false;
+    } else {
+        const mid = Math.floor(list.length/2);
+        if (t === list[mid]) return true;
+        if (t < list[mid]) {
+            const left = list.slice(0, mid);
+            return bSearch(left, t);
+        } else {
+            const right = list.slice(mid);
+            return bSearch(right, t);
+        }
     }
 }
 
-let stack = new Stack();
-stack.push(1);
-stack.print();
-stack.push(2);
-stack.print();
-stack.push(3);
-stack.print();
-stack.push(4);
-stack.print();
-stack.peek(); // Expects 4
+console.log('Binary Search:')
+console.log(bSearch(a1, 3));
+console.log(bSearch(a1, 30));
+
+function fib(v, memo={}) {
+    if (v <= 2) return 1;
+    if (v in memo) return memo[v];
+    memo[v] = fib(v-1, memo) + fib(v-2, memo);
+    return memo[v];
+}
+
+console.log('Fib Memo:')
+console.log(fib(10));
+console.log(fib(100));
+
+function fib2(v) {
+    if (v <= 2) return 1;
+    const table = new Array(v+1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i < v; i++) {
+        table[i+1] += table[i];
+        table[i+2] += table[i];
+    }
+    return table[v];
+}
+
+console.log('Fib Tab:')
+console.log(fib(20));
+console.log(fib(200));
+
+
+
+const selSort = (list, res=[]) => {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selSort(list, res);
+}
+
+console.log('Selection Sort:');
+console.log(selSort(a2));
+
+const qSort = (list) => {
+    if (list.length <= 1) return list;
+    const piv = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < piv) left.push(list[i]);
+        if (list[i] >= piv) right.push(list[i]);
+    }
+    return [...qSort(left), piv, ...qSort(right)];
+}
+
+console.log('Quick sort:')
+console.log(qSort(a2));
+
+
+function mergeSort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
+}
+
+function sort(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    const res = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+console.log('merge sort:');
+console.log(mergeSort(a2));
