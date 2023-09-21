0eb0aa963d94396c2181f03cd029eed05af83bbd
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-20 19:17:32 -0700

    binary tree traversal practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -11,32 +11,50 @@
 
 // hash
 // btree
-// ssort
-// fib tab
 
-const a = [1,0.4,-90,33,2323];
 
-function fib(v) {
-    if (v <= 2) return 1;
-    const tab = new Array(v+1).fill(0);
-    tab[1] = 1;
-    for (let i = 0; i < v; i++) {
-        tab[i+1] += tab[i];
-        tab[i+2] += tab[i];
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
     }
-    return tab[v];
 }
 
-console.log(fib(10));
-console.log(fib(100));
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
 
-function ssort(list, res=[]) {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return ssort(list, res);
+function dft(root, t) {
+    if (root === null) return false;
+    if (root.val === t) return true;
+    return dft(root.left, t) || dft(root.right, t);
 }
 
-console.log(ssort(a));
+// console.log(dft(a));
+console.log(dft(a, 'b'));
+console.log(dft(a, 'z'));
+
+function bft(root, t) {
+    if (root === null) return false;
+    const q = [root];
+    while (q.length > 0) {
+        const cur = q.shift();
+        if (cur.val === t) return true;
+        if (cur.left !== null) q.push(cur.left);
+        if (cur.right !== null) q.push(cur.right);
+    }
+    return false;
+}
+
+// console.log(bft(a));
+console.log(bft(a, 'b'));
+console.log(bft(a, 'z'));
