2574df3e877e21b76107a91df7c098c702de937a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-28 13:47:43 -0700

    tabulation memoization review

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -1,97 +1,116 @@
-// Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
+// // Common operations include push, pull, search, and peek. The below class turns the heap into a max heap.
 
-class Heap {
+// class Heap {
 
-    constructor() {
-        this.data = [];
+//     constructor() {
+//         this.data = [];
+//     }
+
+//     // Helper functions
+
+//     getParentIndex(index) {
+//         return Math.floor((index - 1) / 2);
+//     }
+
+//     getLeftChildIndex(index) {
+//         return index * 2 + 1;
+//     }
+
+//     getRightChildIndex(index) {
+//         return index * 2 + 2;
+//     }
+
+//     swap(index1, index2) {
+//         const temp = this.data[index1];
+//         this.data[index1] = this.data[index2];
+//         this.data[index2] = temp;
+//     }
+
+//     // Operations
+
+//     // Pushes new data (key) into our array/heap.
+//     push(key) {
+//         this.data[this.data.length] = key;
+//         this.heapifyUp();
+//     }
+
+//     // Moves our elements up so that they are in the correct heap order.
+//     heapifyUp() {
+//         let currentIndex = this.data.length - 1;
+//         while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
+//             this.swap(currentIndex, this.getParentIndex(currentIndex));
+//             currentIndex = this.getParentIndex(currentIndex);
+//         }
+//     }
+
+//     heapifyDown() {
+//         let currentIndex = 0;
+//         while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
+//             let biggestChildIndex = this.getLeftChildIndex(currentIndex);
+//             if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
+//                 biggestChildIndex = this.getRightChildIndex(currentIndex);
+//             }
+//             if (this.data[currentIndex] < this.data[biggestChildIndex]) {
+//                 this.swap(currentIndex, biggestChildIndex);
+//                 currentIndex = biggestChildIndex;
+//             } else {
+//                 return;
+//             }
+//         }
+//     }
+
+//     // Remove the top element and replaces it with the last element of our array/heap.
+//     pull() {
+//         const maxValue = this.data[0];
+//         this.data[0] = this.data[this.data.length - 1];
+//         this.data.length--;
+//         this.heapifyDown();
+//         return maxValue;
+//     }
+// }
+
+// const heap = new Heap();
+// console.log(heap);
+// heap.push(25);
+// heap.push(5);
+// heap.push(40);
+// heap.push(70);
+// heap.push(90);
+// heap.push(44);
+
+// console.log(heap.data.join(' > '));
+
+// //        90
+// //       /  \
+// //     70    44
+// //    /  \     \
+// //   5    40    25
+
+// let a = [];
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+// a.push(heap.pull());
+
+// console.log('Top 5 items: ', a);
+// console.log(heap.data.join(' > '));
+
+
+// hash
+// fib
+// graph
+
+const fib = (v) => {
+    if (v <= 2) return 1;
+    const tab = new Array(v+1).fill(0);
+    tab[1] = 1;
+    for (let i = 0; i < v; i++) {
+        tab[i+1] += tab[i];
+        tab[i+2] += tab[i];
     }
-
-    // Helper functions
-
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-
-    getLeftChildIndex(index) {
-        return index * 2 + 1;
-    }
-
-    getRightChildIndex(index) {
-        return index * 2 + 2;
-    }
-
-    swap(index1, index2) {
-        const temp = this.data[index1];
-        this.data[index1] = this.data[index2];
-        this.data[index2] = temp;
-    }
-
-    // Operations
-
-    // Pushes new data (key) into our array/heap.
-    push(key) {
-        this.data[this.data.length] = key;
-        this.heapifyUp();
-    }
-
-    // Moves our elements up so that they are in the correct heap order.
-    heapifyUp() {
-        let currentIndex = this.data.length - 1;
-        while (this.data[currentIndex] > this.data[this.getParentIndex(currentIndex)]) {
-            this.swap(currentIndex, this.getParentIndex(currentIndex));
-            currentIndex = this.getParentIndex(currentIndex);
-        }
-    }
-
-    heapifyDown() {
-        let currentIndex = 0;
-        while (this.data[this.getLeftChildIndex(currentIndex)] !== undefined) {
-            let biggestChildIndex = this.getLeftChildIndex(currentIndex);
-            if (this.data[this.getRightChildIndex(currentIndex)] !== undefined && this.data[this.getRightChildIndex(currentIndex)] > this.data[this.getLeftChildIndex(currentIndex)]) {
-                biggestChildIndex = this.getRightChildIndex(currentIndex);
-            }
-            if (this.data[currentIndex] < this.data[biggestChildIndex]) {
-                this.swap(currentIndex, biggestChildIndex);
-                currentIndex = biggestChildIndex;
-            } else {
-                return;
-            }
-        }
-    }
-
-    // Remove the top element and replaces it with the last element of our array/heap.
-    pull() {
-        const maxValue = this.data[0];
-        this.data[0] = this.data[this.data.length - 1];
-        this.data.length--;
-        this.heapifyDown();
-        return maxValue;
-    }
+    return tab[v];
 }
 
-const heap = new Heap();
-console.log(heap);
-heap.push(25);
-heap.push(5);
-heap.push(40);
-heap.push(70);
-heap.push(90);
-heap.push(44);
-
-console.log(heap.data.join(' > '));
-
-//        90
-//       /  \
-//     70    44
-//    /  \     \
-//   5    40    25
-
-let a = [];
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
-a.push(heap.pull());
-
-console.log('Top 5 items: ', a);
-console.log(heap.data.join(' > '));
+console.log(fib(20))
+console.log(fib(200))
