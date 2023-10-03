6e0516a247869c962073ba259789aba36421c06c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-02 19:51:30 -0700

    algorithm review

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,108 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
-
-
-function fib(v) {
-    if (v <= 2) return 1;
-    const tab = new Array(v+1).fill(0);
-    tab[1] = 1;
-    for (let i = 0; i < v; i++) {
-        tab[i+1] += tab[i];
-        tab[i+2] += tab[i];
-    }
-    return tab[v];
-}
-
-console.log(fib(20))
-console.log(fib(200))
-
-// const l = [1,2,3,4,5];
-const l = [1111,0.5,23,-819,2311,123,5.87,-900];
-
-function bs(list, t) {
-    if (list.length === 1) {
-        if (list[0] === t) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (t === list[mid]) return true;
-        if (t < list[mid]) {
-            const left = list.slice(0, mid);
-            return bs(left, t);
-        } else {
-            const right = list.slice(mid);
-            return bs(right, t);
-        }
-    }
-}
-
-// console.log(bs(l, 2))
-// console.log(bs(l, 9))
-
-function merge(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(merge(left), merge(right));
-}
-
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    const res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-}
-
-// console.log(merge(l));
-
-function selSort(list, res=[]) {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return selSort(list, res);
-}
-
-// console.log(selSort(l));
-
-function selSort(list) {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    const left = [];
-    const right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
-    }
-    return [...selSort(left), piv, ...selSort(right)];
-}
-
-// console.log(selSort(l));
