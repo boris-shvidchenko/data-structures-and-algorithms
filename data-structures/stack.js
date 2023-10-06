eb74648e0ed097cb1e64cfe926f2c0c51a915f8e
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-06 12:45:15 -0700

    algorithm review

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,30 +1,89 @@
-// Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
+// // Stacks are linear collections of elements (like arrays) but are considered 'first-in last-out'. Data can only be removed/added with pop/push type methods.
 
-class Stack {
-    constructor() {
-        this.items = []; // Will store our items
+// class Stack {
+//     constructor() {
+//         this.items = []; // Will store our items
+//     }
+//     push(value) {
+//         this.items.push(value)
+//     }
+//     pop() {
+//         if (this.items.length !== 0) this.items.pop();
+//     }
+//     peek() {
+//         if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
+//     }
+//     print() {
+//         console.log(this.items.join('|'));
+//     }
+// }
+
+// let stack = new Stack();
+// stack.push(1);
+// stack.print();
+// stack.push(2);
+// stack.print();
+// stack.push(3);
+// stack.print();
+// stack.push(4);
+// stack.print();
+// stack.peek(); // Expects 4
+
+
+
+// btree
+
+
+function fib(v) {
+    if (v <= 2) return 1;
+    const tab = new Array(v+1).fill(0);
+    tab[1] = 1;
+    for (let i = 0; i < v; i++) {
+        tab[i+1] += tab[i];
+        tab[i+2] += tab[i];
     }
-    push(value) {
-        this.items.push(value)
-    }
-    pop() {
-        if (this.items.length !== 0) this.items.pop();
-    }
-    peek() {
-        if (this.items.length !== 0) console.log(this.items[this.items.length - 1]);
-    }
-    print() {
-        console.log(this.items.join('|'));
+    return tab[v];
+}
+
+console.log(fib(100))
+console.log(fib(1000))
+
+// const a = [1,2,3,4,5];
+const a = [1,4,-90,3.5,0,32];
+
+function bs(list, t) {
+    if (list.length === 1) {
+        if (list[0] === t) return true;
+        return false;
+    } else {
+        const mid = Math.floor(list.length/2);
+        if (t === list[mid]) return true;
+        if (t < list[mid]) {
+            const left = list.slice(0, mid);
+            return bs(left, t);
+        } else {
+            const right = list.slice(mid);
+            return bs(right, t);
+        }
     }
 }
 
-let stack = new Stack();
-stack.push(1);
-stack.print();
-stack.push(2);
-stack.print();
-stack.push(3);
-stack.print();
-stack.push(4);
-stack.print();
-stack.peek(); // Expects 4
+// console.log(bs(a, 1));
+// console.log(bs(a, 10));
+
+const qs = (list) => {
+    if (list.length <= 1) return list;
+    const piv = list[0];
+    const left = [];
+    const right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < piv) {
+            left.push(list[i]);
+        } else {
+            right.push(list[i]);
+        }
+    }
+    return [...qs(left), piv, ...qs(right)];
+}
+
+// console.log(qs(a));
