ccc485f5fed25e595a30db1f11a417e4718c9c2d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-11 19:56:25 -0700

    file back to original

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,158 +1,64 @@
-// // Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
-// // Common operations include node insertion, checking if a node exists, and removal of nodes.
-
-// class Node {
-//     constructor() {
-//         this.children = {};
-//         this.isWord = false;
-//     }
-// }
-
-// class Trie {
-//     constructor() {
-//         this.root = new Node();
-//     }
-
-//     // Insert a word
-//     insert(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToInsert = word[i];
-//             if (!(charToInsert in current.children)) {
-//                 current.children[charToInsert] = new Node();
-//             }
-//             current = current.children[charToInsert];
-//         }
-//         current.isWord = true;
-//     }
-
-//     // Check if trie contains
-//     contains(word) {
-//         let current = this.root;
-//         for (let i = 0; i < word.length; i++) {
-//             let charToFind = word[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return current.isWord;
-//     }
-
-//     // Check if word starts with prefix
-//     startsWithPrefix(prefix) {
-//         let current = this.root;
-//         for (let i = 0; i < prefix.length; i++) {
-//             let charToFind = prefix[i];
-//             if (!(charToFind in current.children)) {
-//                 return false;
-//             }
-//             current = current.children[charToFind];
-//         }
-//         return true;
-//     }
-
-//     // Print trie
-//     print() {
-//         console.log(this.root);
-//     }
-// }
-
-// let trie = new Trie();
-// trie.insert('Hello');
-// trie.insert('Goodbye');
-// trie.print();
-
+// Tries (prefix tree) are very specific data structures that are based on trees. They are useful when it comes to searching for data. 
+// Common operations include node insertion, checking if a node exists, and removal of nodes.
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+    constructor() {
+        this.children = {};
+        this.isWord = false;
     }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function dft(root, t) {
-    if (root === null) return false;
-    if (root.val === t) return true;
-    return dft(root.left, t) || dft(root.right, t);
-}
-
-// console.log(dft(a));
-console.log(dft(a, 'b'));
-console.log(dft(a, 'cd'));
-
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
+class Trie {
+    constructor() {
+        this.root = new Node();
     }
-    return false;
-}
 
-// console.log(bft(a));
-console.log(bft(a, 'b'));
-console.log(bft(a, 'cs'));
-
-
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let t = 0;
-        for (let i = 0; i < k.length; i++) {
-            t += k.charCodeAt(i);
-        }
-        return t % this.size;
-    }
-    set(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
+    // Insert a word
+    insert(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToInsert = word[i];
+            if (!(charToInsert in current.children)) {
+                current.children[charToInsert] = new Node();
             }
+            current = current.children[charToInsert];
         }
+        current.isWord = true;
     }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
+
+    // Check if trie contains
+    contains(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let charToFind = word[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return current.isWord;
     }
-    display() {
-        this.table.forEach(i => console.log(i));
+
+    // Check if word starts with prefix
+    startsWithPrefix(prefix) {
+        let current = this.root;
+        for (let i = 0; i < prefix.length; i++) {
+            let charToFind = prefix[i];
+            if (!(charToFind in current.children)) {
+                return false;
+            }
+            current = current.children[charToFind];
+        }
+        return true;
+    }
+
+    // Print trie
+    print() {
+        console.log(this.root);
     }
 }
 
-// const table = new HashTable(50);
-// table.set('name', 'Ryan');
-// table.set('age', 25);
-// table.set('color', 'red');
-// table.display();
-// table.remove('color');
-// table.set('mane', 'Clark');
-// table.display();
+let trie = new Trie();
+trie.insert('Hello');
+trie.insert('Goodbye');
+trie.print();
