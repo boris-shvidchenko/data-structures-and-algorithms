7972e1426855613eb1eaab7ae3d624d9c1316d7f
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-23 18:34:28 -0700

    Data structure and algorithm practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -22,138 +22,71 @@
 // quick sort
 
 const l = [1,2,3,4,5];
-// const l = [1,40,-90,3.4,356,-900,2];
+// const l = [1,41110,-90,3.4,356,-900,2];
 
 function mergeSort(list) {}
 function sort(left, right) {}
-function binarySearch(list, t) {}
-function quickSort(list) {}
-function selSort(list, res=[]) {}
-function fibMemo(v, memo={}) {}
-function fibtab(v) {}
-
-
-
-// class Node {
-//     constructor(val) {
-//         this.val = val;
-//         this.next = null;
-//     }
-// }
-// class LinkedList {
-//     constructor() {
-//         this.length = 0;
-//         this.head = null;
-//     }
-//     append(node) {
-//         const newNode = new Node(node);
-//         let cur = this.head;
-//         if (this.head === null) {
-//             this.head = newNode;
-//         } else {
-//             while (cur.next !== null) {
-//                 cur = cur.next;
-//             }
-//             cur.next = newNode;
-//         }
-//         this.length++;
-//     }
-//     appendAt(index, node) {
-//         const newNode = new Node(node);
-//         let cur = this.head;
-//         let curIndex = 0;
-//         let prev;
-//         if (index === 0) {
-//             this.head = newNode;
-//             newNode.next = cur;
-//         } else {
-//             while (curIndex < index) {
-//                 prev = cur;
-//                 cur = cur.next;
-//                 curIndex++;
-//             }
-//             prev.next = newNode;
-//             newNode.next = cur;
-//         }
-//         this.length--;
-//     }
-//     remove(node) {
-//         let cur = this.head;
-//         let prev;
-//         if (this.head.val === node) {
-//             this.head = cur.next;
-//         } else {
-//             while (cur.val !== node) {
-//                 prev = cur;
-//                 cur = cur.next;
-//             }
-//             prev.next = cur.next;
-//         }
-//         this.length--;
-//     }
-//     print() {
-//         let str = '';
-//         let cur = this.head;
-//         while (cur !== null) {
-//             str += cur.val + '>';
-//             cur = cur.next;
-//         }
-//         console.log(str);
-//     }
-// }
-
-// Test
-// const list = new LinkedList();
-// list.append('a');
-// list.append('b');
-// list.append('c');
-// list.print();
-// list.remove('b'); // a -> c -> etc.
-// list.appendAt(1,'z');
-// list.print();
-
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+function binarySearch(list, t) {
+    if (list.length === 1) {
+        if (list[0] === t) return true;
+        return false;
+    } else {
+        const mid = Math.floor(list.length/2);
+        if (list[mid] === t) return true;
+        if (t < list[mid]) {
+            const left = list.slice(0, mid);
+            return binarySearch(left, t);
+        } else {
+            const right = list.slice(mid);
+            return binarySearch(right, t);
+        }
     }
 }
+console.log(binarySearch(l, 2));
+console.log(binarySearch(l, 28));
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// function quickSort(list) {
+//     if (list.length <= 1) return list;
+//     const piv = list[0];
+//     const left = [];
+//     const right = [];
+//     for (let i = 1; i < list.length; i++) {
+//         if (list[i] < piv) left.push(list[i]);
+//         if (list[i] >= piv) right.push(list[i]);
+//     }
+//     return [...quickSort(left), piv, ...quickSort(right)];
+// }
 
-function dft(root, t) {
-    if (root === null) return false;
-    if (root.val === t) return true;
-    return dft(root.left, t) || dft(root.right, t);
-}
-// console.log(dft(a));
-console.log(dft(a, 'b'));
-console.log(dft(a, 'z'));
+// console.log(quickSort(l));
+// function selSort(list, res=[]) {
+//     if (list.length === 0) return res;
+//     const min = Math.min(...list);
+//     const minIndex = list.indexOf(min);
+//     list.splice(minIndex, 1);
+//     res.push(min);
+//     return selSort(list, res);
+// }
 
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
-    }
-    return false;
-}
-// console.log(bft(a));
-console.log(bft(a, 'b'));
-console.log(bft(a, 'z'));
+// console.log(selSort(l));
+// function fibMemo(v, memo={}) {
+//     if (v <= 2) return 1;
+//     if (v in memo) return memo[v];
+//     memo[v] = fibMemo(v-1, memo) + fibMemo(v-2, memo);
+//     return memo[v];
+// }
+// function fibtab(v) {
+//     if (v <= 2) return 1;
+//     const table = new Array(v+1).fill(0);
+//     table[1] = 1;
+//     for (let i = 0; i < v; i++) {
+//         table[i+1] += table[i];
+//         table[i+2] += table[i];
+//     }
+//     return table[v];
+// }
+
+// console.log(fibMemo(10))
+// console.log(fibMemo(100))
+// console.log(fibtab(20))
+// console.log(fibtab(200))
 
