2d9d9dd85d4d3d836cf06da25290413febc9effa
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-26 18:37:57 -0700

    Data structure and algorithm practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,10 +1,122 @@
-// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-let countries = new Map();
-countries.set('US', 'North America');
-countries.set('Canada', 'North America');
-countries.set('Thailand', 'Asia');
-console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+// let countries = new Map();
+// countries.set('US', 'North America');
+// countries.set('Canada', 'North America');
+// countries.set('Thailand', 'Asia');
+// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// Iteration splits each pair into sub-array
-for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+// // Iteration splits each pair into sub-array
+// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
+
+// merge sort
+// sel sort
+// linked list
+// b tree
+
+const a = [3,5,-90,4.3,22];
+
+function mergeSort(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(mergeSort(left), mergeSort(right));
+}
+
+function sort(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let res = [];
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+console.log(mergeSort(a));
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    } 
+    append(node) {
+        let newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(index, node) {
+        let newNode = new Node(node);
+        let cur = this.head;
+        let curIndex = 0;
+        let prev;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let cur = this.head;
+        let str = '';
+        while (cur !== null) {
+            str += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(str);
+    }
+}
+
+// Test
+// const list = new LinkedList();
+// list.append('a');
+// list.append('b');
+// list.append('c');
+// list.print();
+// list.remove('b'); // a -> c -> etc.
+// list.appendAt(1,'z');
+// list.print();
