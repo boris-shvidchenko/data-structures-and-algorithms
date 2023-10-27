86d96982ea00e2081a4aae02476de083399c42bc
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-10-26 18:46:41 -0700

    DSA practice

--- data-structures/map.js
+++ data-structures/map.js
@@ -1,71 +1,10 @@
-// // Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
+// Maps are similar to objects in that keys and values are stored as pairs, however maps allow any data type to be a key (num, array, objects). Objects usually describe a single entity whereas maps can store more complex 'collections' of data.
 
-// let countries = new Map();
-// countries.set('US', 'North America');
-// countries.set('Canada', 'North America');
-// countries.set('Thailand', 'Asia');
-// console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
+let countries = new Map();
+countries.set('US', 'North America');
+countries.set('Canada', 'North America');
+countries.set('Thailand', 'Asia');
+console.log(countries); // Expects: {US => North America, Canada => North America, Thailand => Asia}
 
-// // Iteration splits each pair into sub-array
-// for (let country of countries) console.log(country[0] + ' > ' + country[1]);
-
-
-// sel sort
-// b tree
-
-// const a = [3,5,-90,4.3,22];
-
-function selSort(list, res=[]) {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    list.splice(minIndex, 1);
-    res.push(min);
-    return selSort(list, res);
-}
-
-// console.log(selSort(a));
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null
-    }
-}
-
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-// function dft(root, t) {
-//     if (root === null) return false;
-//     if (root.val === t) return true;
-//     return dft(root.left, t) || dft(root.right, t);
-// }
-// console.log(dft(a));
-// console.log(dft(a, 'b'));
-// console.log(dft(a, 'z'));
-
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
-    }
-    return false;
-}
-// console.log(bft(a));
-console.log(bft(a, 'b'));
-console.log(bft(a, 'z'));
+// Iteration splits each pair into sub-array
+for (let country of countries) console.log(country[0] + ' > ' + country[1]);
