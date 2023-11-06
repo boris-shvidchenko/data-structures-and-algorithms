754a75033177532f271fa19903b87467ce6ce691
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-11-06 08:23:34 -0800

    algorithm practice

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -66,56 +66,143 @@
 // merge sort
 // sel sort
 // quick sort
-// b search
-// fib 2x
-// hash
 // linked list
 // graph
 // b tree
 
-const a = [1,2,3,4,5];
+// const a = [1,2,3,4,5];
 // const a = [1,-90,3.5,66,3,-27];
 
-function fib(val, memo={}) {
-    if (val <= 2) return 1;
-    if (val in memo) return memo[val];
-    memo[val] = fib(val-1, memo) + fib(val-2, memo);
-    return memo[val];
-}
-
-console.log(fib(10));
-console.log(fib(100));
-
-function fib(val) {
-    if (val <= 2) return 1;
-    const tab = new Array(val+1).fill(0);
-    tab[1] = 1;
-    for (let i = 0; i < val; i++) {
-        tab[i+1] += tab[i];
-        tab[i+2] += tab[i];
-    }
-    return tab[val];
-}
-
-console.log(fib(20));
-console.log(fib(200));
-
-const bs = (list, tar) => {
-    if (list.length === 1) {
-        if (list[0] === tar) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (tar === list[mid]) return true;
-        if (tar < list[mid]) {
-            const left = list.slice(0, mid);
-            return bs(left, tar);
-        } else {
-            const right = list.slice(mid);
-            return bs(right, tar);
-        }
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
     }
 }
 
-console.log(bs(a, 2));
-console.log(bs(a, 29));
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+function dft(root, t) {
+    if (root === null) return false;
+    if (root.val === t) return true;
+    return dft(root.left, t) || dft(root.right, t);
+}
+// console.log(dft(a));
+console.log(dft(a, 'b'));
+console.log(dft(a, 'z'));
+
+function bft(root, t) {
+    if (root === null) return false;
+    const q = [root];
+    while (q.length > 0) {
+        const cur = q.shift();
+        if (cur.val === t) return true;
+        if (cur.left !== null) q.push(cur.left);
+        if (cur.right !== null) q.push(cur.right);
+    }
+    return false;
+}
+// console.log(bft(a));
+console.log(bft(a, 'b'));
+console.log(bft(a, 'z'));
+
+// class HashMap {
+//     constructor(size) {
+//         this.size = size;
+//         this.table = new Array(size);
+//     }
+//     hash(k) {
+//         let t = 0;
+//         for (let i = 0; i < k.length; i++) {
+//             t += k.charCodeAt(i);
+//         }
+//         return t % this.size;
+//     }
+//     set(k,v) {
+//         const index = this.hash(k);
+//         const bucket = this.table[index];
+//         if (!bucket) {
+//             this.table[index] = [[k,v]];
+//         } else {
+//             const sameKey = bucket.find(i => i[0] === k);
+//             if (sameKey) {
+//                 sameKey[1] = v;
+//             } else {
+//                 bucket.push([k,v]);
+//             }
+//         }
+//     }
+//     remove(k) {
+//         const index = this.hash(k);
+//         const bucket = this.table[index];
+//         const sameKey = bucket.find(i => i[0] === k);
+//         bucket.splice(bucket.indexOf(sameKey), 1);
+//     }
+//     display() {
+//         this.table.forEach(i => console.log(i));
+//     }
+// }
+
+// const table = new HashMap(50);
+// table.set('name', 'Ryan');
+// table.set('age', 25);
+// table.set('color', 'red');
+// table.display();
+// table.remove('color');
+// table.set('mane', 'Clark');
+// table.display();
+
+// function fib(val, memo={}) {
+//     if (val <= 2) return 1;
+//     if (val in memo) return memo[val];
+//     memo[val] = fib(val-1, memo) + fib(val-2, memo);
+//     return memo[val];
+// }
+
+// console.log(fib(10));
+// console.log(fib(100));
+
+// function fib(val) {
+//     if (val <= 2) return 1;
+//     const tab = new Array(val+1).fill(0);
+//     tab[1] = 1;
+//     for (let i = 0; i < val; i++) {
+//         tab[i+1] += tab[i];
+//         tab[i+2] += tab[i];
+//     }
+//     return tab[val];
+// }
+
+// console.log(fib(20));
+// console.log(fib(200));
+
+// const bs = (list, tar) => {
+//     if (list.length === 1) {
+//         if (list[0] === tar) return true;
+//         return false;
+//     } else {
+//         const mid = Math.floor(list.length/2);
+//         if (tar === list[mid]) return true;
+//         if (tar < list[mid]) {
+//             const left = list.slice(0, mid);
+//             return bs(left, tar);
+//         } else {
+//             const right = list.slice(mid);
+//             return bs(right, tar);
+//         }
+//     }
+// }
+
+// console.log(bs(a, 2));
+// console.log(bs(a, 29));
