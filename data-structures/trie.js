a65c28223f877d4f1cf52c0f815da43e639a0ace
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-11-14 10:17:56 -0800

    Algorithm review

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -64,101 +64,45 @@
 // trie.print();
 
 // btree
-// merge sort
 // quick sort
 // sel sort
-// b search
 
-const l = [1,2,3,4,5];
-// const l = [30,-7,4.5,1,1,34,-0.8];
+const l = [3220,-7,4.5,1,1,34,-0.8];
 
-function bSearch(list, t) {
-    if (list.length === 1) {
-        if (list[0] === t) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (t === list[mid]) return true;
-        if (t < list[mid]) {
-            const left = list.slice(0, mid);
-            return bSearch(left, t);
-        } else {
-            const right = list.slice(mid);
-            return bSearch(right, t);            
-        }
-    }
+function selSort(list, res=[]) {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selSort(list, res);
 }
 
-console.log(bSearch(l, 2))
-console.log(bSearch(l, 29))
+console.log(selSort(l));
 
-// const fib = (val, memo={}) => {
-//     if (val <= 2) return 1;
-//     if (val in memo) return memo[val];
-//     memo[val] = fib(val-1, memo) + fib(val-2, memo);
-//     return memo[val];
-// }
-
-// console.log(fib(10))
-// console.log(fib(100))
-
-// const fib2 = (val) => {
-//     if (val <= 2) return 1;
-//     const table = new Array(val+1).fill(0);
-//     table[1] = 1;
-//     for (let i = 0; i < val; i++) {
-//         table[i+1] += table[i];
-//         table[i+2] += table[i];
-//     }
-//     return table[val];
-// }
-
-// console.log(fib2(20))
-// console.log(fib2(200))
-
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-    }
-    hash(k) {
-        let t = 0;
-        for (let i = 0; i < k.length; i++) {
-            t += k.charCodeAt(i);
-        }
-        return t % this.size;
-    }
-    set(k,v) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        if (!bucket) {
-            this.table[index] = [[k,v]];
-        } else {
-            const sameKey = bucket.find(i => i[0] === k);
-            if (sameKey) {
-                sameKey[1] = v;
-            } else {
-                bucket.push([k,v]);
-            }
-        }
-    }
-    remove(k) {
-        const index = this.hash(k);
-        const bucket = this.table[index];
-        const sameKey = bucket.find(i => i[0] === k);
-        bucket.splice(bucket.indexOf(sameKey), 1);
-    }
-    display() {
-        this.table.forEach(i => console.log(i));
-    }
+function merge(list) {
+    if (list.length <= 1) return list;
+    const mid = Math.floor(list.length/2);
+    const left = list.slice(0, mid);
+    const right = list.slice(mid);
+    return sort(merge(left), merge(right));
 }
 
-// const table = new HashTable(50);
-// table.set('name', 'Ryan');
-// table.set('age', 25);
-// table.set('color', 'red');
-// table.display();
-// table.remove('color');
-// table.set('mane', 'Clark');
-// table.display();
+function sort(left, right) {
+    const res = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+// console.log(merge(l));
 
