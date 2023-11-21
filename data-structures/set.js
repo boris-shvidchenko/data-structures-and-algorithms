16c92a9fcf391e0b1d7baf478be7ecda8b8d4e5d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-11-20 16:43:35 -0800

    Binary tree practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -14,7 +14,7 @@
 // // Iteration works just like with an array
 // for (let char of nums) console.log(char); 
 
-const a = [1,2,3,4,5];
+// const a = [1,2,3,4,5];
 // const a = [-90,3.4,4,0,509,-6.4];
 
 // b search
@@ -23,187 +23,92 @@ const a = [1,2,3,4,5];
 // sel sort
 // fib 2
 // btree
-// hash
-// l list
 
 class Node {
     constructor(val) {
         this.val = val;
-        this.next = null;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+function dft(root, t) {
+    if (root === null) return false;
+    if (root.val === t) return true;
+    return dft(root.left, t) || dft(root.right, t);
+}
+// console.log(dft(a));
+console.log(dft(a, 'b'));
+console.log(dft(a, 'z'));
+
+function bft(root, t) {
+    if (root === null) return false;
+    const q = [root];
+    while (q.length > 0) {
+        const cur = q.shift();
+        if (cur.val === t) return true;
+        if (cur.left !== null) q.push(cur.left);
+        if (cur.right !== null) q.push(cur.right);
     }
-    append(node) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        if (this.head === null) {
-            this.head = newNode;
+    return false;
+}
+// console.log(bft(a));
+console.log(bft(a, 'b'));
+console.log(bft(a, 'z'));
+
+class HashTable {
+    constructor(size) {
+        this.size = size;
+        this.table = new Array(size);
+    }
+    hash(k) {
+        let t = 0;
+        for (let i = 0; i < k.length; i++) {
+            t += k.charCodeAt(i);
+        }
+        return t % this.size;
+    }
+    set(k, v) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        if (!bucket) {
+            this.table[index] = [[k,v]];
         } else {
-            while (cur.next !== null) {
-                cur = cur.next;
+            const sameKey = bucket.find(i => i[0] === k);
+            if (sameKey) {
+                sameKey[1] = v;
+            } else {
+                bucket.push([k,v]);
             }
-            cur.next = newNode;
         }
-        this.length++;
+    } 
+    remove(k) {
+        const index = this.hash(k);
+        const bucket = this.table[index];
+        const sameKey = bucket.find(i => i[0] === k);
     }
-    appendAt(index, node) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        let curIndex = 0;
-        let prev;
-        if (index === 0) {
-            this.head = newNode;
-            newNode.next = cur;
-        } else {
-            while (curIndex < index) {
-                prev = cur;
-                cur = cur.next;
-                curIndex++;
-            }
-            prev.next = newNode;
-            newNode.next = cur;
-        }
-        this.length--;
-    }
-    remove(node) {
-        let cur = this.head;
-        let prev;
-        if (this.head.val === node) {
-            this.head = cur.next;
-        } else {
-            while (cur.val !== node) {
-                prev = cur;
-                cur = cur.next;
-            }
-            prev.next = cur.next;
-        }
-        this.length--;
-    }
-    print() {
-        let s = '';
-        let cur = this.head;
-        while (cur !== null) {
-            s += cur.val + '>';
-            cur = cur.next;
-        }
-        console.log(s);
+    display() {
+        this.table.forEach(i => console.log(i));
     }
 }
 
-// Test
-const list = new LinkedList();
-list.append('a');
-list.append('b');
-list.append('c');
-list.print();
-list.remove('b'); // a -> c -> etc.
-list.appendAt(1,'z');
-list.print();
-
-
-// class Node {
-//     constructor(val) {
-//         this.val = val;
-//         this.edges = [];
-//     }
-//     connect(node) {
-//         this.edges.push(node);
-//         node.edges.push(this);
-//     }
-// }
-
-// class Graph {
-//     constructor(nodes) {
-//         this.nodes = [...nodes];
-//     }
-//     dft(start, end, visited=new Set()) {
-//         if (start === end) {
-//             console.log('Found');
-//         }
-//         console.log(start.val);
-//         visited.add(start);
-//         for (let a of start.edges) {
-//             if (!visited.has(a)) {
-//                 visited.add(a);
-//                 this.dft(a, end, visited);
-//             }
-//         }
-//     }
-//     bft(start, end) {
-//         const visited = new Set();
-//         const q = [start];
-//         while (q.length > 0) {
-//             const cur = q.shift();
-//             if (cur === end) {
-//                 console.log('Found');
-//             }
-//             console.log(cur.val);
-//             for (let a of cur.edges) {
-//                 if (!visited.has(a)) {
-//                     visited.add(a);
-//                     q.push(a);
-//                 }
-//             }
-//         }
-//     }
-//     sp(start, end) {
-//         const visited = {};
-//         const q = [start];
-//         visited[start.val] = null;
-//         while (q.length > 0) {
-//             const cur = q.shift();
-//             if (cur === end) return this.rp(visited, end);
-//             for (let a of cur.edges) {
-//                 if (!visited.hasOwnProperty(a.val)) {
-//                     visited[a.val] = cur;
-//                     q.push(a);
-//                 }
-//             }
-//         }
-//     }
-//     rp(visited, end) {
-//         const path = [];
-//         let cur = end;
-//         while (cur !== null) {
-//             path.push(cur.val);
-//             cur = visited[cur.val];
-//         }
-//         return path.reverse();
-//     }
-// }
-
-
-// const DFW = new Node('DFW');
-// const JFK = new Node('JFK');
-// const LAX = new Node('LAX');
-// const HNL = new Node('HNL');
-// const SAN = new Node('SAN');
-// const EWR = new Node('EWR');
-// const BOS = new Node('BOS');
-// const MIA = new Node('MIA');
-// const MCO = new Node('MCO');
-// const PBI = new Node('PBI');
-// const HKG = new Node('HKG');
-
-// const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-// DFW.connect(JFK);
-// DFW.connect(LAX);
-// JFK.connect(BOS);
-// JFK.connect(MIA);
-// LAX.connect(HNL);
-// LAX.connect(EWR);
-// LAX.connect(SAN);
-// SAN.connect(HKG);
-// MIA.connect(MCO);
-// MIA.connect(PBI);
-// MCO.connect(PBI);
-
-// graph.bft(DFW, PBI);
-// graph.dft(DFW, HKG);
-// console.log(graph.sp(DFW, PBI));
+// const table = new HashTable(50);
+// table.set('name', 'Ryan');
+// table.set('age', 25);
+// table.set('color', 'red');
+// table.display();
+// table.remove('color');
+// table.set('mane', 'Clark');
+// table.display();
