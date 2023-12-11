2554f0aeaf296397b129d0aed0006b5e0fe0d57f
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-12-11 15:13:36 -0800

    Graph review

--- data-structures/set.js
+++ data-structures/set.js
@@ -20,55 +20,107 @@ const l = [2,0.4,-43,23423,23,-77];
 // b tree
 // l list
 // graph
-// fib memo
-// fib tab
 // s sort
 
-const fib = (v, memo={}) => {
-    if (v <= 2) return 1;
-    if (v in memo) return memo[v];
-    memo[v] = fib(v-1, memo) + fib(v-2, memo);
-    return memo[v];
-}
-console.log(fib(10));
-console.log(fib(100));
 
-console.log('Fib 2 => ', fib2(20));;
-console.log('Fib 2 => ', fib2(200));;
-function fib2(val) {
-    if (val <= 2) return 1;
-    const table = new Array(val+1).fill(0);
-    table[1] = 1;
-    for (let i = 0; i < val; i++) {
-        table[i+1] += table[i];
-        table[i+2] += table[i];
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.edges = [];
     }
-    return table[val];
+    connect(node) {
+        this.edges.push(node);
+        node.edges.push(this);
+    }
 }
 
-function mergeSort(list) {
-    if (list.length <= 1) return list;
-    const mid = Math.floor(list.length/2);
-    const left = list.slice(0, mid);
-    const right = list.slice(mid);
-    return sort(mergeSort(left), mergeSort(right));
-}
-
-function sort(left, right) {
-    let leftIndex = 0;
-    let rightIndex = 0;
-    const res = [];
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            res.push(right[rightIndex]);
-            rightIndex++;
+class Graph {
+    // constructor(nodes) {
+    //     this.nodes = [...nodes];
+    // }
+    // dft(start, end, visited = new Set()) {
+    //     if (start === end) {
+    //         console.log('Found')
+    //     } 
+    //     console.log(start.val);
+    //     visited.add(start);
+    //     for (let a of start.edges) {
+    //         if (!visited.has(a)) {
+    //             visited.add(a);
+    //             this.dft(a, end, visited);
+    //         }
+    //     }
+    // }
+    // bft(start, end) {
+    //     const visited = new Set();
+    //     const q = [start];
+    //     while (q.length > 0) {
+    //         const cur = q.shift();
+    //         if (cur === end) {
+    //             console.log('Found');
+    //         }
+    //         console.log(cur.val);
+    //         for (let a of cur.edges) {
+    //             if (!visited.has(a)) {
+    //                 visited.add(a);
+    //                 q.push(a);
+    //             }
+    //         }
+    //     }
+    // }
+    sp(start, end) {
+        const visited = {};
+        const q = [start];
+        visited[start.val] = null;
+        while (q.length > 0) {
+            const cur = q.shift();
+            if (cur === end) return this.rp(visited, end);
+            for (let a of cur.edges) {
+                if (!visited.hasOwnProperty(a.val)) {
+                    visited[a.val] = cur;
+                    q.push(a);
+                }
+            }
         }
     }
-    return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+    rp(visited, end) {
+        const path = [];
+        let cur = end;
+        while (cur !== null) {
+            path.push(cur.val);
+            cur = visited[cur.val];
+        }
+        return path.reverse();
+    }
 }
 
-// console.log(mergeSort(l));
+const DFW = new Node('DFW');
+const JFK = new Node('JFK');
+const LAX = new Node('LAX');
+const HNL = new Node('HNL');
+const SAN = new Node('SAN');
+const EWR = new Node('EWR');
+const BOS = new Node('BOS');
+const MIA = new Node('MIA');
+const MCO = new Node('MCO');
+const PBI = new Node('PBI');
+const HKG = new Node('HKG');
+
+const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
+
+DFW.connect(JFK);
+DFW.connect(LAX);
+JFK.connect(BOS);
+JFK.connect(MIA);
+LAX.connect(HNL);
+LAX.connect(EWR);
+LAX.connect(SAN);
+SAN.connect(HKG);
+MIA.connect(MCO);
+MIA.connect(PBI);
+MCO.connect(PBI);
+
+// graph.bft(DFW, PBI);
+// graph.dft(DFW, HKG);
+console.log(graph.sp(DFW, PBI));
 
