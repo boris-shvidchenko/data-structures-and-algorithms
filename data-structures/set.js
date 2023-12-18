179bb10bf9f2ca3033dfa9eab89ca650e3df5b7d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-12-18 15:11:44 -0800

    Algorithm practice

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,81 +1,16 @@
-// // Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
-// let str = new Set('bookkeeper');
-// console.log(str); // Expects ['b','o','k','e','p','r']
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
-// let nums = new Set();
-// nums.add(1);
-// nums.add(2);
-// nums.add(1); // Will be ignored
-// nums.add('Hello');
-// nums.add('Hi');
-// console.log(nums); // Expects ['1','2','Hello','Hi']
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// // Iteration works just like with an array
-// for (let char of nums) console.log(char); 
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
-
-
-
-
-
-// function qs(list) {
-//     if (list.length <= 2) return list;
-//     const piv = list[0];
-//     const left = [];
-//     const right = [];
-//     for (let i = 1; i < list.length; i++) {
-//         if (list[i] < piv) {
-//             left.push(list[i]);
-//         } else {
-//             right.push(list[i]);
-//         }
-//     }
-//     return [...qs(left), piv, ...qs(right)];
-// }
-
-// console.log(qs(l));
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function dft(root, t) {
-    if (root === null) return false;
-    if (root.val === t) return true;
-    return dft(root.left, t) || dft(root.right, t);
-}
-// console.log(dft(a));
-console.log(dft(a, 'b'));
-console.log(dft(a, 'z'));
-
-function bft(root, t) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === t) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
-    }
-    return false;
-}
-// console.log(bft(a));
-console.log(bft(a, 'b'));
-console.log(bft(a, 'z'));
