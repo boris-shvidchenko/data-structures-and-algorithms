172c190129ccd264778cd36f8bf8da1fd53b6c88
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-12-26 15:32:30 -0800

    Further data structure/alg practice

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -97,33 +97,14 @@
 // console.log(heap.data.join(' > '));
 
 
-// b search
-// merge sort
 // sel sort
 // quick sort
 // hash
 // linked list
-// graph
 
-const l = [1,2,3,4,5];
-// const l = [12,-90,3.23,0.4,-900];
+const l = [12,-90,3.23,0.4,-900];
 
-// function bs(val, t) {
 
-// }
-
-// console.log(bs(l, 2));
-// console.log(bs(l, 43));
-
-// function merge(list) {
-
-// }
-
-// function sort(left, right) {
-
-// }
-
-// console.log(merge(l));
 
 // function selSort(list, res=[]) {
 
@@ -163,133 +144,81 @@ const l = [1,2,3,4,5];
 
 // LINKED LIST
 
-// class Node {
-//     constructor(val) {
-//         this.val = val;
-//         this.next = null;
-//     }
-// }
-
-// class LinkedList {
-//     constructor() {
-//         this.head = null;
-//         this.length = 0;
-//     }
-//     append(node) {}
-//     appendAt(index, node) {}
-//     remove(node) {}
-//     print() {}
-// }
-
-// const list = new LinkedList();
-// list.append('a');
-// list.append('b');
-// list.append('c');
-// list.print();
-// list.remove('b'); // a -> c -> etc.
-// list.appendAt(1,'z');
-// list.print();
-
-// GRAPH
-
 class Node {
     constructor(val) {
         this.val = val;
-        this.edges = [];
-    }
-    connect(node) {
-        this.edges.push(node);
-        node.edges.push(this);
+        this.next = null;
     }
 }
 
-class Graph {
-    constructor(nodes) {
-        this.nodes = [...nodes];
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
     }
-    // dft(start, end, visited=new Set()) {
-    //     if (start === end) {
-    //         console.log('Found');
-    //     }
-    //     console.log(start.val);
-    //     visited.add(start);
-    //     for (let a of start.edges) {
-    //         if (!visited.has(a)) {
-    //             visited.add(a);
-    //             this.dft(a, end, visited);
-    //         }
-    //     }
-    // }
-    // bft(start, end) {
-    //     const q = [start];
-    //     const visited = new Set();
-    //     while (q.length > 0) {
-    //         const cur = q.shift();
-    //         if (cur === end) {
-    //             console.log('Found');
-    //         }
-    //         console.log(cur.val);
-    //         for (let a of cur.edges) {
-    //             if (!visited.has(a)) {
-    //                 visited.add(a);
-    //                 q.push(a);
-    //             }
-    //         }
-    //     }
-    // }
-    sp(start, end) {
-        const q = [start];
-        const visited = {};
-        visited[start.val] = null;
-        while (q.length > 0) {
-            const cur = q.shift();
-            if (cur === end) return this.rp(visited, end);
-            for (let a of cur.edges) {
-                if (!visited.hasOwnProperty(a.val)) {
-                    visited[a.val] = cur;
-                    q.push(a);
-                }
+    append(node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
             }
+            cur.next = newNode;
         }
+        this.length++;
     }
-    rp(visited, end) {
-        const path = [];
-        let cur = end;
-        while (cur !== null) {
-            path.push(cur.val);
-            cur = visited[cur.val];
+    appendAt(index, node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let curIndex = 0;
+        let prev;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
         }
-        return path.reverse();
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let str = '';
+        let cur = this.head;
+        while (cur !== null) {
+            str += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(str);
     }
 }
 
-const DFW = new Node('DFW');
-const JFK = new Node('JFK');
-const LAX = new Node('LAX');
-const HNL = new Node('HNL');
-const SAN = new Node('SAN');
-const EWR = new Node('EWR');
-const BOS = new Node('BOS');
-const MIA = new Node('MIA');
-const MCO = new Node('MCO');
-const PBI = new Node('PBI');
-const HKG = new Node('HKG');
-
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-DFW.connect(JFK);
-DFW.connect(LAX);
-JFK.connect(BOS);
-JFK.connect(MIA);
-LAX.connect(HNL);
-LAX.connect(EWR);
-LAX.connect(SAN);
-SAN.connect(HKG);
-MIA.connect(MCO);
-MIA.connect(PBI);
-MCO.connect(PBI);
-
-// graph.bft(DFW, PBI);
-// graph.dft(DFW, HKG);
-console.log(graph.sp(DFW, PBI));
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.print();
+list.remove('b'); // a -> c -> etc.
+list.appendAt(1,'z');
+list.print();
 
