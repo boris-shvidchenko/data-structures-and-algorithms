b1947f85e924cf6ab9b5e3eab0d279d515dfe9e3
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2024-01-16 15:30:28 -0800

    review of DSA

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,46 +1,16 @@
-// 
+// Similar to arrays, but cannot have duplicate values. Order is also not maintained via indexes (iterating with (for x in arr) wont work).
 
+let str = new Set('bookkeeper');
+console.log(str); // Expects ['b','o','k','e','p','r']
 
+let nums = new Set();
+nums.add(1);
+nums.add(2);
+nums.add(1); // Will be ignored
+nums.add('Hello');
+nums.add('Hi');
+console.log(nums); // Expects ['1','2','Hello','Hi']
 
-// merge sort
-// sel sort
-
-const l = [1,522220,-4,2.533,34];
-
-function selSort(list, res=[]) {
-    if (list.length === 0) return res;
-    const min = Math.min(...list);
-    const minIndex = list.indexOf(min);
-    res.push(min);
-    list.splice(minIndex, 1);
-    return selSort(list, res);
-}
-
-console.log(selSort(l));
-
-// function mergeSort(list) {
-//     if (list.length <= 1) return list;
-//     const mid = Math.floor(list.length/2);
-//     const left = list.slice(0, mid);
-//     const right = list.slice(mid);
-//     return sort(mergeSort(left), mergeSort(right));
-// }
-
-// function sort(left, right) {
-//     let leftIndex = 0;
-//     let rightIndex = 0;
-//     let res = [];
-//     while (leftIndex < left.length && rightIndex < right.length) {
-//         if (left[leftIndex] < right[rightIndex]) {
-//             res.push(left[leftIndex]);
-//             leftIndex++;
-//         } else {
-//             res.push(right[rightIndex]);
-//             rightIndex++;
-//         }
-//     }
-//     return res.concat(left.slice(leftIndex), right.slice(rightIndex));
-// }
-
-// // console.log(mergeSort(l));
+// Iteration works just like with an array
+for (let char of nums) console.log(char); 
 
