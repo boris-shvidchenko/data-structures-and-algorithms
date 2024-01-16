561848c83215f81d9dcb5337efa93c64454cec3b
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2024-01-16 15:28:59 -0800

    review of DSA

--- data-structures/set.js
+++ data-structures/set.js
@@ -5,145 +5,42 @@
 // merge sort
 // sel sort
 
-// fib memo
-// fib tab
+const l = [1,522220,-4,2.533,34];
 
-const l = [1,50,-4,2.5,34];
-
-function fibMemo(v, memo={}) {
-    if (v <= 2) return 1;
-    if (v in memo) return memo[v];
-    memo[v] = fibMemo(v-1, memo) + fibMemo(v-2, memo);
-    return memo[v];
+function selSort(list, res=[]) {
+    if (list.length === 0) return res;
+    const min = Math.min(...list);
+    const minIndex = list.indexOf(min);
+    res.push(min);
+    list.splice(minIndex, 1);
+    return selSort(list, res);
 }
 
-console.log(fibMemo(10));
-console.log(fibMemo(100));
+console.log(selSort(l));
 
-function fibTab(v) {
-    if (v <= 2) return 1;
-    const table = new Array(v+1).fill(0);
-    table[1] = 1;
-    for (let i = 0; i < v; i++) {
-        table[i+1] += table[i];
-        table[i+2] += table[i];
-    }
-    return table[v];
-}
-console.log(fibTab(20))
-console.log(fibTab(200))
+// function mergeSort(list) {
+//     if (list.length <= 1) return list;
+//     const mid = Math.floor(list.length/2);
+//     const left = list.slice(0, mid);
+//     const right = list.slice(mid);
+//     return sort(mergeSort(left), mergeSort(right));
+// }
 
-function bs(list, target) {
-    if (list.length === 1) {
-        if (list[0] === target) return true;
-        return false;
-    } else {
-        const mid = Math.floor(list.length/2);
-        if (target === list[mid]) return true;
-        if (target < list[mid]) {
-            const left = list.slice(0, mid);
-            return bs(left, target);
-        } else {
-            const right = list.slice(mid);
-            return bs(right, target);
-        }
-    }
-}
+// function sort(left, right) {
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+//     let res = [];
+//     while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             res.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             res.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+//     return res.concat(left.slice(leftIndex), right.slice(rightIndex));
+// }
 
-// console.log(bs(l, 2))
-// console.log(bs(l, 23))
+// // console.log(mergeSort(l));
 
-function qs(list) {
-    if (list.length <= 1) return list;
-    const piv = list[0];
-    let left = [];
-    let right = [];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] < piv) {
-            left.push(list[i]);
-        } else {
-            right.push(list[i]);
-        }
-    }
-    return [...qs(left), piv, ...qs(right)];
-}
-// console.log(qs(l));
-
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.length = 0;
-    }
-    append(node) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            while (cur.next !== null) {
-                cur = cur.next;
-            }
-            cur.next = newNode;
-        }
-        this.length++;
-    }
-    appendAt(index, node) {
-        const newNode = new Node(node);
-        let cur = this.head;
-        let prev;
-        let curIndex = 0;
-        if (index === 0) {
-            this.head = newNode;
-            newNode.next = cur;
-        } else {
-            while (curIndex < index) {
-                prev = cur;
-                cur = cur.next;
-                curIndex++;
-            }
-            prev.next = newNode;
-            newNode.next = cur;
-        }
-        this.length++;
-    }
-    remove(node) {
-        let cur = this.head;
-        let prev;
-        if (this.head.val === node) {
-            this.head = cur.next;
-        } else {
-            while (cur.val !== node) {
-                prev = cur;
-                cur = cur.next;
-            }
-            prev.next = cur.next;
-        }
-        this.length--;
-    }
-    print() {
-        let s = '';
-        let cur = this.head;
-        while (cur !== null) {
-            s += cur.val + '>';
-            cur = cur.next;
-        }
-        console.log(s);
-    }
-}
-
-// Test
-// const list = new LinkedList();
-// list.append('a');
-// list.append('b');
-// list.append('c');
-// list.print();
-// list.remove('b'); // a -> c -> etc.
-// list.appendAt(1,'z');
-// list.print();
