51b0f2af6a31d569b2c7136fdc2f723d1fab050a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2024-01-16 15:21:53 -0800

    DSA and algorithm review

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,207 +1,149 @@
 // 
 
-// btree
-// linked list
+
 
 // merge sort
-// q sort
 // sel sort
-// b search
 
 // fib memo
 // fib tab
 
-const l = [1,2,3,4,5];
-// const l = [1,50,-4,2.5,34];
+const l = [1,50,-4,2.5,34];
+
+function fibMemo(v, memo={}) {
+    if (v <= 2) return 1;
+    if (v in memo) return memo[v];
+    memo[v] = fibMemo(v-1, memo) + fibMemo(v-2, memo);
+    return memo[v];
+}
+
+console.log(fibMemo(10));
+console.log(fibMemo(100));
+
+function fibTab(v) {
+    if (v <= 2) return 1;
+    const table = new Array(v+1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i < v; i++) {
+        table[i+1] += table[i];
+        table[i+2] += table[i];
+    }
+    return table[v];
+}
+console.log(fibTab(20))
+console.log(fibTab(200))
+
+function bs(list, target) {
+    if (list.length === 1) {
+        if (list[0] === target) return true;
+        return false;
+    } else {
+        const mid = Math.floor(list.length/2);
+        if (target === list[mid]) return true;
+        if (target < list[mid]) {
+            const left = list.slice(0, mid);
+            return bs(left, target);
+        } else {
+            const right = list.slice(mid);
+            return bs(right, target);
+        }
+    }
+}
+
+// console.log(bs(l, 2))
+// console.log(bs(l, 23))
+
+function qs(list) {
+    if (list.length <= 1) return list;
+    const piv = list[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < piv) {
+            left.push(list[i]);
+        } else {
+            right.push(list[i]);
+        }
+    }
+    return [...qs(left), piv, ...qs(right)];
+}
+// console.log(qs(l));
 
 class Node {
     constructor(val) {
         this.val = val;
-        this.left = null;
-        this.right = null;
+        this.next = null;
     }
 }
 
-const a = new Node('a');
-const b = new Node('b');
-const c = new Node('c');
-const d = new Node('d');
-const e = new Node('e');
-const f = new Node('f');
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-function dft(root, target) {
-    if (root === null) return false;
-    if (root.val === target) return true;
-    return dft(root.left, target) || dft(root.right, target);
-}
-// console.log(dft(a));
-console.log(dft(a, 'b'));
-console.log(dft(a, 'z'));
-
-function bft(root, target) {
-    if (root === null) return false;
-    const q = [root];
-    while (q.length > 0) {
-        const cur = q.shift();
-        if (cur.val === target) return true;
-        if (cur.left !== null) q.push(cur.left);
-        if (cur.right !== null) q.push(cur.right);
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
     }
-    return false;
+    append(node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            while (cur.next !== null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        this.length++;
+    }
+    appendAt(index, node) {
+        const newNode = new Node(node);
+        let cur = this.head;
+        let prev;
+        let curIndex = 0;
+        if (index === 0) {
+            this.head = newNode;
+            newNode.next = cur;
+        } else {
+            while (curIndex < index) {
+                prev = cur;
+                cur = cur.next;
+                curIndex++;
+            }
+            prev.next = newNode;
+            newNode.next = cur;
+        }
+        this.length++;
+    }
+    remove(node) {
+        let cur = this.head;
+        let prev;
+        if (this.head.val === node) {
+            this.head = cur.next;
+        } else {
+            while (cur.val !== node) {
+                prev = cur;
+                cur = cur.next;
+            }
+            prev.next = cur.next;
+        }
+        this.length--;
+    }
+    print() {
+        let s = '';
+        let cur = this.head;
+        while (cur !== null) {
+            s += cur.val + '>';
+            cur = cur.next;
+        }
+        console.log(s);
+    }
 }
-// console.log(bft(a));
-console.log(bft(a, 'b'));
-console.log(bft(a, 'z'));
 
-// class HashTable {
-//     constructor(size) {
-//         this.size = size;
-//         this.table = new Array(size);
-//     }
-//     hash(k) {
-//         let t = 0;
-//         for (let i = 0; i<k.length; i++) {
-//             t += k.charCodeAt(i);
-//         }
-//         return t % this.size;
-//     }
-//     set(k,v) {
-//         const index = this.hash(k);
-//         const bucket = this.table[index];
-//         if (!bucket) {
-//             this.table[index] = [[k,v]];
-//         } else {
-//             const sameKey = bucket.find(i => i[0] === k);
-//             if (sameKey) {
-//                 sameKey[1] = v;
-//             } else {
-//                 bucket.push([k,v]);
-//             }
-//         }
-//     }
-//     remove(k) {
-//         const index = this.hash(k);
-//         const bucket = this.table[index];
-//         const sameKey = bucket.find(i => i[0] === k);
-//         bucket.splice(bucket.indexOf(sameKey), 1);
-//     }
-//     display() {
-//         this.table.forEach(i => console.log(i));
-//     }
-// }
-
-// const table = new HashTable(50);
-// table.set('name', 'Ryan');
-// table.set('age', 25);
-// table.set('color', 'red');
-// table.display();
-// table.remove('color');
-// table.set('mane', 'Clark');
-// table.display();
-
-// class Node {
-    // constructor(val) {
-    //     this.val = val;
-    //     this.edges = [];
-    // }
-    // connect(node) {
-    //     this.edges.push(node);
-    //     node.edges.push(this);
-    // }
-// }
-
-// class Graph {
-//     constructor(nodes) {
-//         this.nodes = [...nodes];
-//     }
-//     // dft(start, end, visited = new Set()) {
-//     //     if (start === end) {
-//     //         console.log('Found Node');
-//     //     }
-//     //     console.log(start.val);
-//     //     visited.add(start);
-//     //     for (let a of start.edges) {
-//     //         if (!visited.has(a)) {
-//     //             visited.add(a);
-//     //             this.dft(a, end, visited);
-//     //         }
-//     //     }
-//     // }
-//     // bft(start, end) {
-//     //     const visited = new Set();
-//     //     const q = [start];
-//     //     while (q.length > 0) {
-//     //         const cur = q.shift();
-//     //         if (cur === end) {
-//     //             console.log("Found node");
-//     //         }
-//     //         for (let a of cur.edges) {
-//     //             if (!visited.has(a)) {
-//     //                 visited.add(a);
-//     //                 q.push(a);
-//     //             }
-//     //         }
-//     //         console.log(cur.val);
-//     //     }
-//     // }
-//     sp(start, end) {
-//         const visited = {};
-//         const q = [start];
-//         visited[start.val] = null;
-//         while (q.length > 0) {
-//             const cur = q.shift();
-//             if (cur === end) return this.rp(visited, end);
-//             for (let a of cur.edges) {
-//                 if (!visited.hasOwnProperty(a.val)) {
-//                     visited[a.val] = cur;;
-//                     q.push(a);
-//                 }
-//             }
-//         }
-//     }
-//     rp(visited, end) {
-//         let cur = end;
-//         let path = [];
-//         while (cur !== null) {
-//             path.push(cur.val);
-//             cur = visited[cur.val];
-//         }
-//         return path.reverse();
-//     }
-// }
-
-// const DFW = new Node('DFW');
-// const JFK = new Node('JFK');
-// const LAX = new Node('LAX');
-// const HNL = new Node('HNL');
-// const SAN = new Node('SAN');
-// const EWR = new Node('EWR');
-// const BOS = new Node('BOS');
-// const MIA = new Node('MIA');
-// const MCO = new Node('MCO');
-// const PBI = new Node('PBI');
-// const HKG = new Node('HKG');
-
-// const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG]);
-
-// DFW.connect(JFK);
-// DFW.connect(LAX);
-// JFK.connect(BOS);
-// JFK.connect(MIA);
-// LAX.connect(HNL);
-// LAX.connect(EWR);
-// LAX.connect(SAN);
-// SAN.connect(HKG);
-// MIA.connect(MCO);
-// MIA.connect(PBI);
-// MCO.connect(PBI);
-
-// // graph.bft(DFW, PBI);
-// // console.log("==============")
-// // graph.dft(DFW, HKG);
-// console.log(graph.sp(DFW, PBI));
+// Test
+// const list = new LinkedList();
+// list.append('a');
+// list.append('b');
+// list.append('c');
+// list.print();
+// list.remove('b'); // a -> c -> etc.
+// list.appendAt(1,'z');
+// list.print();
